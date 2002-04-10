d97619e3b11b5341e34d7828a71ebca4d1424c24
Ryan Cumming <ryan@kde.org>
2002-04-10 03:49:48 +0000

    KopeteStdAction updates
    svn path=/trunk/kdenetwork/kopete/; revision=148337

--- libkopete/ui/kopetestdaction.cpp
+++ libkopete/ui/kopetestdaction.cpp
@@ -5,6 +5,25 @@
 #include "kopetestdaction.h"
 #include "contactlist.h"
 
+/** KopeteGroupList **/
+KopeteGroupList::KopeteGroupList(const QString& text, const QString& pix, const KShortcut& cut, const QObject* receiver, const char* slot, QObject* parent, const char* name)
+                : KListAction(text, pix, cut, receiver, slot, parent, name)
+{
+	connect(kopeteapp->contactList(), SIGNAL(groupAdded(const QString &)), this, SLOT(slotUpdateList()));
+	connect(kopeteapp->contactList(), SIGNAL(groupRemoved(const QString &)), this, SLOT(slotUpdateList()));
+	slotUpdateList();
+}
+
+KopeteGroupList::~KopeteGroupList()
+{
+}
+
+void KopeteGroupList::slotUpdateList()
+{
+	setItems(kopeteapp->contactList()->groups());
+}
+
+/** KopeteStdAction **/
 KAction* KopeteStdAction::sendMessage(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0)
 {
 	return new KAction("&Send Message", "mail_generic", 0, recvr, slot, parent, name);
@@ -12,7 +31,7 @@ KAction* KopeteStdAction::sendMessage(const QObject *recvr, const char *slot, QO
 
 KAction* KopeteStdAction::contactInfo(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0)
 {
-	return new KAction("Contact &Info", "identity", 0, recvr, slot, parent, name);
+	return new KAction("User &Info", "identity", 0, recvr, slot, parent, name);
 }
 
 KAction* KopeteStdAction::viewHistory(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0)
@@ -27,9 +46,7 @@ KAction* KopeteStdAction::addGroup(const QObject *recvr, const char *slot, QObje
 
 KListAction *KopeteStdAction::moveContact(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0)
 {
-	KListAction *tmp = new KListAction("&Move Contact", "editcut", 0, recvr, slot, parent, name);
-	tmp->setItems(kopeteapp->contactList()->groups());
-	return tmp;
+	return new KopeteGroupList("&Move Contact", "editcut", 0, recvr, slot, parent, name);
 }
 
 KAction* KopeteStdAction::deleteContact(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0)
--- libkopete/ui/kopetestdaction.h
+++ libkopete/ui/kopetestdaction.h
@@ -2,8 +2,7 @@
 #define KOPETESTDACTION_H
 
 #include <kaction.h>
-
-class QObject;
+#include <qobject.h>
 
 class KopeteStdAction
 {
@@ -17,6 +16,19 @@ class KopeteStdAction
 		static KAction *addGroup(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0);
 		static KListAction *moveContact(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0);
 		static KAction *deleteContact(const QObject *recvr, const char *slot, QObject* parent, const char *name = 0);
+};
+
+// Fuckin' moc will only see signals/slots if they're defined in the .h
+// This is a private class :(
+class KopeteGroupList : public KListAction
+{
+	Q_OBJECT
+	public:
+		KopeteGroupList(const QString &, const QString &, const KShortcut &, const QObject *, const char *, QObject *, const char *);
+		~KopeteGroupList();
+
+	protected slots:
+		void slotUpdateList();
 };
 
 #endif
