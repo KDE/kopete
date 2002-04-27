945f72e6aff091dec348a17b6a3f4cb412624bb6
Duncan Mac-Vicar Prett <duncan@kde.org>
2002-04-27 20:15:40 +0000

    - Sound effect is now played at arrive time (to the notifier) - License changes in my code to LGPL (still waiting auth for the rest of libkopete)
    svn path=/trunk/kdenetwork/kopete/; revision=152321

--- kopete/kopete.cpp
+++ kopete/kopete.cpp
@@ -1,17 +1,27 @@
 /***************************************************************************
-                          Kopete Instant Messenger
-                                kopete.cpp
-                             -------------------
-                (C) 2001-2002 by Duncan Mac-Vicar P. <duncan@kde.org>
- ***************************************************************************
+    kopete.cpp - Kopete Instant Messenger Main Class
+
+    (C) 2001-2002 by Duncan Mac-Vicar P. <duncan@kde.org>
 
  ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+
+/***************************************************************************
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ *
  ***************************************************************************/
 
 #include "kopete.h"
