4fcbf59b7fb925d91e0aedb32a9cef1820ef05dc
Duncan Mac-Vicar Prett <duncan@kde.org>
2002-05-27 20:08:49 +0000

    Added Andres to active developers list
    svn path=/trunk/kdenetwork/kopete/; revision=158134

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char *argv[])
 	aboutData.addAuthor ( "Richard Stellingwerff", I18N_NOOP("features and bugfixes"), "remenic@linuxfromscratch.org");
 	aboutData.addAuthor ( "Daniel Stone", I18N_NOOP("Jabber plugin, random hacks"), "dstone@kde.org", "http://raging.dropbear.id.au");
 	aboutData.addAuthor ( "Stefan Gehn", I18N_NOOP("cleanups, features and bugfixes") );
+    aboutData.addAuthor ( "Andres Krapf", I18N_NOOP("random hacks and bugfixes"), "dae@chez.com" );
 
 	aboutData.addCredit ( "Herwin Jan Steehouwer", I18N_NOOP("KxEngine ICQ code") );
 	aboutData.addCredit ( "Olaf Lueg", I18N_NOOP("Kmerlin MSN Code") );
