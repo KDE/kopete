24c8753100c43cf4ac479f9b368b68fbcb3290d7
Andy Goossens <andygoossens@gmail.com>
2002-10-27 18:51:38 +0000

    new command-line options:
    kopete --disable <plugin>    prevents a plugin from loading
    kopete <pluginA> <pluginB>   only load plugin A and B
    
    svn path=/trunk/kdenetwork/kopete/; revision=186018

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -33,6 +33,8 @@ static const char *description =
 static KCmdLineOptions options[] =
 {
 	{ "noplugins", I18N_NOOP("Do not load plugins"), 0 },
+	{ "disable <plugin>", I18N_NOOP("Do not load specified plugin"), 0 },
+	{ "!+[plugin]", I18N_NOOP("Load specified plugins"), 0 },
 	{ 0, 0, 0 }
 };
 
