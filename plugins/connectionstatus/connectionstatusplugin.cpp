c7e3eb2eda7059925471eda577f94500327dfa33
Chris Howells <howells@kde.org>
2002-10-28 21:00:55 +0000

    Adding my connection status plugin.
    svn path=/trunk/kdenetwork/kopete/; revision=186251

---
+++ plugins/connectionstatus/connectionstatusplugin.cpp
@@ -0,0 +1,122 @@
+/***************************************************************************
+                          connectionstatusplugin.cpp  -  description
+                             -------------------
+    begin                : 26th Oct 2002
+    copyright            : (C) 2002 by Chris Howells, Duncan Mac-Vicar Prett
+    email                : howells@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <kdebug.h>
+#include <kgenericfactory.h>
+#include <qtimer.h>
+#include <kprocess.h>
+
+#include "connectionstatusplugin.h"
+#include "connectionstatuspreferences.h"
+#include "kopete.h"
+
+K_EXPORT_COMPONENT_FACTORY(kopete_connectionstatus, KGenericFactory<ConnectionStatusPlugin>);
+
+ConnectionStatusPlugin::ConnectionStatusPlugin(QObject *parent, const char *name,
+	const QStringList& /* args */ )
+: KopetePlugin(parent, name)
+{
+	m_ConnectionStatusPrefs = new ConnectionStatusPreferences("kppp", this );
+
+	kdDebug() << "ConnectionStatusPlugin::ConnectionStatusPlugin()" << endl;
+	qtTimer = new QTimer();
+	connect(qtTimer, SIGNAL(timeout()), this,
+		 SLOT(slotCheckStatus()) );
+	qtTimer->start(2500);
+	
+	m_boolPluginConnected = false;
+}
+
+ConnectionStatusPlugin::~ConnectionStatusPlugin()
+{
+	kdDebug() << "ConnectionStatusPlugin::~ConnectionStatusPlugin()" << endl;
+	delete qtTimer;
+	delete m_ConnectionStatusPrefs;
+}
+
+void ConnectionStatusPlugin::init()
+{
+}
+
+bool ConnectionStatusPlugin::unload()
+{
+}
+
+void ConnectionStatusPlugin::slotCheckStatus()
+{
+	/* Use KProcess to run ifconfig. We'll then parse the output of *
+	* ifconfig in slotProcessStdout() to see if it mentions the     *
+	* relevant network interface. If so, we're connected, if not, we're*
+	* offline * */
+	
+	if (m_ConnectionStatusPrefs->interface() == i18n("Modem"))
+	{
+		m_qsInterface = "ppp0";
+	}
+	else
+	if (m_ConnectionStatusPrefs->interface() == i18n("Broadband or LAN"))
+	{
+		m_qsInterface = "eth0";
+	}
+	kdDebug() << "ConnectionStatusPlugin::checkStatus()" << endl;
+	kdDebug() << "Using interface: " << m_qsInterface << endl;
+	KProcess *kpIfconfig = new KProcess;
+	*kpIfconfig << "/sbin/ifconfig";
+	kpIfconfig->start(KProcess::DontCare, KProcess::Stdout);
+	connect(kpIfconfig, SIGNAL(receivedStdout(KProcess *, char *, int)),
+		this, SLOT(slotProcessStdout(KProcess *, char *, int)));
+}
+
+void ConnectionStatusPlugin::slotProcessStdout(KProcess *, char *buffer, int buflen)
+{
+	QString qsBuffer = QString::fromLatin1(buffer, buflen);
+	//kdDebug() << qsBuffer << endl;
+	setConnectedStatus(qsBuffer.contains(m_qsInterface));
+}
+
+void ConnectionStatusPlugin::setConnectedStatus(bool connected)
+{
+	/* We have to handle a few cases here. First is the machine is connected, and the plugin thinks
+	* we're connected. Then we don't do anything. Next, we can have machine connected, but plugin thinks
+	* we're disconnected. Then we call slotConnectAll(). Also, machine disconnected, plugin disconnected -- we
+	* don't do anything. Finally, we can have the machine disconnected, and the plugin thinks we're
+	* connected -- then we call slotDisconnectAll(). This mechanism is required so that we don't
+	* keep calling slotConnectAll() or slotDisconnectAll() constantly.
+	*/
+	
+	kdDebug() << "Entering ifs" << endl;
+	
+	if (connected && !m_boolPluginConnected) // the machine is connected and plugin thinks we're disconnected
+	{
+		kdDebug() << "Setting m_boolPluginConnected to true" << endl;
+		m_boolPluginConnected = true;
+		kdDebug() << "ConnectionStatusPlugin::setConnectedStatus() -- we're connected" << endl;
+		kopeteapp->slotConnectAll();
+	}
+	else
+	if (!connected && m_boolPluginConnected) // the machine isn't connected and plugin thinks we're connected
+	{
+		kdDebug() << "Setting m_boolPluginConnected to false" << endl;
+		m_boolPluginConnected = false;
+		kdDebug() << "ConnectionStatusPlugin::setConnectedStatus() -- we're offline" << endl;
+		kopeteapp->slotDisconnectAll();
+	}
+}
+
+
+// vim: set noet ts=4 sts=4 sw=4:
+
---
+++ plugins/connectionstatus/connectionstatusplugin.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+                          connectionstatusplugin.h -  description
+                             -------------------
+    begin                : 26th Oct 2002
+    copyright            : (C) 2002 by Chris Howells, Duncan Mac-Vicar Prett
+    email                : howells@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CONNECTIONSTATUSPLUGIN_H
+#define CONNECTIONSTATUSPLUGIN_H
+
+#include "kopeteplugin.h"
+
+class ConnectionStatusPreferences;
+class QTimer;
+class KProcess;
+
+/**
+ * @author Chris Howells
+ */
+class ConnectionStatusPlugin : public KopetePlugin
+{
+	Q_OBJECT
+
+public:
+	ConnectionStatusPlugin( QObject *parent, const char *name, const QStringList &args );
+	~ConnectionStatusPlugin();
+	virtual void init();
+	virtual bool unload();
+
+private slots:
+	void slotCheckStatus();
+	void slotProcessStdout(KProcess *, char *, int);
+
+private:
+	QTimer *qtTimer;
+	ConnectionStatusPreferences *m_ConnectionStatusPrefs;
+	void setConnectedStatus(bool);
+	bool m_boolPluginConnected;
+	QString m_qsInterface;
+
+};
+
+#endif
+
+// vim: set noet ts=4 sts=4 sw=4:
+
