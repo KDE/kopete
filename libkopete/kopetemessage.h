75a968687749026f2c16214ed8e3b363391bad3c
Christopher TenHarmsel <epchris@gmail.com>
2002-11-05 05:51:38 +0000

    Added Doxygen comments
    svn path=/trunk/kdenetwork/kopete/; revision=187459

--- libkopete/kopetemessage.h
+++ libkopete/kopetemessage.h
@@ -51,61 +51,201 @@ public:
 	*/
 	enum MessageFormat  { PlainText, RichText, ParsedHTML };
 
-	/*
-		Constructs a new message
-		Please note that body -must- be valid HTML, so all HTML control
-		characters must be escaped.
-	*/
+	/**
+	 * Constructs a new message
+	 * Please note that body -must- be valid HTML, so all HTML control
+	 * characters must be escaped.
+	 */
 	KopeteMessage();
-	KopeteMessage(const KopeteContact *, KopeteContactPtrList, QString, MessageDirection, MessageFormat f=PlainText );
-	KopeteMessage(const KopeteContact*, KopeteContactPtrList, QString, QString, MessageDirection, MessageFormat f=PlainText );
 
-	KopeteMessage(QDateTime, const KopeteContact *, KopeteContactPtrList, QString, MessageDirection, MessageFormat f=PlainText);
-	KopeteMessage(QDateTime, const KopeteContact *, KopeteContactPtrList, QString, QString, MessageDirection, MessageFormat f=PlainText);
+	/**
+	 * Constructs a new message
+	 * Please note that body -must- be valid HTML, so all HTML control
+	 * characters must be escaped.
+	 * @param fromKC The KopeteContact that the message is coming from
+	 * @param toKC List of KopeteContacts the message is going to
+	 * @param body Message body
+	 * @param direction The direction of the message, KopeteMessage::Inbound, KopeteMessage::Outbound, KopeteMessage::Internal
+	 * @param f Format of the message
+	 */
+	KopeteMessage(const KopeteContact *fromKC, KopeteContactPtrList toKC,
+									QString body, MessageDirection direction, MessageFormat f=PlainText );
+
+	/**
+	 * Constructs a new message
+	 * Please note that body -must- be valid HTML, so all HTML control
+	 * characters must be escaped.
+	 * @param fromKC The KopeteContact that the message is coming from
+	 * @param toKC List of KopeteContacts the message is going to
+	 * @param body Message body
+	 * @param subject The subject of the message
+	 * @param direction The direction of the message, KopeteMessage::Inbound, KopeteMessage::Outbound, KopeteMessage::Internal
+	 * @param f Format of the message
+	 */
+	KopeteMessage(const KopeteContact* fromKC, KopeteContactPtrList toKC, QString body,
+									QString subject, MessageDirection direction, MessageFormat f=PlainText );
+
+	/**
+	 * Constructs a new message
+	 * Please note that body -must- be valid HTML, so all HTML control
+	 * characters must be escaped.
+	 * @param timeStamp Timestamp for the message
+	 * @param fromKC The KopeteContact that the message is coming from
+	 * @param toKC List of KopeteContacts the message is going to
+	 * @param body Message body
+	 * @param direction The direction of the message, KopeteMessage::Inbound, KopeteMessage::Outbound, KopeteMessage::Internal
+	 * @param f Format of the message
+	 */
+	KopeteMessage(QDateTime timeStamp, const KopeteContact *fromKC, KopeteContactPtrList toKC,
+									QString body, MessageDirection direction, MessageFormat f=PlainText);
+
+	/**
+	 * Constructs a new message
+	 * Please note that body -must- be valid HTML, so all HTML control
+	 * characters must be escaped.
+	 * @param timeStamp Timestamp for the message
+	 * @param fromKC The KopeteContact that the message is coming from
+	 * @param toKC List of KopeteContacts the message is going to
+	 * @param body Message body
+	 * @param subject The subject of the message
+	 * @param direction The direction of the message, KopeteMessage::Inbound, KopeteMessage::Outbound, KopeteMessage::Internal
+	 * @param f Format of the message
+	 */
+	KopeteMessage(QDateTime timeStamp, const KopeteContact *fromKC, KopeteContactPtrList toKC,
+									QString body, QString subject, MessageDirection direction, MessageFormat f=PlainText);
+	
 
 	// Accessors
+	
+	/**
+	 * Accessor method for the timestamp of the message
+	 * @return The message's timestamp
+	 */
 	QDateTime timestamp() const { return mTimestamp; }
 
+	/**
+	 * Accessor method for the KopeteContact that sent this message
+	 * @return The KopeteContact who sent this message
+	 */
 	const KopeteContact *from() const { return mFrom; }
+
+	/**
+	 * Accessor method for the KopeteContacts that this message was sent to
+	 * @return Pointer list of the KopeteContacts this message was sent to
+	 */
 	KopeteContactPtrList to() const { return mTo; }
+
+	/**
+	 * Acessor method for the foreground color
+	 * @return The message's foreground color
+	 */
 	QColor fg() const { return mFg; }
+
+	/**
+	 * Accessor method for the background color of the message
+	 * @return The message's background color
+	 */
 	QColor bg() const { return mBg; }
+
+	/**
+	 * Accessor method for the font used in the message
+	 * @return The message's font
+	 */
 	QFont font() const { return mFont; }
+
+	/**
+	 * Accessor method for the body of the message
+	 * @return The message body
+	 */
 	QString body() const { return mBody; }
+
+	/**
+	 * Accessor method for the subject of the message
+	 * @return The message subject
+	 */
 	QString subject() const { return mSubject; }
+
+	/**
+	 * Accessor method for the format of the message
+	 * @return The message format
+	 */
 	MessageFormat format() const { return mFormat; }
-
-	MessageDirection direction() const { return mDirection; }
-
-	// Mutators
-	void setFg(QColor color);
-	void setBg(QColor color);
-	void setFont(QFont font);
-	void setBody( const QString& body , MessageFormat f=PlainText );
-
-	/*
-		Access to body whith specifiedFormat
-	*/
-	//plain text
-	QString plainBody() const ;
-	//HTML escaped
-	QString escapedBody() const ;
-	//Parsed (HTML and Emoticon, ready to use in the chatwindow)
-	QString parsedBody() const ;
 	
+	/**
+	 * Accessor method for the direction of the message
+	 * @return The message direction
+	 */
+	MessageDirection direction() const { return mDirection; }
+	
+	/**
+	 * Sets the foreground color for the message
+	 * @param color The color
+	 */
+	void setFg(QColor color);
+
+	/**
+	 * Sets the background color for the message
+	 * @param The color
+	 */
+	void setBg(QColor color);
+
+	/**
+	 * Sets the font for the message
+	 * @param font The font
+	 */
+	void setFont(QFont font);
+
+	/**
+	 * Sets the body of the message
+	 * @param body The body
+	 * @param f The format of the message
+	 */
+	void setBody( const QString& body , MessageFormat f=PlainText );
+	
+	/**
+	 * Get the message body back as plain text
+	 * @return The message body as plain text
+	 */
+	QString plainBody() const ;
+
+	/**
+	 * Get the message body as escaped text
+	 * @return The message body as escaped text
+	 */
+	QString escapedBody() const ;
+	
+	//Parsed (HTML and Emoticon, ready to use in the chatwindow)
+	/**
+	 * Get the message body as parsed HTML with Emoticons,
+	 * this should be ready to show in the chatwindow
+	 * @return The HTML and Emoticon parsed message body
+	 */
+	QString parsedBody() const ;
+		
 protected:
 	// Helper for constructors
 	void init(QDateTime timeStamp, const KopeteContact * from, KopeteContactPtrList to,
 			  QString body, QString subject, MessageDirection direction, MessageFormat f);
 
+	/** Timestamp */
 	QDateTime mTimestamp;
+	/** Contact the message came from */
 	const KopeteContact *mFrom;
+	/** Pointer list of contacts the message is going to */
 	QPtrList<KopeteContact> mTo;
-	QString mBody, mSubject;
+	/** Message body */
+	QString mBody;
+	/** Message subject */
+	QString mSubject;
+	/** Message font */
 	QFont mFont;
-	QColor mFg, mBg;
-
+	/** The foreground color */
+	QColor mFg;
+	/** The background color */
+	QColor mBg;
+	/** The message direction */
 	MessageDirection mDirection;
+	/** The message format */
 	MessageFormat mFormat;
 
 public:
