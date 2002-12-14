5e1ab34a89f97acc7487363ff6c4d419523b35a4
Jason Keirstead <jason@keirstead.org>
2002-12-14 22:24:02 +0000

    Removed KDB reference
    svn path=/trunk/kdenetwork/kopete/; revision=193962

--- kio_kopete/kio_kopete.h
+++ kio_kopete/kio_kopete.h
@@ -1,7 +1,7 @@
 /*
-   This file is part of the KDB libraries
    Copyright (c) 2000 Praduroux Alessandro <pradu@thekompany.com>
- 
+   Modifications (c) 2002 Jason Keirstead <jason@keirstead.org> 
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
