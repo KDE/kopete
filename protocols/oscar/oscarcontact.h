cecdda72786db9ba51d60dd085035c95ef420557
Christopher TenHarmsel <epchris@gmail.com>
2002-12-26 06:32:32 +0000

    Cleaned up code a little
    svn path=/trunk/kdenetwork/kopete/; revision=195822

--- protocols/oscar/oscarcontact.h
+++ protocols/oscar/oscarcontact.h
@@ -35,15 +35,23 @@ class OscarProtocol;
 class OscarContact : public KopeteContact  {
    Q_OBJECT
 public:
-	OscarContact(const QString name, OscarProtocol *protocol,	KopeteMetaContact *parent);
+	OscarContact(const QString name,
+			OscarProtocol *protocol,
+			KopeteMetaContact *parent);
 	~OscarContact();
-	/** Return the protocol specific serialized data that a plugin may want to store a contact list. */
+	/**
+	 * Return the protocol specific serialized data
+	 * that a plugin may want to store a contact list.
+	 */
 	virtual QString data(void) const;
 	/** Returns the online status of the contact */
 	virtual ContactStatus status(void) const;
 	/** Returns the status icon of the contact */
 	virtual QString statusIcon(void) const;
-	/** Returns a set of custom menu items for the context menu */
+	/**
+	 * Returns a set of custom menu items for
+	 * the context menu
+	 */
 	virtual KActionCollection *customContextMenuActions(void);
 	/* Return whether or not this contact is REACHABLE. */
 	virtual bool isReachable(void);
@@ -51,8 +59,6 @@ public:
 public slots:
 	/** Pops up a chat window */
 	virtual void execute(void);
-	/** Show a context menu of actions pertaining to this contact */
-	//virtual void showContextMenu(const QPoint &p, const QString &group);
 	/** Method to delete a contact from the contact list */
 	virtual void slotDeleteContact(void);
 
@@ -69,7 +75,10 @@ private: // Private members
 	/** Initialzes the actions */
 	void initActions(void);
 
-	/* parses HTML AIM-Clients send to us and strips off most of it */
+	/**
+	 * parses HTML AIM-Clients send to us and
+	 * strips off most of it
+	 */
 	KopeteMessage parseAIMHTML ( QString m );
 	/* used by above to stripp off a tag*/
 	QStringList removeTag ( QString &message, QString tag );
