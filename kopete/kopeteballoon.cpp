6b64b4c61495e151a95bb51ac9a36fcfdc2d6548
Simon Perreault <nomis80@nomis80.org>
2003-02-22 19:28:54 +0000

    Added ignore button and made clicking on the bubble view the message.
    svn path=/trunk/kdenetwork/kopete/; revision=208621

--- kopete/kopeteballoon.cpp
+++ kopete/kopeteballoon.cpp
@@ -57,7 +57,10 @@ KopeteBalloon::KopeteBalloon( const QString &text, const QString &pix )
 
 	QPushButton *viewButton = new QPushButton( this, "m_reject" );
 	viewButton->setText( i18n( "View" ) );
+	QPushButton *ignoreButton = new QPushButton( this );
+	ignoreButton->setText( i18n( "Ignore" ) );
 	Layout2->addWidget( viewButton );
+	Layout2->addWidget( ignoreButton );
 	QSpacerItem* spacer_2 = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
 	Layout2->addItem( spacer_2 );
 	BalloonLayout->addLayout( Layout2 );
@@ -72,7 +75,9 @@ KopeteBalloon::KopeteBalloon( const QString &text, const QString &pix )
 	m_caption->setText(text);
 //	m_caption->setAlignment ( m_caption->alignment() | Qt::WordBreak ); // does not work
 	connect( viewButton, SIGNAL( clicked() ), SLOT( hide() ) );
+	connect( ignoreButton, SIGNAL( clicked() ), SLOT( hide() ) );
 	connect( viewButton, SIGNAL( clicked() ), SIGNAL( signalButtonClicked() ) );
+	connect( ignoreButton, SIGNAL( clicked() ), SIGNAL( signalIgnoreButtonClicked() ) );
 }
 
 void KopeteBalloon::setAnchor( const QPoint &anchor)
--- kopete/kopeteballoon.h
+++ kopete/kopeteballoon.h
@@ -43,6 +43,7 @@ public:
 
 signals:
 	void signalButtonClicked();
+	void signalIgnoreButtonClicked();
 	void signalBalloonClicked();
 
 protected:
