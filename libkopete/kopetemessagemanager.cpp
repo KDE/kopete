79b09f7cbcda2ab21cea5d059d4c76c8ff48dea5
Jason Keirstead <jason@keirstead.org>
2003-03-05 05:42:15 +0000

    Make this text a bit more descriptive
    svn path=/trunk/kdenetwork/kopete/; revision=211450

--- libkopete/kopetemessagemanager.cpp
+++ libkopete/kopetemessagemanager.cpp
@@ -209,7 +209,7 @@ void KopeteMessageManager::appendMessage( KopeteMessage &msg )
 		if( KopetePrefs::prefs()->highlightEnabled()  && msg.plainBody().contains( QRegExp( QString::fromLatin1("\\b(%1)\\b").arg( d->mUser->displayName() ) ) ) )
 		{
 			msg.highlight();
-			KNotifyClient::event( QString::fromLatin1( "kopete_highlight"), i18n("A Message For You Arrived") );
+			KNotifyClient::event( QString::fromLatin1( "kopete_highlight"), i18n("A Message Mentioning You Arrived From %1").arg( msg.from()->metaContact()->displayName() ) );
 		}
 	}
 
