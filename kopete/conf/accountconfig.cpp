dd007cb6e874fe64662f479681cfc92f81897632
Jason Keirstead <jason@keirstead.org>
2003-03-06 00:55:43 +0000

    Whoops! This returns continue not yes
    svn path=/trunk/kdenetwork/kopete/; revision=211650

--- kopete/conf/accountconfig.cpp
+++ kopete/conf/accountconfig.cpp
@@ -157,7 +157,7 @@ void AccountConfig::slotRemoveAccount()
 	{
 		KopeteAccount *i=m_accountItems[lvi];
 		if( KMessageBox::warningContinueCancel( this, i18n("Are you sure you want to remove the account \"%1?\"").arg( i->accountId() ),
-			 i18n("Remove Account"), i18n("Remove Account")) == KMessageBox::Yes )
+			 i18n("Remove Account"), i18n("Remove Account")) == KMessageBox::Continue )
 		{
 			m_accountItems.remove(lvi);
 			i->deleteLater();
