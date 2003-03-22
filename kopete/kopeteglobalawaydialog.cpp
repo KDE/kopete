d830903b792dae8236dd8a7721ce6246b1dc2529
Martijn Klingens <klingens@kde.org>
2003-03-22 18:27:37 +0000

    this one is not used by libkopete, move to kopete/
    svn path=/trunk/kdenetwork/kopete/; revision=215643

---
+++ kopete/kopeteglobalawaydialog.cpp
@@ -0,0 +1,50 @@
+/*
+  kopeteglobalawaydialog.cpp  -  Kopete Global Away Dialog
+
+  Copyright (c) 2002 by Christopher TenHarmsel <tenharmsel@users.sourceforge.net>
+  
+  Kopete    (c) 2002 by the Kopete developers  <kopete-devel@kde.org>
+
+  *************************************************************************
+  *                                                                       *
+  * This program is free software; you can redistribute it and/or modify  *
+  * it under the terms of the GNU General Public License as published by  *
+  * the Free Software Foundation; either version 2 of the License, or     *
+  * (at your option) any later version.                                   *
+  *                                                                       *
+  *************************************************************************
+  */
+
+#include "kopeteglobalawaydialog.h"
+#include "kopeteaccountmanager.h"
+#include "kopeteaway.h"
+#include <kdebug.h>
+
+KopeteGlobalAwayDialog::KopeteGlobalAwayDialog(QWidget *parent, const char *name)
+	: KopeteAwayDialog(parent, name)
+{
+	// We don't do anything specific..oh well
+}
+
+void KopeteGlobalAwayDialog::setAway(QString/* type*/)
+{
+	kdDebug(14012) << "[GLOBAL AWAY DIALOG] Setting all protocols away " << endl;
+	// Set the global away message
+	awayInstance->setGlobalAwayMessage(
+		getSelectedAwayMessage());
+
+	// Tell all the protocols to set themselves away.
+	KopeteAccountManager::manager()->setAwayAll();
+}
+
+#include "kopeteglobalawaydialog.moc"
+/*
+ * Local variables:
+ * mode: c++
+ * c-indentation-style: k&r
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ */
+
+// vim: set noet ts=4 sts=4 sw=4:
