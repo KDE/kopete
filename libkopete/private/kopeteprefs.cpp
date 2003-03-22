bef13b784beb324d4cf8ae52d1c11b590ba27001
Martijn Klingens <klingens@kde.org>
2003-03-22 19:29:23 +0000

    one more from libkopete/ to libkopete/private/: kopeteprefs
    svn path=/trunk/kdenetwork/kopete/; revision=215661

---
+++ libkopete/private/kopeteprefs.cpp
@@ -0,0 +1,359 @@
+/*
+    kopeteprefs.cpp - Kopete Preferences Container-Class
+
+    Copyright (c) 2002      by Stefan Gehn            <sgehn@gmx.net>
+
+    Kopete    (c) 2002-2003 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#include "kopeteprefs.h"
+
+#include <kapplication.h>
+#include <klocale.h>
+#include <qcolor.h>
+#include <kconfig.h>
+#include <kglobal.h>
+#include <kdebug.h>
+//#include <kopetechatwindow.h> // FIXME: I do not like this dependency; mETz
+
+KopetePrefs *KopetePrefs::s_prefs = 0L;
+
+KopetePrefs *KopetePrefs::prefs()
+{
+	if( !s_prefs )
+		s_prefs = new KopetePrefs;
+	return s_prefs;
+}
+
+KopetePrefs::KopetePrefs() : QObject( kapp, "KopetePrefs" )
+{
+	config = KGlobal::config();
+
+	load();
+}
+
+KopetePrefs::~KopetePrefs()
+{
+//	save(); // crashes because KopeteApp is already destructed!
+}
+
+void KopetePrefs::load()
+{
+
+//	kdDebug(14010) << "KopetePrefs::load()" << endl;
+	config->setGroup("Appearance");
+
+	mIconTheme		= config->readEntry("EmoticonTheme", defaultTheme());
+	mUseEmoticons		= config->readBoolEntry("Use Emoticons", true);
+	mShowOffline		= config->readBoolEntry("ShowOfflineUsers", true);
+	mGreyIdle		= config->readBoolEntry("GreyIdleMetaContacts", true);
+	mSortByGroup		= config->readBoolEntry("SortByGroup" , true);
+	mTreeView		= config->readBoolEntry("TreeView", true);
+	mStartDocked		= config->readBoolEntry("StartDocked", false);
+	mUseQueue		= config->readBoolEntry("Use Queue", true);
+	mRaiseMsgWindow		= config->readBoolEntry("Raise Msg Window", false);
+	mShowEvents		= config->readBoolEntry("Show Events in Chat Window", true);
+	mTrayflashNotify	= config->readBoolEntry("Trayflash Notification", true);
+	mBalloonNotify		= config->readBoolEntry("Balloon Notification", true);
+	mSoundIfAway		= config->readBoolEntry("Sound Notification If Away", false);
+	mChatWindowPolicy	= config->readNumEntry("Chatwindow Policy", 0);
+	mTransparencyEnabled	= config->readBoolEntry("ChatView Transparency Enabled", false);
+	mTransparencyValue	= config->readNumEntry("ChatView Transparency Value", 50);
+
+	mTransparencyColor	= config->readColorEntry("ChatView Transparency Tint Color", &Qt::white);
+	mChatViewBufferSize	= config->readNumEntry("ChatView BufferSize", 250);
+
+	mHighlightBackground	= config->readColorEntry("Highlight Background Color", &Qt::black);
+	mHighlightForeground	= config->readColorEntry("Highlight Foreground Color", &Qt::yellow);
+	mHighlightEnabled	= config->readBoolEntry("Highlighting Enabled", true);
+	mBgOverride		= config->readBoolEntry("ChatView Override Background", true);
+	mInterfacePreference	= config->readNumEntry("Interface Preference", 1);
+	mTextColor		= config->readColorEntry("Text Color", &Qt::black );
+	mBgColor		= config->readColorEntry("Bg Color", &Qt::white );
+	mFontFace		= config->readFontEntry("Font Face");
+
+	mShowTray		= config->readBoolEntry( "Show Systemtray", true);
+	mKindMessagesHtml    	= config->readEntry("MessagesHTML", QString::null );
+
+	config->setGroup( QString::fromLatin1("Chat Styles") );
+	QString keyBase = QString::fromLatin1("Style");
+	int loopPosition = 0;
+	QString styleNum = keyBase + QString::number(loopPosition);
+	while( config->hasKey( styleNum + QString::fromLatin1(" Name") ) )
+	{
+		QString styleName = config->readEntry( styleNum + QString::fromLatin1(" Name") );
+		QString styleCode = config->readEntry( styleNum + QString::fromLatin1(" Code") );
+		loopPosition++;
+		styleNum = keyBase + QString::number(loopPosition);
+		mChatStyles.insert(styleName, styleCode );
+	}
+
+	if( mChatStyles.isEmpty() )
+	{
+		QString start = QString::fromLatin1( "<table width=\"100%%\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\">" );
+		QString end =   QString::fromLatin1( "\n</td></tr></table>" );
+
+  		QString msnStyle = start + QString::fromLatin1( "\n\t<tr>%e<td colspan=\"2\"><font color=\"#606060\"> " ) + i18n( "%f says:" ) +
+			QString::fromLatin1( "\n\t</font></td></tr><tr><td width=\"15\">&nbsp;</td>%e<td width=\"100%%\" bgcolor=\"%b\">"
+			"\n\t%a<font color='green'>* %f%a %F%M%F %a</font>%a") + end;
+
+		QString xchatStyle = start + QString::fromLatin1( "\n\t<tr><td bgcolor=\"%b\">%a<font color='green'>%a [%T] %s-<font color=\"cyan\">--</font> %F%M%F%s"
+				"\n\t%e<font color=\"blue\">&lt;</font>%i%L%f%L%i%o<font color=\"yellow\">%f</font>%o<font color=\"blue\">&gt;"
+				"\n\t</font>%F%M%F%e%a * %f <font color='green'> %M</font>%a" ) + end;
+
+		QString kopeteStyle = start + QString::fromLatin1( "\n\t %e<tr><td>%i<font color=\"#0360B1\"><b>" ) +
+			i18n( "Message from %f %i %o" ) + QString::fromLatin1( "\n\t<font color=\"#E11919\"><b>" ) +
+			i18n( "Message to %t %o at %T" ) + QString::fromLatin1( "\n\t</b></font></td></tr>%e <tr><td bgcolor=\"%b\">%a<font color='green'>* %f%a %F%M%F%a</font>%a" ) + end;
+
+		mChatStyles.insert( i18n("MSN Style"), msnStyle );
+		mChatStyles.insert( i18n("XChat Style"), xchatStyle );
+		mChatStyles.insert( i18n("Kopete Style"), kopeteStyle );
+
+		if( mKindMessagesHtml.isNull() )
+			mKindMessagesHtml = kopeteStyle;
+		else if( mKindMessagesHtml != xchatStyle && mKindMessagesHtml != msnStyle && mKindMessagesHtml != kopeteStyle )
+			mChatStyles.insert( i18n("Personal Style"), mKindMessagesHtml );
+	}
+
+	config->setGroup("Appearance");
+
+
+	mTransparancyChanged = false;
+}
+
+
+void KopetePrefs::save()
+{
+//	kdDebug(14010) << "KopetePrefs::save()" << endl;
+
+	config->setGroup ( "Appearance");
+
+	config->writeEntry("EmoticonTheme", mIconTheme);
+	config->writeEntry("Use Emoticons", mUseEmoticons);
+	config->writeEntry("ShowOfflineUsers", mShowOffline);
+	config->writeEntry("GreyIdleMetaContacts", mGreyIdle);
+	config->writeEntry("TreeView", mTreeView);
+	config->writeEntry("SortByGroup", mSortByGroup);
+	config->writeEntry("StartDocked", mStartDocked);
+	config->writeEntry("Use Queue", mUseQueue);
+	config->writeEntry("Raise Msg Window", mRaiseMsgWindow);
+	config->writeEntry("Show Events in Chat Window", mShowEvents);
+	config->writeEntry("Trayflash Notification", mTrayflashNotify);
+	config->writeEntry("Balloon Notification", mBalloonNotify);
+	config->writeEntry("Sound Notification If Away", mSoundIfAway);
+	config->writeEntry("Chatwindow Policy", mChatWindowPolicy);
+	config->writeEntry("ChatView Transparency Enabled", mTransparencyEnabled);
+	config->writeEntry("ChatView Transparency Value", mTransparencyValue);
+	config->writeEntry("ChatView Transparency Tint Color", mTransparencyColor);
+	config->writeEntry("ChatView Override Background", mBgOverride);
+	config->writeEntry("ChatView BufferSize", mChatViewBufferSize);
+	config->writeEntry("Highlight Background Color", mHighlightBackground);
+	config->writeEntry("Highlight Foreground Color", mHighlightForeground);
+	config->writeEntry("Highlighting Enabled", mHighlightEnabled );
+	config->writeEntry("Font Face", mFontFace);
+	config->writeEntry("Text Color",mTextColor);
+	config->writeEntry("Bg Color", mBgColor);
+
+	config->writeEntry("Interface Preference", mInterfacePreference);
+
+//	config->writeEntry("ContactList Transparency Enabled", mCTransparencyEnabled);
+//	config->writeEntry("ContactList Transparency Value", mCTransparencyValue);
+//	config->writeEntry("ContactList Transparency Tint Color", mCTransparencyColor);
+
+	config->writeEntry("Show Systemtray", mShowTray);
+	config->writeEntry("MessagesHTML", mKindMessagesHtml);
+
+	config->deleteGroup( QString::fromLatin1("Chat Styles") );
+	config->setGroup( QString::fromLatin1("Chat Styles") );
+	QString keyBase = QString::fromLatin1("Style");
+	int loopPosition = 0;
+	for( KopeteChatStyleMap::Iterator it = mChatStyles.begin(); it != mChatStyles.end(); ++it)
+	{
+		QString scriptNum = keyBase + QString::number( loopPosition );
+		config->writeEntry( scriptNum + QString::fromLatin1(" Name"), it.key() );
+		config->writeEntry( scriptNum + QString::fromLatin1(" Code"), it.data() );
+		loopPosition++;
+	}
+
+	config->sync();
+	emit saved();
+
+	if( mTransparancyChanged )
+		emit( transparancyChanged() );
+
+	mTransparancyChanged = false;
+}
+
+
+void KopetePrefs::setIconTheme(const QString &value)
+{
+	mIconTheme = value;
+}
+
+void KopetePrefs::setUseEmoticons(bool value)
+{
+	mUseEmoticons = value;
+}
+
+void KopetePrefs::setShowOffline(bool value)
+{
+	mShowOffline = value;
+}
+
+void KopetePrefs::setTreeView(bool value)
+{
+	mTreeView = value;
+}
+
+void KopetePrefs::setSortByGroup(bool value)
+{
+	mSortByGroup = value;
+}
+
+void KopetePrefs::setGreyIdleMetaContacts(bool value)
+{
+	mGreyIdle = value;
+}
+
+void KopetePrefs::setStartDocked(bool value)
+{
+	mStartDocked = value;
+}
+
+void KopetePrefs::setUseQueue(bool value)
+{
+	mUseQueue = value;
+}
+
+void KopetePrefs::setRaiseMsgWindow(bool value)
+{
+	mRaiseMsgWindow = value;
+}
+
+void KopetePrefs::setShowEvents(bool value)
+{
+	mShowEvents = value;
+}
+
+void KopetePrefs::setTrayflashNotify(bool value)
+{
+	mTrayflashNotify = value;
+}
+
+void KopetePrefs::setBalloonNotify(bool value)
+{
+	mBalloonNotify = value;
+}
+
+void KopetePrefs::setSoundIfAway(bool value)
+{
+	mSoundIfAway = value;
+}
+
+void KopetePrefs::setKindMessagesHtml(const QString &value)
+{
+	if( mAppearanceChanged || mKindMessagesHtml != value )
+	{
+		mKindMessagesHtml = value;
+		emit( messageAppearanceChanged() );
+	}
+}
+
+void KopetePrefs::setChatStyles( const KopeteChatStyleMap &value )
+{
+	mChatStyles = value;
+}
+
+void KopetePrefs::setFontFace( const QFont &value )
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mFontFace);
+	mFontFace = value;
+}
+
+void KopetePrefs::setTextColor( const QColor &value )
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mTextColor);
+	mTextColor = value;
+}
+
+void KopetePrefs::setBgColor( const QColor &value )
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mBgColor);
+	mBgColor = value;
+}
+
+void KopetePrefs::setChatWindowPolicy(int value)
+{
+	mChatWindowPolicy = value;
+}
+
+void KopetePrefs::setInterfacePreference(int value)
+{
+	mInterfacePreference = value;
+}
+
+void KopetePrefs::setTransparencyEnabled(bool value)
+{
+	mTransparancyChanged =  mTransparancyChanged || !(value == mTransparencyEnabled);
+	mTransparencyEnabled = value;
+}
+
+void KopetePrefs::setTransparencyColor(const QColor &value)
+{
+	mTransparancyChanged =  mTransparancyChanged || !(value == mTransparencyColor);
+	mTransparencyColor = value;
+}
+
+void KopetePrefs::setChatViewBufferSize(const int value)
+{
+	mChatViewBufferSize = value;
+}
+
+void KopetePrefs::setHighlightBackground(const QColor &value)
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mHighlightBackground);
+	mHighlightBackground = value;
+}
+
+void KopetePrefs::setHighlightForeground(const QColor &value)
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mHighlightForeground);
+	mHighlightForeground = value;
+}
+
+void KopetePrefs::setHighlightEnabled(bool value)
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mHighlightEnabled);
+	mHighlightEnabled = value;
+}
+
+void KopetePrefs::setTransparencyValue(int value)
+{
+	mTransparancyChanged = mTransparancyChanged || !(value == mTransparencyValue);
+	mTransparencyValue = value;
+}
+
+void KopetePrefs::setBgOverride(bool value)
+{
+	mAppearanceChanged = mAppearanceChanged || !(value == mBgOverride);
+	mBgOverride = value;
+}
+
+void KopetePrefs::setShowTray(bool value)
+{
+	mShowTray = value;
+}
+
+
+#include "kopeteprefs.moc"
+
+// vim: set noet ts=4 sts=4 sw=4:
---
+++ libkopete/private/kopeteprefs.h
@@ -0,0 +1,175 @@
+/*
+    kopeteprefs.cpp - Kopete Preferences Container-Class
+
+    Copyright (c) 2002      by Stefan Gehn            <sgehn@gmx.net>
+    Kopete    (c) 2002-2003 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#ifndef __KOPETEPREFS_H__
+#define __KOPETEPREFS_H__
+
+#include <qobject.h>
+#include <kdeversion.h>
+#include <qcolor.h>
+#include <qfont.h>
+
+class KConfig;
+
+typedef QMap<QString,QString> KopeteChatStyleMap;
+
+class KopetePrefs : public QObject
+{
+	Q_OBJECT
+
+public:
+	/**
+	 * The prefs container-class is a singleton object. Use this method to retrieve
+	 * the instance.
+	 */
+	static KopetePrefs *prefs();
+
+	~KopetePrefs();
+
+	/**
+	 * Reads all pref-variables from KConfig
+	 * usually you don't need this as KopePrefs loads settings
+	 * when an instance is created
+	 */
+	void load();
+	
+	/**
+	 * Stores all pref-variables into KConfig
+	 */
+	void save();
+
+	QString iconTheme() const { return mIconTheme; }
+	bool useEmoticons() const { return mUseEmoticons; }
+	bool showOffline() const { return mShowOffline; }
+	bool treeView() const { return mTreeView; }
+	bool sortByGroup() const { return mSortByGroup; }
+	bool greyIdleMetaContacts() const { return mGreyIdle; }
+	bool startDocked() const { return mStartDocked; }
+	bool useQueue() const { return mUseQueue; }
+	bool raiseMsgWindow() const{ return mRaiseMsgWindow; }
+	bool showEvents() const{ return mShowEvents; }
+	bool trayflashNotify() const { return mTrayflashNotify; }
+	bool balloonNotify() const { return mBalloonNotify; }
+	bool soundIfAway() const { return mSoundIfAway; }
+	bool transparencyEnabled() const { return mTransparencyEnabled; }
+	int transparencyValue() const { return mTransparencyValue; }
+	QColor transparencyColor() const { return mTransparencyColor; }
+	int chatViewBufferSize() const { return mChatViewBufferSize; }
+	QColor highlightBackground() const { return mHighlightBackground; }
+	QColor highlightForeground() const { return mHighlightForeground; }
+	QColor textColor() const { return mTextColor; }
+	QColor bgColor() const { return mBgColor; }
+	QFont fontFace() const { return mFontFace; }
+	bool highlightEnabled() const { return mHighlightEnabled; }
+	bool bgOverride() const { return mBgOverride; }
+	int interfacePreference() const { return mInterfacePreference; }
+	bool showTray() const { return mShowTray; }
+
+	int chatWindowPolicy() const { return mChatWindowPolicy; }
+	KopeteChatStyleMap chatStyles() const { return mChatStyles; }
+	QString kindMessagesHtml() const { return mKindMessagesHtml; }
+	QString defaultTheme() const { return QString::fromLatin1( "Default" ); }
+
+	void setIconTheme( const QString &value );
+	void setUseEmoticons( bool value );
+	void setShowOffline( bool value );
+	void setTreeView( bool );
+	void setSortByGroup( bool );
+	void setGreyIdleMetaContacts( bool );
+	void setStartDocked( bool );
+	void setUseQueue( bool );
+	void setRaiseMsgWindow( bool );
+	void setShowEvents( bool );
+	void setTrayflashNotify( bool );
+	void setBalloonNotify( bool );
+	void setSoundIfAway( bool );
+	void setBeepNotify( bool );
+	void setChatWindowPolicy( int );
+	void setKindMessagesHtml ( const QString &);
+	void setTransparencyEnabled(bool);
+	void setTransparencyColor(const QColor &);
+	void setChatViewBufferSize(int);
+	void setHighlightBackground(const QColor &);
+	void setHighlightForeground(const QColor &);
+	void setHighlightEnabled(bool);
+	void setTransparencyValue(int);
+	void setBgOverride(bool);
+	void setInterfacePreference(int);
+	void setBgColor( const QColor &);
+	void setTextColor( const QColor &);
+	void setFontFace( const QFont & );
+	void setShowTray(bool);
+	void setChatStyles( const KopeteChatStyleMap &);
+
+signals:
+	/**
+	 * Emitted when config gets saved by save()
+	 */
+	void saved();
+	void messageAppearanceChanged();
+	void transparancyChanged();
+
+private:
+	/**
+	 * Private constructor: we are a singleton
+	 */
+	KopetePrefs();
+
+	/**
+	 * Our instance
+	 */
+	static KopetePrefs *s_prefs;
+
+	KConfig *config;
+
+	QString mIconTheme;
+	bool mUseEmoticons;
+	bool mShowOffline;
+	bool mGreyIdle;
+	bool mTreeView;
+	bool mSortByGroup;
+	bool mStartDocked;
+	bool mUseQueue;
+	bool mRaiseMsgWindow;
+	bool mShowEvents;
+	bool mTrayflashNotify;
+	bool mBalloonNotify;
+	bool mSoundIfAway;
+	bool mTransparencyEnabled;
+	int mTransparencyValue;
+	int mInterfacePreference;
+	QColor mTransparencyColor;
+	int mChatViewBufferSize;
+	QColor mHighlightBackground;
+	QColor mHighlightForeground;
+	QColor mTextColor;
+	QColor mBgColor;
+	QFont mFontFace;
+	bool mHighlightEnabled;
+	bool mBgOverride;
+	bool mShowTray;
+	bool mTransparancyChanged;
+	bool mAppearanceChanged;
+	KopeteChatStyleMap mChatStyles;
+
+	int mChatWindowPolicy;
+	QString mKindMessagesHtml;
+};
+
+#endif
+
+// vim: set noet ts=4 sts=4 sw=4:
+
