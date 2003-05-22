107e349e51da5407b113107df8b5a84df8845418
Will Stephenson <wstephenson@kde.org>
2003-05-22 14:09:56 +0000

    Use new BRB and Invisible icons
    svn path=/trunk/kdenetwork/kopete/; revision=227252

--- protocols/msn/msnprotocol.cpp
+++ protocols/msn/msnprotocol.cpp
@@ -45,7 +45,7 @@ MSNProtocol::MSNProtocol( QObject *parent, const char *name, const QStringList &
 	PHN( KopeteOnlineStatus::Away,    12, this, 5, "msn_phone",     i18n( "Set On The &Phone" ),  i18n( "On the Phone" ) ),
 	LUN( KopeteOnlineStatus::Away,    15, this, 6, "msn_lunch",     i18n( "Set Out To &Lunch" ),  i18n( "Out to Lunch" ) ),
 	FLN( KopeteOnlineStatus::Offline,  0, this, 7, QString::null,   i18n( "Go &Offline" ),        i18n( "Offline" ) ),
-	HDN( KopeteOnlineStatus::Online,   3, this, 8, QString::null,   i18n( "Set &Invisible" ),     i18n( "Invisible" ) ),
+	HDN( KopeteOnlineStatus::Online,   3, this, 8, "msn_invisible",   i18n( "Set &Invisible" ),     i18n( "Invisible" ) ),
 	IDL( KopeteOnlineStatus::Away,    10, this, 9, "msn_away",      "FIXME: Make this unselectable", i18n( "Idle" ) ),
 	UNK( KopeteOnlineStatus::Unknown, 25, this, 0, "status_unknown","FIXME: Make this unselectable", i18n( "Status not available" ) ),
 	CNT( KopeteOnlineStatus::Unknown,  2, this, 10,"msn_connecting","FIXME: Make this unselectable", i18n( "Connecting" ) )
