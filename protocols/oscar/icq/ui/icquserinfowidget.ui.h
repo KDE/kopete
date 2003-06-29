210888e8c060e4b86073fe3a0e0395462325b2f3
Will Stephenson <wstephenson@kde.org>
2003-06-29 20:46:23 +0000

    Added license header
    svn path=/trunk/kdenetwork/kopete/; revision=234601

--- protocols/oscar/icq/ui/icquserinfowidget.ui.h
+++ protocols/oscar/icq/ui/icquserinfowidget.ui.h
@@ -1,11 +1,20 @@
-/****************************************************************************
-** ui.h extension file, included from the uic-generated form implementation.
-**
-** If you wish to add, delete or rename functions or slots use
-** Qt Designer which will update this file, preserving your code. Create an
-** init() function in place of a constructor, and a destroy() function in
-** place of a destructor.
-*****************************************************************************/
+/*
+  icquserinfowidget.ui.h  -  Oscar Protocol Plugin
+
+  Copyright (c) 2003 by Will Stephenson <lists@stevello.free-online.co.uk>
+
+  Kopete    (c) 2003 by the Kopete developers  <kopete-devel@kde.org>
+
+  *************************************************************************
+  *                                                                       *
+  * This program is free software; you can redistribute it and/or modify  *
+  * it under the terms of the GNU General Public License as published by  *
+  * the Free Software Foundation; either version 2 of the License, or     *
+  * (at your option) any later version.                                   *
+  *                                                                       *
+  *************************************************************************
+  */
+
 #include <qstringlist.h>
 
 void ICQUserInfoWidget::init()
