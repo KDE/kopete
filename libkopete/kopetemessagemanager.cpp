c1a57e6fa577059efe5cbe061e4166d82a063637
Ladislav Strojil <ladislav@strojil.cz>
2003-07-14 14:55:13 +0000

    Highlight incoming message only if displayName for current user is not empty
    svn path=/trunk/kdenetwork/kopete/; revision=237028

--- libkopete/kopetemessagemanager.cpp
+++ libkopete/kopetemessagemanager.cpp
@@ -215,7 +215,7 @@ void KopeteMessageManager::appendMessage( KopeteMessage &msg )
 
 	if( msg.direction() == KopeteMessage::Inbound )
 	{
-		if( KopetePrefs::prefs()->highlightEnabled() && msg.plainBody().contains( QRegExp(QString::fromLatin1("\\b(%1)\\b").arg(user()->displayName()),false) ) )
+		if( KopetePrefs::prefs()->highlightEnabled() && !user()->displayName().isEmpty() && msg.plainBody().contains( QRegExp(QString::fromLatin1("\\b(%1)\\b").arg(user()->displayName()),false) ) )
 			msg.setImportance( KopeteMessage::Highlight );
 
 		emit( messageReceived( msg, this ) );
