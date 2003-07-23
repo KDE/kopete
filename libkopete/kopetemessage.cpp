4c70ff764b0f3f1d7bdcd7d1516b47469015b332
Ladislav Strojil <ladislav@strojil.cz>
2003-07-23 20:42:43 +0000

    Fix for the previous "fix" Only add prefix if it is not already there (is there a better way? negative lookbehind?)
    svn path=/trunk/kdenetwork/kopete/; revision=238852

--- libkopete/kopetemessage.cpp
+++ libkopete/kopetemessage.cpp
@@ -479,8 +479,9 @@ QString KopeteMessage::parseLinks( const QString &message ) const
 	QString result = message;
 
 	//Replace http/https/ftp links
-	result.replace( QRegExp( QString::fromLatin1("\\b((http://\\w|ftp://\\w|https://\\w)[-\\w\\._]+[-\\w\\./#&;:=\\?~%_,]*)\\b") ), QString::fromLatin1("<a href=\"\\1\">\\1</a>" ) );
-	result.replace( QRegExp( QString::fromLatin1("\\b((www\\.)[-\\w\\._]+[-\\w\\./#&;:=\\?~%_,]*)\\b") ), QString::fromLatin1("<a href=\"http://\\1\">\\1</a>" ) );
+	result.replace( QRegExp( QString::fromLatin1("\\b((http(s?)://\\w|ftp://\\w)[-\\w\\._]+[-\\w\\./#&;:=\\?~%_,]*)\\b") ), QString::fromLatin1("<a href=\"\\1\">\\1</a>" ) );
+	result.replace( QRegExp( QString::fromLatin1("^(www\\.[-\\w\\._]+[-\\w\\./#&;:=\\?~%_,]*)\\b") ), QString::fromLatin1("<a href=\"http://\\1\">\\1</a>" ) );
+	result.replace( QRegExp( QString::fromLatin1("([^/])\\b(www\\.[-\\w\\._]+[-\\w\\./#&;:=\\?~%_,]*)\\b") ), QString::fromLatin1("\\1<a href=\"http://\\2\">\\2</a>" ) );
 
 
 	//Replace Email Links
