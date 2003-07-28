d287a7eced0e06102edb2e3e66c37bbfedc93b2a
Andy Goossens <andygoossens@gmail.com>
2003-07-28 16:07:00 +0000

    help our translators
    svn path=/trunk/kdenetwork/kopete/; revision=239690

--- kopete/kopeteballoon.cpp
+++ kopete/kopeteballoon.cpp
@@ -62,7 +62,7 @@ KopeteBalloon::KopeteBalloon(const QString &text, const QString &pix)
 
 	// BEGIN Layout2
 	QHBoxLayout *Layout2 = new QHBoxLayout(BalloonLayout, KDialog::spacingHint(), "Layout2");
-	QPushButton *mViewButton = new QPushButton(i18n("View"), this, "mViewButton");
+	QPushButton *mViewButton = new QPushButton(i18n("to view", "View"), this, "mViewButton");
 	QPushButton *mIgnoreButton = new QPushButton(i18n("Ignore" ), this, "mIgnoreButton");
 
 	Layout2->addStretch();
