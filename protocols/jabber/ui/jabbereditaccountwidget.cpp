b16a56f1e6e99883b608618d44fe5188eb21e966
Till Gerken <till@tantalo.net>
2003-08-09 20:10:29 +0000

    Add better JID validation CCMAIL: 62324-done@bugs.kde.org
    svn path=/trunk/kdenetwork/kopete/; revision=241758

--- protocols/jabber/ui/jabbereditaccountwidget.cpp
+++ protocols/jabber/ui/jabbereditaccountwidget.cpp
@@ -217,6 +217,16 @@ void JabberEditAccountWidget::writeConfig ()
 
 bool JabberEditAccountWidget::validateData ()
 {
+
+	if(!mID->text().contains('@'))
+	{
+		KMessageBox::sorry(this, i18n("The Jabber ID you have chosen is invalid. "
+							"Please make sure it is in the form user@jabber.org."),
+							i18n("Invalid Jabber ID"));
+
+		return false;
+	}
+
 	return true;
 }
 
