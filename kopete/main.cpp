54574363e599bea3dfff719d7c09c72acef22420
Matt Rogers <mattr@kde.org>
2003-08-11 05:36:39 +0000

    bump this version too, since kopete 0.7.1 is out now
    svn path=/trunk/kdenetwork/kopete/; revision=242003

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -24,7 +24,7 @@
 #include <dcopclient.h>
 #include "kopeteiface.h"
 
-#define KOPETE_VERSION "0.7.90cvs >= 20030803"
+#define KOPETE_VERSION "0.7.90cvs >= 20030811"
 
 static const char description[] =
 	I18N_NOOP("Kopete, the KDE Instant Messenger");
