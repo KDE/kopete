1830e904d9fc1483c265ce93bcd30919e8c10d00
Duncan Mac-Vicar Prett <duncan@kde.org>
2003-08-25 03:51:33 +0000

    o missing license
    svn path=/trunk/kdenetwork/kopete/; revision=244555

--- kopete/contactlist/kopetelviprops.h
+++ kopete/contactlist/kopetelviprops.h
@@ -1,3 +1,21 @@
+/*
+    kopetelviprops.h
+
+    Kopete Contactlist Properties GUI for Groups and MetaContacts
+
+    Copyright (c) 2002-2003 by Stefan Gehn            <metz AT gehn.net>
+    Kopete    (c) 2002-2003 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
 #ifndef KOPETELVIPROPS_H
 #define KOPETELVIPROPS_H
 
