04d0f89a48e4efe6b653c1c1dec7320d26ed397a
Olivier Goffart <ogoffart@kde.org>
2003-08-26 17:48:54 +0000

    Remove the date in the Kopete version string because: 1) It is useless, when someone uses CVS, we can exept it use a recent version,     particulary when he send a bugreport 2) That's not compatible with all  (for example, i can't send the >= sing to MSN) 3) that will allow to add this version in bugzilla
    CCMAIL: kopete-devel@kde.org
    
    svn path=/trunk/kdenetwork/kopete/; revision=245154

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -24,7 +24,7 @@
 #include <dcopclient.h>
 #include "kopeteiface.h"
 
-#define KOPETE_VERSION "0.7.90cvs >= 20030812"
+#define KOPETE_VERSION "0.7.90"
 
 static const char description[] =
 	I18N_NOOP("Kopete, the KDE Instant Messenger");
