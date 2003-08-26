43c93c36717eba5ce01bba42f03f2e2327838d64
Olivier Goffart <ogoffart@kde.org>
2003-08-26 18:52:37 +0000

    Because not anybody agreed, i reverse my last change. Anyway, i still think the date is not needed. and it is never up to date :-(
    svn path=/trunk/kdenetwork/kopete/; revision=245182

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -24,7 +24,7 @@
 #include <dcopclient.h>
 #include "kopeteiface.h"
 
-#define KOPETE_VERSION "0.7.90"
+#define KOPETE_VERSION "0.7.90>=20030826"
 
 static const char description[] =
 	I18N_NOOP("Kopete, the KDE Instant Messenger");
