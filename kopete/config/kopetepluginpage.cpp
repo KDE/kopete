6b9fb24859d5a65fe94fb67e1cd88c427a45d970
Martijn Klingens <klingens@kde.org>
2003-08-28 23:14:54 +0000

    Add the new plugin selector widget. It's still empty because no plugins are ported, but it's a start.
    And given the amount of lines of code (about 10 :) in this class it's a
    massive improvement over the old version.
    
    This new kdelibs stuff is cool...
    
    svn path=/trunk/kdenetwork/kopete/; revision=246197

---
+++ kopete/config/kopetepluginpage.cpp
@@ -0,0 +1,34 @@
+/*
+    kopetepluginpage.cpp - Kopete Plugin Loader KCM
+
+    Copyright (c) 2003      by Martijn Klingens      <klingens@kde.org>
+
+    Kopete    (c) 2001-2003 by the Kopete developers <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#include "kopetepluginpage.h"
+
+#include <kgenericfactory.h>
+#include <kpluginselector.h>
+
+typedef KGenericFactory<KopetePluginConfig, QWidget> KopetePluginConfigFactory;
+K_EXPORT_COMPONENT_FACTORY( kcm_kopete_pluginconfig, KopetePluginConfigFactory( "kcm_kopete_pluginconfig" ) );
+
+KopetePluginConfig::KopetePluginConfig( QWidget *parent, const char * /* name */, const QStringList &args )
+: KCDPluginPage( KopetePluginConfigFactory::instance(), parent, args )
+{
+    pluginSelector()->addPlugins( KGlobal::instance()->instanceName(), i18n( "Protocols" ), "Protocols" );
+    pluginSelector()->addPlugins( KGlobal::instance()->instanceName(), i18n( "General Plugins" ), "Plugins" );
+}
+
+// vim: set noet ts=4 sts=4 sw=4:
+
---
+++ kopete/config/kopetepluginpage.h
@@ -0,0 +1,39 @@
+/*
+    kopetepluginpage.h - Kopete Plugin Loader KCM
+
+    Copyright (c) 2003      by Martijn Klingens      <klingens@kde.org>
+
+    Kopete    (c) 2001-2003 by the Kopete developers <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#ifndef KOPETEPLUGINPAGE_H
+#define KOPETEPLUGINPAGE_H
+
+#include <kcdpluginpage.h>
+
+/**
+ * Plugin selector. See KCDPluginPage in kdelibs for documentation.
+ *
+ * @author Martijn Klingens <klingens@kde.org>
+ */
+class KopetePluginConfig : public KCDPluginPage
+{
+	Q_OBJECT
+
+public:
+	KopetePluginConfig( QWidget *parent, const char *name, const QStringList &args );
+};
+
+#endif // KOPETEPLUGINPAGE_H
+
+// vim: set noet ts=4 sts=4 sw=4:
+
