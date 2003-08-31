638720ff1ebeea2ed6da576b81dbf4d928e05112
Gav Wood <gav@kde.org>
2003-08-31 09:41:44 +0000

    CCMAIL:Christian Nitschkowski <segfault_ii@web.de> Quick bug fix for stangisms reported by Christian Nitschkowski.
    svn path=/trunk/kdenetwork/kopete/; revision=247133

--- protocols/winpopup/winpopup-install.sh
+++ protocols/winpopup/winpopup-install.sh
@@ -2,7 +2,7 @@
 
 i=`find /etc -name smb.conf`
 echo "[global]" > ~/smb.conf.new
-echo "   message command = $1 %s %f &" >> ~/smb.conf.new
+echo "   message command = $1 %s %m &" >> ~/smb.conf.new
 cat $i | grep -v "message command = " | grep -v "\[global\]" >> ~/smb.conf.new
 mv $i "$i.old"
 mv ~/smb.conf.new $i
--- protocols/winpopup/winpopup-send.sh
+++ protocols/winpopup/winpopup-send.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "$2" > /tmp/.winpopup-new
 echo `date +"%a %l:%m %p"` >> /tmp/.winpopup-new
-cat "$1" >> /tmp/.winpopup-new
+cat "$1" | tr "\000" "\012" >> /tmp/.winpopup-new
 mv -f /tmp/.winpopup-new /tmp/.winpopup
