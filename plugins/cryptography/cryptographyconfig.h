c51840c7a9835329e6c32d2c04a139d4fb419c89
Matt Rogers <mattr@kde.org>
2003-09-05 02:30:53 +0000

    get the license header and copyrights and stuff right
    svn path=/trunk/kdenetwork/kopete/; revision=248446

--- plugins/cryptography/cryptographyconfig.h
+++ plugins/cryptography/cryptographyconfig.h
@@ -1,7 +1,7 @@
 /*
     cryptographyconfig.h
 
-    Copyright (c) 2003      by Roberto Pariset       <victorheremita@fastwebnet.it>
+    Copyright (c) 2003      by Olivier Goffart       <ogoffart@tiscalinet.be>
     Copyright (c) 2003      by Matt Rogers           <matt@matt.rogers.name>
 
     Kopete    (c) 2002-2003 by the Kopete developers <kopete-devel@kde.org>
