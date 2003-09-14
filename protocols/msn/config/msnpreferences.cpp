7340bd1158f37bc214ecf119b4f7087a93faca12
Olivier Goffart <ogoffart@kde.org>
2003-09-14 13:37:16 +0000

    Port MSN to KCAutoModuleConfig
    svn path=/trunk/kdenetwork/kopete/; revision=250989

--- protocols/msn/config/msnpreferences.cpp
+++ protocols/msn/config/msnpreferences.cpp
@@ -1,10 +1,7 @@
 /*
     msnpreferences.cpp - MSN Preferences Widget
 
-    Copyright (c) 2002      by Duncan Mac-Vicar Prett <duncan@kde.org>
-    Copyright (c) 2002-2003 by Martijn Klingens       <klingens@kde.org>
     Copyright (c) 2002-2003 by Olivier Goffart        <ogoffart@tiscalinet.be>
-
     Kopete    (c) 2002-2003 by the Kopete developers  <kopete-devel@kde.org>
 
     *************************************************************************
@@ -17,65 +14,20 @@
     *************************************************************************
 */
 
-#include "msnpreferences.h"
-
-#include <qcheckbox.h>
-#include <qlayout.h>
-
-#include <kconfig.h>
-#include <kautoconfig.h>
-#include <kdebug.h>
 #include <kgenericfactory.h>
-#include <kglobal.h>
-#include <klineedit.h>
-#include <klocale.h>
-#include <knuminput.h>
-#include <kcmodule.h>
-
+#include <kcautoconfigmodule.h>
 #include "msnprefs.h"
 
-typedef KGenericFactory<MSNPreferences> MSNProtocolConfigFactory;
+class MSNPreferences;
 
+typedef KGenericFactory<MSNPreferences> MSNProtocolConfigFactory;
 K_EXPORT_COMPONENT_FACTORY( kcm_kopete_msn, MSNProtocolConfigFactory( "kcm_kopete_msn" ) )
 
-MSNPreferences::MSNPreferences( QWidget *parent, const char * /* name */, const QStringList &args )
-: KCModule( MSNProtocolConfigFactory::instance(), parent, args )
+class MSNPreferences : public KCAutoConfigModule
 {
-	( new QVBoxLayout( this ) )->setAutoAdd( true );
-
-	kautoconfig = new KAutoConfig(KGlobal::config(), this, "kautoconfig");
-	connect(kautoconfig, SIGNAL(widgetModified()), SLOT(slotSettingsDirty()));
-	connect(kautoconfig, SIGNAL(settingsChanged()), SLOT(slotSettingsDirty()));
-    kautoconfig->addWidget( new msnPrefsUI( this ) , "MSN");
-
-	load();
-}
-
-void MSNPreferences::load()
-{
-	kautoconfig->retrieveSettings(true);
-}
-
-void MSNPreferences::save()
-{
-	kautoconfig->saveSettings();
-	setChanged( false );
-}
-
-void MSNPreferences::defaults ()
-{
-	kautoconfig->resetSettings();
-}
-
-
-void MSNPreferences::slotSettingsDirty()
-{
-	// Just mark settings dirty, even if the user undoes his changes,
-	// because KPrefs will handle it in the near future.
-	setChanged( kautoconfig->hasChanged() );
-}
-
-#include "msnpreferences.moc"
-
-// vim: set noet ts=4 sts=4 sw=4:
-
+public:
+	MSNPreferences( QWidget *parent = 0, const char * = 0, const QStringList &args = QStringList() ) : KCAutoConfigModule( MSNProtocolConfigFactory::instance(), parent, args )
+	{
+		setMainWidget( new msnPrefsUI( this ) , "MSN");
+	}
+};
