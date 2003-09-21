45c99cf3bf666ea80aa59fad6a0fdc6af8a8c56e
Andy Goossens <andygoossens@gmail.com>
2003-09-21 23:14:00 +0000

    sync our version somewhat to KDE Alpha2
    don't expect Kopete 0.8.0 with KDE 3.2.0 though,
    because we will release 0.8.0 sooner to find more bugs
    
    svn path=/trunk/kdenetwork/kopete/; revision=252983

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -24,7 +24,7 @@
 #include <dcopclient.h>
 #include "kopeteiface.h"
 
-#define KOPETE_VERSION "0.7.91>=20030912"
+#define KOPETE_VERSION "0.7.92 >= 20030922"
 
 static const char description[] =
 	I18N_NOOP( "Kopete, the KDE Instant Messenger" );
