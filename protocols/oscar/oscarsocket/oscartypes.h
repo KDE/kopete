aceb0d8a601fe2996778d23456bb8fdf3727388b
Stefan Gehn <metz81@web.de>
2003-09-26 20:42:59 +0000

    forgot that new file
    svn path=/trunk/kdenetwork/kopete/; revision=254212

---
+++ protocols/oscar/oscarsocket/oscartypes.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+                          oscartypes.h  -  description
+                             -------------------
+    begin                : Fri Sep 26 2003
+
+    Copyright (c) 2003 by Stefan Gehn <metz AT gehn.net>
+    Kopete    (c) 2003 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#ifndef OSCARTYPES_H
+#define OSCARTYPES_H
+
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned long DWORD;
+
+#endif
