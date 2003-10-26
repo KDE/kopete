3af17a12ce733064ec619e3c2c24a87e661e9cb7
KDE Sysadmin <cvs2svn@kde.org>
2003-10-26 16:27:27 +0000

    This commit was manufactured by cvs2svn to create branch 'make_kopete_cool'.
    svn path=/branches/make_kopete_cool/kdenetwork/kopete/; revision=262199

--- kopete/kconf_update/kopete-account-kconf_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-exec "`kde-config --prefix`/bin/kconf_update_bin/kopete-account-kconf_update"
-
--- kopete/kconf_update/kopete-pluginloader2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-exec "`kde-config --prefix`/bin/kconf_update_bin/kopete-pluginloader2-kconf_update"
-
