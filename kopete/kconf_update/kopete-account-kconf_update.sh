c2ac8b7e91c61e8e3b5e8716a6b9c63cab472e8c
Stephan Kulow <coolo@kde.org>
2004-01-04 14:26:26 +0000

    subdirs of $bindir are a nono. Settle for $(libdir) subdirs (as we do not use $(libexecdir) - that is made for that) CCMAIL: 71420-done@bugs.kde.org
    svn path=/trunk/kdenetwork/kopete/; revision=276680

--- kopete/kconf_update/kopete-account-kconf_update.sh
+++ kopete/kconf_update/kopete-account-kconf_update.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-exec "`kde-config --prefix`/bin/kconf_update_bin/kopete-account-kconf_update"
+IFS=:
+SUFF=kconf_update_bin/kopete-account-kconf_update
+for path in `kde-config --path lib`; do
+  if test -x "$path/$SUFF"; then
+     exec "$path/$SUFF"
+  fi
+done
 
