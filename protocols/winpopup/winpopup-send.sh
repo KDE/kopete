1476324a001d22ef9ccef369f54f7e4f6e9b043f
Gav Wood <gav@kde.org>
2004-01-10 22:55:22 +0000

    A somewhat urgent security bug fix, guarding against smbd running the affected script root.
    svn path=/trunk/kdenetwork/kopete/; revision=278576

--- protocols/winpopup/winpopup-send.sh
+++ protocols/winpopup/winpopup-send.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+PATH=/bin
+
+rm -f /tmp/.winpopup-new
 echo "$2" > /tmp/.winpopup-new
 echo `date --iso-8601=seconds` >> /tmp/.winpopup-new
 cat "$1" | tr "\000" "\012" >> /tmp/.winpopup-new
+chown nobody /tmp/.winpopup-new
+chmod 644 /tmp/.winpopup-new
 mv -f /tmp/.winpopup-new /tmp/.winpopup
