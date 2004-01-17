1efd83a92ca4936ddb2f5e3363cafce01507bbe2
Martijn Klingens <klingens@kde.org>
2004-01-17 19:44:42 +0000

    Bump version.
    I'll be working in the branch for now, could someone please forward port
    to HEAD?
    
    svn path=/branches/KDE_3_2_BRANCH/kdenetwork/kopete/; revision=280563

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -24,7 +24,7 @@
 #include <dcopclient.h>
 #include "kopeteiface.h"
 
-#define KOPETE_VERSION "0.7.94"
+#define KOPETE_VERSION "0.8.0 RC1"
 
 static const char description[] =
 	I18N_NOOP( "Kopete, the KDE Instant Messenger" );
