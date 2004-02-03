12a751e54e60440a11cba52e2485347ac04e1b46
Richard Smith <kde@metafoo.co.uk>
2004-02-03 16:22:56 +0000

    Added KopetePasswordWidget - widget to enter password into Edit Account dialogs
    svn path=/branches/lilac-accounts/kdenetwork/kopete/; revision=284802

---
+++ libkopete/ui/kopetepasswordwidget.cpp
@@ -0,0 +1,88 @@
+/*
+    kopetepasswordwidget.cpp - widget for modifying a KopetePassword
+
+    Copyright (c) 2003 by Richard Smith          <kde@metafoo.co.uk>
+
+    Kopete    (c) 2003 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU Lesser General Public            *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#include "kopetepasswordwidget.h"
+#include "kopetepassword.h"
+
+#include <kpassdlg.h>
+
+#include <qcheckbox.h>
+
+struct KopetePasswordWidget::KopetePasswordWidgetPrivate
+{
+
+};
+
+KopetePasswordWidget::KopetePasswordWidget( QWidget *parent, KopetePassword *from, const char *name )
+ : KopetePasswordWidgetBase( parent, name ), d( new KopetePasswordWidgetPrivate )
+{
+	load( from );
+}
+
+KopetePasswordWidget::~KopetePasswordWidget()
+{
+	delete d;
+}
+
+void KopetePasswordWidget::load( KopetePassword *source )
+{
+	if ( source && source->remembered() )
+	{
+		mRemembered->setTristate();
+		mRemembered->setNoChange();
+		source->requestWithoutPrompt( this, SLOT( receivePassword( const QString & ) ) );
+	}
+	else
+	{
+		mRemembered->setTristate( false );
+		mRemembered->setChecked( false );
+	}
+}
+
+void KopetePasswordWidget::receivePassword( const QString &pwd )
+{
+	// pwd == null could mean user declined to open wallet
+	// don't uncheck the remembered field in this case.
+	if ( !pwd.isNull() )
+	{
+		mRemembered->setTristate( false );
+		mRemembered->setChecked( true );
+		mPassword->setText( pwd );
+	}
+}
+
+void KopetePasswordWidget::save( KopetePassword *target )
+{
+	if ( !target || mRemembered->state() == QButton::NoChange )
+		return;
+	
+	if ( mRemembered->isChecked() )
+		target->set( QString::fromLocal8Bit( mPassword->password() ) );
+	else
+		target->set();
+}
+
+bool KopetePasswordWidget::validate( int maxLength )
+{
+	if ( !mRemembered->isChecked() ) return true;
+	if ( maxLength <= 0 ) return true;
+	return QString::fromLocal8Bit( mPassword->password() ).length() < maxLength;
+}
+
+#include "kopetepasswordwidget.moc"
+
+// vim: set noet ts=4 sts=4 sw=4:
---
+++ libkopete/ui/kopetepasswordwidget.h
@@ -0,0 +1,53 @@
+/*
+    kopetepasswordwidget.cpp - widget for editing a KopetePassword
+
+    Copyright (c) 2003 by Richard Smith          <kde@metafoo.co.uk>
+
+    Kopete    (c) 2003 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU Lesser General Public            *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#ifndef KOPETEPASSWORDWIDGET_H
+#define KOPETEPASSWORDWIDGET_H
+
+#include "kopetepasswordwidgetbase.h"
+
+class KopetePassword;
+
+/**
+ * @author Richard Smith <kde@metafoo.co.uk>
+ */
+class KopetePasswordWidget : public KopetePasswordWidgetBase
+{
+	Q_OBJECT
+
+public:
+	KopetePasswordWidget( QWidget *parent, KopetePassword *from = 0, const char *name = 0 );
+	~KopetePasswordWidget();
+
+	void load( KopetePassword *source );
+	void save( KopetePassword *target );
+
+	bool validate( int maxLength );
+
+public slots:
+	/** @internal */
+	void receivePassword( const QString & );
+
+private:
+	struct KopetePasswordWidgetPrivate;
+	KopetePasswordWidgetPrivate *d;
+};
+
+#endif
+
+// vim: set noet ts=4 sts=4 sw=4:
+
