b9fabeec2c133db5adb2d9047090b927d1ab629a
Martijn Klingens <klingens@kde.org>
2004-02-07 18:07:10 +0000

    Connecting is, well, connecting, and not unknown :)
    svn path=/trunk/kdenetwork/kopete/; revision=285896

--- protocols/msn/msnprotocol.cpp
+++ protocols/msn/msnprotocol.cpp
@@ -39,17 +39,17 @@ MSNProtocol *MSNProtocol::s_protocol = 0L;
 MSNProtocol::MSNProtocol( QObject *parent, const char *name, const QStringList & /* args */ )
 : KopeteProtocol( MSNProtocolFactory::instance(), parent, name ),
 
-	NLN( KopeteOnlineStatus::Online,  25, this, 1, QString::null,   i18n( "Set O&nline" ),        i18n( "Online" ) ),
-	BSY( KopeteOnlineStatus::Away,    20, this, 2, "msn_busy",      i18n( "Set &Busy" ),          i18n( "Busy" ) ),
-	BRB( KopeteOnlineStatus::Away,    22, this, 3, "msn_brb",       i18n( "Set Be &Right Back" ), i18n( "Be Right Back" ) ),
-	AWY( KopeteOnlineStatus::Away,    18, this, 4, "msn_away",      i18n( "Set &Away" ),          i18n( "Away From Computer" ) ),
-	PHN( KopeteOnlineStatus::Away,    12, this, 5, "msn_phone",     i18n( "Set On The &Phone" ),  i18n( "On the Phone" ) ),
-	LUN( KopeteOnlineStatus::Away,    15, this, 6, "msn_lunch",     i18n( "Set Out To &Lunch" ),  i18n( "Out to Lunch" ) ),
-	FLN( KopeteOnlineStatus::Offline,  0, this, 7, QString::null,   i18n( "Set &Offline" ),       i18n( "Offline" ) ),
-	HDN( KopeteOnlineStatus::Away,     3, this, 8, "msn_invisible", i18n( "Set &Invisible" ),     i18n( "Invisible" ) ), //We use away because we don't want to see this state changed when autoaway.
-	IDL( KopeteOnlineStatus::Away,    10, this, 9, "msn_away",      "FIXME: Make this unselectable", i18n( "Idle" ) ),
-	UNK( KopeteOnlineStatus::Unknown, 25, this, 0, "status_unknown","FIXME: Make this unselectable", i18n( "Status not available" ) ),
-	CNT( KopeteOnlineStatus::Unknown,  2, this, 10,"msn_connecting","FIXME: Make this unselectable", i18n( "Connecting" ) )
+	NLN( KopeteOnlineStatus::Online,    25, this, 1, QString::null,   i18n( "Set O&nline" ),        i18n( "Online" ) ),
+	BSY( KopeteOnlineStatus::Away,      20, this, 2, "msn_busy",      i18n( "Set &Busy" ),          i18n( "Busy" ) ),
+	BRB( KopeteOnlineStatus::Away,      22, this, 3, "msn_brb",       i18n( "Set Be &Right Back" ), i18n( "Be Right Back" ) ),
+	AWY( KopeteOnlineStatus::Away,      18, this, 4, "msn_away",      i18n( "Set &Away" ),          i18n( "Away From Computer" ) ),
+	PHN( KopeteOnlineStatus::Away,      12, this, 5, "msn_phone",     i18n( "Set On The &Phone" ),  i18n( "On the Phone" ) ),
+	LUN( KopeteOnlineStatus::Away,      15, this, 6, "msn_lunch",     i18n( "Set Out To &Lunch" ),  i18n( "Out to Lunch" ) ),
+	FLN( KopeteOnlineStatus::Offline,    0, this, 7, QString::null,   i18n( "Set &Offline" ),       i18n( "Offline" ) ),
+	HDN( KopeteOnlineStatus::Away,       3, this, 8, "msn_invisible", i18n( "Set &Invisible" ),     i18n( "Invisible" ) ), //We use away because we don't want to see this state changed when autoaway.
+	IDL( KopeteOnlineStatus::Away,      10, this, 9, "msn_away",      "FIXME: Make this unselectable", i18n( "Idle" ) ),
+	UNK( KopeteOnlineStatus::Unknown,   25, this, 0, "status_unknown","FIXME: Make this unselectable", i18n( "Status not available" ) ),
+	CNT( KopeteOnlineStatus::Connecting, 2, this, 10,"msn_connecting","FIXME: Make this unselectable", i18n( "Connecting" ) )
 {
 	s_protocol = this;
 
