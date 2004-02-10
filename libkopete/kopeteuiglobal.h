2f2a72a19f8cca723bdb5a66923ca250bdce5bd5
Richard Smith <kde@metafoo.co.uk>
2004-02-10 15:10:18 +0000

    Added kopeteuiglobals.{cpp,h} for, well, UI globals.
    svn path=/trunk/kdenetwork/kopete/; revision=286806

---
+++ libkopete/kopeteuiglobal.h
@@ -0,0 +1,52 @@
+/*
+    kopeteuiglobal.h - Kopete UI Globals
+
+    Copyright (c) 2004      by Richard Smith         <kde@metafoo.co.uk>
+
+    Kopete    (c) 2004      by the Kopete developers <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU Lesser General Public            *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#ifndef KOPETEUIGLOBAL_H
+#define KOPETEUIGLOBAL_H
+
+class QWidget;
+
+namespace Kopete
+{
+
+namespace UI
+{
+
+/**
+ * This namespace contains the Kopete user interface's global settings
+ */
+namespace Global
+{
+	/**
+	 * Set the main widget to widget
+	 */
+	void setMainWidget( QWidget *widget );
+	/**
+	 * Returns the main widget - this is the widget that message boxes
+	 * and KNotify stuff should use as a parent.
+	 */
+	QWidget *mainWidget();
+} //Global::UI
+
+} //UI
+
+}
+
+#endif
+
+// vim: set noet ts=4 sts=4 sw=4:
+
