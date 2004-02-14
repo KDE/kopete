05c6a5c770a1b900cb421373e9263d0f9e3e3f33
Matt Rogers <mattr@kde.org>
2004-02-14 23:08:06 +0000

    Apply Martijn's patch which fixes my backport for 74562
    svn path=/branches/KDE_3_2_BRANCH/kdenetwork/kopete/; revision=288080

--- kopete/chatwindow/chatview.cpp
+++ kopete/chatwindow/chatview.cpp
@@ -785,37 +785,31 @@ void ChatView::slotContactStatusChanged( KopeteContact *contact, const KopeteOnl
 {
 	if ( contact && KopetePrefs::prefs()->showEvents() )
 	{
+		// If we just went offline ourself, disable the send button
 		if ( contact->account() && contact == contact->account()->myself() )
-		{
-			//Seperate notification for the 'self' contact
-			if ( newStatus.status() != KopeteOnlineStatus::Connecting )
-			{
-				if ( newStatus.status() == KopeteOnlineStatus::Offline )
-					mainWindow()->setSendEnabled( false );
-				else
-					mainWindow()->setSendEnabled( true );
+			mainWindow()->setSendEnabled( newStatus.status() != KopeteOnlineStatus::Offline );
 
-				sendInternalMessage( i18n( "%2 has changed their status to %1." )
+		if ( contact->metaContact() )
+		{
+			sendInternalMessage( i18n( "%2 has changed their status to %1." )
 #if QT_VERSION < 0x030200
 				.arg(contact->onlineStatus().description() ).arg( contact->metaContact()->displayName() )
 #else
 				.arg(contact->onlineStatus().description(), contact->metaContact()->displayName() )
 #endif
-				);
-			}
+			);
 		}
-	}
-	else
-	{
-		sendInternalMessage( i18n( "%2 has changed their status to %1." )
+		else
+		{
+			sendInternalMessage( i18n( "%2 has changed their status to %1." )
 #if QT_VERSION < 0x030200
-			.arg( contact->onlineStatus().description() ).arg( contact->displayName() )
+				.arg( contact->onlineStatus().description() ).arg( contact->displayName() )
 #else
-			.arg( contact->onlineStatus().description(), contact->displayName() )
+				.arg( contact->onlineStatus().description(), contact->displayName() )
 #endif
 			);
+		}
 	}
-
 
 	if(m_tabBar)
 	{
