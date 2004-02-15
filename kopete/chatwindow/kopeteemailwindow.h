618795b2471fada774edcfed442c5929038bb2ef
Richard Smith <kde@metafoo.co.uk>
2004-02-15 16:12:45 +0000

    Make enter and return work as keyboard shortcuts in the single-shot message window. This is a hack, but I couldn't find a better way to divert the keypresses from the text input widget, so I stole this idea from the chat window. [BACKPORT]
    svn path=/branches/KDE_3_2_BRANCH/kdenetwork/kopete/; revision=288209

--- kopete/chatwindow/kopeteemailwindow.h
+++ kopete/chatwindow/kopeteemailwindow.h
@@ -78,6 +78,7 @@ signals:
 protected:
 	virtual bool queryExit();
 	virtual void windowActivationChange( bool activated );
+	virtual bool KopeteEmailWindow::eventFilter( QObject *o, QEvent *e );
 
 private slots:
 	void slotReplySendClicked();
