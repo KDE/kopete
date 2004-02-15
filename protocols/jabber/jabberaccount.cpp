b5d6da56e86d6c5914a66219729b532b33d876ed
Matt Rogers <mattr@kde.org>
2004-02-15 20:09:51 +0000

    backport of the fix for the constantly blinking jabber icon beastie
    svn path=/branches/KDE_3_2_BRANCH/kdenetwork/kopete/; revision=288287

--- protocols/jabber/jabberaccount.cpp
+++ protocols/jabber/jabberaccount.cpp
@@ -350,6 +350,7 @@ void JabberAccount::slotConnected (bool success, int statusCode, const QString &
 		kdDebug (JABBER_DEBUG_GLOBAL) << "[JabberAccount] Connection failed! Status: " << statusCode << ", " << statusString << endl;
 
 		KMessageBox::queuedMessageBox(qApp->mainWidget (), KMessageBox::Error, i18n ("Connection failed with reason \"%1\".").arg (statusString, 1), i18n ("Connection Failed"));
+		disconnect();
 	}
 
 }
