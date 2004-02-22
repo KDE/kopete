532ea667dad8e96f03e94258e5814141e37e3e6e
KDE Sysadmin <cvs2svn@kde.org>
2004-02-22 21:07:15 +0000

    This commit was manufactured by cvs2svn to create branch 'iris-port'.
    svn path=/branches/iris-port/kdenetwork/kopete/; revision=290360

--- protocols/irc/libkirc/kircentity.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "kircentity.h"
-
-const QRegExp userRegexp(QString::fromLatin1("(.*)(?:!(.*))(?:@(.*))"));
-const QRegExp channelRegexp( QString::fromLatin1("^[#!+&][^\\s,:]+$") );
-
-#include "kircentity.moc"
