0afdc0940729f6f28f9e6b4c849e35c8709231d6
Ralf Hoelzer <ralf@well.com>
2004-02-28 00:19:34 +0000

    Make it compile. I hope its supposed to be && and not ||.
    CCMAIL: ogoffart@tiscalinet.be
    
    svn path=/trunk/kdenetwork/kopete/; revision=292083

--- protocols/msn/msnaccount.cpp
+++ protocols/msn/msnaccount.cpp
@@ -637,7 +637,7 @@ void MSNAccount::slotKopeteGroupRenamed( KopeteGroup *g )
 	if ( g->type() == KopeteGroup::Normal )
 	{
 		if ( !g->pluginData( protocol(), accountId() + " id" ).isEmpty() &&
-			g->displayName() != g->pluginData( protocol(), accountId() + " displayName" ) ||  && 
+			g->displayName() != g->pluginData( protocol(), accountId() + " displayName" ) && 
 			m_groupList.contains( g->pluginData( protocol(), accountId() + " id" ).toUInt() ) )
 		{
 			notifySocket()->renameGroup( g->displayName(), g->pluginData( protocol(), accountId() + " id" ).toUInt() );
