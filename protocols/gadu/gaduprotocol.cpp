54b63b5dab0cfd6daffe39cd74702ebb0a69e14e
Uwe Thiem <uwe@kde.org>
2004-05-25 08:25:45 +0000

    unnecessary and wrong include
    Uwe
    
    svn path=/trunk/kdenetwork/kopete/; revision=314373

--- protocols/gadu/gaduprotocol.cpp
+++ protocols/gadu/gaduprotocol.cpp
@@ -20,7 +20,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 // 02111-1307, USA.
 
-#include <libgadu.h>
+//#include <libgadu.h>
 
 #include <kdebug.h>
 #include <kgenericfactory.h>
