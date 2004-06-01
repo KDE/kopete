9be6d2e3da6f97e72c3274c45924519d20426818
Will Stephenson <wstephenson@kde.org>
2004-06-01 23:57:40 +0000

    Don't block the UI on these errors.
    svn path=/trunk/kdenetwork/kopete/; revision=316908

--- kopete/contactlist/kopetecontactlistview.cpp
+++ kopete/contactlist/kopetecontactlistview.cpp
@@ -1566,13 +1566,13 @@ void KopeteContactListView::slotSendFile()
 			if ( !emailAddr.isEmpty() )
 				kapp->invokeMailer( emailAddr, QString::null );
 			else
-				KMessageBox::sorry( this, i18n( "There is no email address set for this contact in the KDE Address Book" ), i18n( "No Email Address in Address Book" ) );
+				KMessageBox::queuedMessageBox( this, KMessageBox::Sorry, i18n( "There is no email address set for this contact in the KDE Address Book" ), i18n( "No Email Address in Address Book" ) );
 		}
 		else
-			KMessageBox::sorry( this, i18n( "This contact was not found in the KDE Address Book.  Check that a contact is selected in the Properties dialog." ), i18n( "Not Found in Address Book" ) );
+			KMessageBox::queuedMessageBox( this, KMessageBox::Sorry, i18n( "This contact was not found in the KDE Address Book.  Check that a contact is selected in the Properties dialog." ), i18n( "Not Found in Address Book" ) );
 	}
 	else
-		KMessageBox::sorry( this, i18n( "This contact is not associated with a KDE Address Book entry, where the email address is stored.  Check that a contact is selected in the Properties dialog." ), i18n( "Not Found in Address Book" ) );
+		KMessageBox::queuedMessageBox( this, KMessageBox::Sorry, i18n( "This contact is not associated with a KDE Address Book entry, where the email address is stored.  Check that a contact is selected in the Properties dialog." ), i18n( "Not Found in Address Book" ) );
 }
 
 void KopeteContactListView::slotMoveToGroup()
