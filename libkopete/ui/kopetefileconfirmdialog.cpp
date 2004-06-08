e688034129f2becc00bf1b02b239fd98add487cc
Matt Rogers <mattr@kde.org>
2004-06-08 14:42:17 +0000

    Fix the order of creation for the File Transfer Confirmation box. User2 is created before User1
    CCMAIL: 82336-done@bugs.kde.org
    
    svn path=/trunk/kdenetwork/kopete/; revision=318765

--- libkopete/ui/kopetefileconfirmdialog.cpp
+++ libkopete/ui/kopetefileconfirmdialog.cpp
@@ -30,7 +30,8 @@
 
 KopeteFileConfirmDialog::KopeteFileConfirmDialog(const KopeteFileTransferInfo &info,const QString& description,QWidget *parent, const char *name )
 : KDialogBase( parent, name, true, i18n( "A User Would Like to Send You a File" ),
-	KDialogBase::User1 | KDialogBase::User2, KDialogBase::User1, true, i18n( "&Accept" ), i18n( "&Refuse" ) ), m_info( info )
+	KDialogBase::User1 | KDialogBase::User2, KDialogBase::User1, true, i18n( "&Refuse" ), i18n( "&Accept" ) ),
+	m_info( info )
 {
 	setWFlags( WDestructiveClose );
 	m_emited=false;
