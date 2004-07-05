aabd53a8cf97fa3912955f33cf0aea6757f65b04
Till Gerken <till@tantalo.net>
2004-07-05 19:16:49 +0000

    Make sure the plugin's disconnect() method is called
    svn path=/trunk/kdenetwork/kopete/; revision=326093

--- libkopete/kopeteaccount.cpp
+++ libkopete/kopeteaccount.cpp
@@ -103,6 +103,11 @@ void KopeteAccount::connect( const KopeteOnlineStatus& )
 
 void KopeteAccount::disconnect( DisconnectReason reason )
 {
+
+	//since plugins call this method directly, we need
+	//to call their disconnect method first
+	disconnect();
+
 	//reconnect if needed
 	if ( KopetePrefs::prefs()->reconnectOnDisconnect() == true && reason != Manual )
 		connect();
