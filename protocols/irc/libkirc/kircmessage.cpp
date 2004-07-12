659010b267df892dac3abdbb32e0ab0344e66d4e
Michel Hermier <michel.hermier@gmail.com>
2004-07-12 21:17:59 +0000

    Workaround for a KStringHandler::isUtf8(const char *) bug(KDE-3.2.x).
    svn path=/trunk/kdenetwork/kopete/; revision=328470

--- protocols/irc/libkirc/kircmessage.cpp
+++ protocols/irc/libkirc/kircmessage.cpp
@@ -192,6 +192,12 @@ QString KIRCMessage::quote(const QString &str)
 // FIXME: The unquote system is buggy.
 QString KIRCMessage::unquote(const QString &str)
 {
+#if !KDE_IS_VERSION( 3, 1, 90 )
+	// Workaround a KStringHandler bug.
+	if (str.isEmpty())
+		return QString::fromLatin1("");
+#endif
+
 	QString tmp = str;
 
 	char b[3];
