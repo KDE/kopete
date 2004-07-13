ad5167d3b592a84c50dbde36b9b053d98e31fc0a
KDE Sysadmin <cvs2svn@kde.org>
2004-07-13 08:33:17 +0000

    This commit was manufactured by cvs2svn to create branch 'groupwise_in_anger'.
    svn path=/branches/groupwise_in_anger/kdenetwork/kopete/; revision=328584

--- kopete/kconf_update/kopete-jabberproxytype-kconf_update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sed "s/PluginData_JabberProtocol_ProxyType=SOCKS4/PluginData_JabberProtocol_ProxyType=SOCKS/" | sed "s/PluginData_JabberProtocol_ProxyType=SOCKS5/PluginData_JabberProtocol_ProxyType=SOCKS/"
