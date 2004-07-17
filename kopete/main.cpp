77749922ee82a658b3b315ade3564796c9136036
Stephan Kulow <coolo@kde.org>
2004-07-17 17:10:19 +0000

    new version
    svn path=/trunk/kdenetwork/kopete/; revision=330345

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -25,7 +25,7 @@
 #include "kopeteiface.h"
 #include "kimifaceimpl.h"
 
-#define KOPETE_VERSION "0.8.91 (CVS >= 20040626)"
+#define KOPETE_VERSION "0.8.922 (CVS >= 20040717)"
 
 static const char description[] =
 	I18N_NOOP( "Kopete, the KDE Instant Messenger" );
