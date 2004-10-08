0da2f3597b5de050b83b397928e416b495a56cd2
Matt Rogers <mattr@kde.org>
2004-10-08 13:03:16 +0000

    M-x tabify
    svn path=/trunk/kdenetwork/kopete/; revision=352899

--- libkopete/kopeteblacklister.h
+++ libkopete/kopeteblacklister.h
@@ -1,15 +1,15 @@
 /*
     kopeteblacklister.h - Kopete BlackLister
 
-    Copyright (c) 2004      by Roie Kerstein         <sf_kersteinroie@bezeqint.net>
-    
+    Copyright (c) 2004	    by Roie Kerstein	     <sf_kersteinroie@bezeqint.net>
+
     *************************************************************************
-    *                                                                       *
-    * This library is free software; you can redistribute it and/or         *
-    * modify it under the terms of the GNU Lesser General Public            *
-    * License as published by the Free Software Foundation; either          *
-    * version 2 of the License, or (at your option) any later version.      *
-    *                                                                       *
+    *									    *
+    * This library is free software; you can redistribute it and/or	    *
+    * modify it under the terms of the GNU Lesser General Public	    *
+    * License as published by the Free Software Foundation; either	    *
+    * version 2 of the License, or (at your option) any later version.	    *
+    *									    *
     *************************************************************************
 */
 #ifndef KOPETEKOPETEBLACKLISTER_H
@@ -54,59 +54,59 @@ public:
     BlackLister(QString protocolId, QString accountId, QObject *parent = 0, const char *name = 0);
 
     ~BlackLister();
-    
+
     /**
      * \return whether contact is blocked.
      */
     bool isBlocked(Contact *contact);
-    
+
     /**
      * \return whether contactId is blocked.
      */
     bool isBlocked(QString &contactId);
-       
+
 public slots:
-    
+
     /**
      * Add a contact to the blacklist. 
      * This function emits the @ref contactAdded() signal.
      * @param contactId is the ID of the contact to be added to the list.
      */
     void slotAddContact(QString &contactId);
-    
+
     /**
      * Overloaded function provided for convinience. Behaves exactly like the above.
      */
     void slotAddContact(Contact *contact);
-    
+
     /**
      * Remove a contact from the blacklist.
      * This function emits the @ref contactRemoved() signal.
      * @param contact is the contact to be removed from the list.
      */
     void slotRemoveContact(Contact *contact);
-    
+
     /**
      * Overloaded function provided for convinience. Behaves exactly like the above.
      */
     void slotRemoveContact(QString &contactId);
-    
+
 signals:
-    
+
     /**
      * \brief A new contact has been added to the list
      * Connect to this signal if you want to perform additional actions,
      * and you prefer not to derieve from this class.
      */
     void contactAdded(QString &contactId);
-    
+
     /**
      * \brief A contact has been removed from the list
      * Connect to this signal if you want to perform additional actions,
      * and you prefer not to derieve from this class.
      */
     void contactRemoved(QString &contactId);
-    
+
 private:
     QStringList m_blacklist;
     QString m_owner;
