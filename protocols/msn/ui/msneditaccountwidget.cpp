88597a5102c22b7be96708d6acab8fbf7a9fa5da
Olivier Goffart <ogoffart@kde.org>
2004-10-24 12:51:35 +0000

    *prototype* for the "exclude from connect all" label.
    svn path=/trunk/kdenetwork/kopete/; revision=357303

--- protocols/msn/ui/msneditaccountwidget.cpp
+++ protocols/msn/ui/msneditaccountwidget.cpp
@@ -86,7 +86,7 @@ MSNEditAccountWidget::MSNEditAccountWidget( MSNProtocol *proto, Kopete::Account 
 
 		//remove me after we can change account ids (Matt)
 		d->ui->m_login->setDisabled( true );
-		d->ui->m_autologin->setChecked( ( account && account->autoConnect() ) );
+		d->ui->m_autologin->setChecked( !account->autoConnect()  );
 		if ( ( static_cast<MSNAccount*>(account)->serverName() != "messenger.hotmail.com" ) || ( static_cast<MSNAccount*>(account)->serverPort() != 1863) ) {
 			d->ui->optionOverrideServer->setChecked( true );
 		}
@@ -157,7 +157,7 @@ Kopete::Account * MSNEditAccountWidget::apply()
 	
 	KConfigGroup *config=account()->configGroup();
 
-	account()->setAutoConnect( d->ui->m_autologin->isChecked() );
+	account()->setAutoConnect( !d->ui->m_autologin->isChecked() );
 	d->ui->m_password->save( &static_cast<MSNAccount *>(account())->password() );
 
 	config->writeEntry( "exportCustomPicture", d->ui->m_useDisplayPicture->isChecked() );
