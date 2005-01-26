6923423ad6fbca4d24ee339132c5ce2bbd72dab4
Matt Rogers <mattr@kde.org>
2005-01-26 03:11:41 +0000

    We support UTF-8
    svn path=/trunk/kdenetwork/kopete/; revision=382529

--- protocols/oscar/liboscar/profiletask.cpp
+++ protocols/oscar/liboscar/profiletask.cpp
@@ -93,7 +93,7 @@ void ProfileTask::sendProfileUpdate()
 	if ( client()->isIcq() )
 	{
 		//capBuf.addString( oscar_caps[CAP_ICQSERVERRELAY], 16 ); // we support type-2 messages
-		//capBuf.addString( oscar_caps[CAP_UTF8], 16 ); // we can send/receive UTF encoded messages
+		capBuf.addString( oscar_caps[CAP_UTF8], 16 ); // we can send/receive UTF encoded messages
 		capBuf.addString( oscar_caps[CAP_ISICQ], 16 ); // I think this is an icq client, but maybe I'm wrong
 		capBuf.addString( oscar_caps[CAP_KOPETE], 16 ); // we are the borg, resistance is futile
 		//capBuf.addString( oscar_caps[CAP_RTFMSGS], 16 ); // we do incoming RTF messages
@@ -101,6 +101,7 @@ void ProfileTask::sendProfileUpdate()
 	}
 	else
 	{
+		capBuf.addString( oscar_caps[CAP_UTF8], 16 ); //we can send/receive UTF encoded messages
 		capBuf.addString( oscar_caps[CAP_KOPETE], 16 ); // we are the borg, resistance is futile
 		capBuf.addString( oscar_caps[CAP_TYPING], 16 );
 	}
