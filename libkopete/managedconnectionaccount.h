0848bdff2f3894f619f38d415955e27b7e85a381
Will Stephenson <wstephenson@kde.org>
2005-02-02 20:52:39 +0000

    Make slotConnectionStatusChanged virtual, and add api docs
    svn path=/trunk/kdenetwork/kopete/; revision=385521

--- libkopete/managedconnectionaccount.h
+++ libkopete/managedconnectionaccount.h
@@ -25,18 +25,50 @@
 namespace Kopete
 {
 class Protocol;
-	
+
+/**
+ * A ManagedConnectionAccount queries the NetworkStatus KDED Module before trying to connect using 
+ * connectwithPassword, starting a network connection if needed.  If the network is not available, 
+ * it delays calling performConnectWithPassword until it receives notification from the daemon 
+ * that the network is up.  The account receiveds notifications from the daemon of network failures
+ * and calls disconnect to set the account offline in a timely manner.
+ */
 class KOPETE_EXPORT ManagedConnectionAccount : public PasswordedAccount
 {
 	Q_OBJECT
 	public:
-			ManagedConnectionAccount( Protocol *parent, const QString &acctId, uint maxPasswordLength = 0, const char *name = 0 );
+		/**
+		* @brief ManagedConnectionAccount constructor.
+		* @param parent The protocol this account connects via
+		* @param acctId The ID of this account - should be unique within this protocol
+		* @param maxPasswordLength The maximum length for passwords for this account, or 0 for no limit
+		* @param name The name for this QObject
+		*/
+		ManagedConnectionAccount( Protocol *parent, const QString &acctId, uint maxPasswordLength = 0, const char *name = 0 );
 	public slots:
+		/**
+		 * @brief Begin the connection process, by checking if the connection is available with the ConnectionManager.
+		 * This method is called by PasswordedAccount::connect()
+		 * @param password the password to connect with.
+		 */
 		void connectWithPassword( const QString &password );
 	protected:
+		/** 
+		 * @brief Connect to the server, once the network is available.
+		 * This method is called by the ManagedConnectionAccount once the network is available. In this method you should set up your 
+		 * network connection and connect to the server.
+		 */
 		virtual void performConnectWithPassword( const QString & password ) = 0;
 	protected slots:
-		void slotConnectionStatusChanged( const QString & host, NetworkStatus::EnumStatus );
+		/**
+		 * @brief Handle a change in the network connection
+		 * Called by the ConnectionManager when the network comes up or fails.  
+		 * The default implementation calls performConnectWithPassword when the network goes online and connectWithPassword() was
+		 * previously called, and calls disconnect() when the connection goes down.
+		 * @param host For future expansion.
+		 * @param status the new status of the network
+		 */
+		virtual void slotConnectionStatusChanged( const QString & host, NetworkStatus::EnumStatus status );
 	private:
 		QString m_password;
 		bool m_waitingForConnection;
