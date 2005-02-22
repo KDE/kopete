efa103c6a06774f2711d0e7b84a08b2dd6857b4a
Stephan Binner <binner@kde.org>
2005-02-22 20:44:55 +0000

    increase version
    svn path=/trunk/kdenetwork/kopete/; revision=392385

--- kopete/main.cpp
+++ kopete/main.cpp
@@ -25,7 +25,7 @@
 #include "kopeteiface.h"
 #include "kimifaceimpl.h"
 
-#define KOPETE_VERSION "0.9.92"
+#define KOPETE_VERSION "0.10"
 
 static const char description[] =
 	I18N_NOOP( "Kopete, the KDE Instant Messenger" );
