cd910cdcb8a828e38876b1034a550d8852b0a965
Will Stephenson <wstephenson@kde.org>
2005-03-16 13:10:55 +0000

    this KTempFile was accidentally readded by mattr when merging make-it-better branch, creating a lot of empty tempfiles...
    svn path=/trunk/kdenetwork/kopete/; revision=398078

--- libkopete/kopetemessage.cpp
+++ libkopete/kopetemessage.cpp
@@ -28,7 +28,6 @@
 #include <klocale.h>
 #include <kiconloader.h>
 #include <kstringhandler.h>
-#include <ktempfile.h>
 #include <kmdcodec.h>
 
 #include "kopetemessage.h"
@@ -64,7 +63,6 @@ public:
 	bool rtfOverride;
 	QDateTime timeStamp;
 	QFont font;
-	KTempFile photoFile;
 
 	QColor fgColor;
 	QColor bgColor;
