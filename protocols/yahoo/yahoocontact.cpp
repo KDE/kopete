dc47161bd36d8800d89bdf9ee90bdedbcd611494
Olivier Goffart <ogoffart@kde.org>
2005-04-07 13:25:34 +0000

    Backport the fix in YahooContact::sync
    svn path=/branches/KDE_3_4_BRANCH/kdenetwork/kopete/; revision=403776

--- protocols/yahoo/yahoocontact.cpp
+++ protocols/yahoo/yahoocontact.cpp
@@ -49,7 +49,7 @@ YahooContact::YahooContact( YahooAccount *account, const QString &userId, const 
 	m_account = account;
 
 	// Update ContactList
-	setDisplayName( fullName );
+	setNickName( fullName );
 	setOnlineStatus( static_cast<YahooProtocol*>( m_account->protocol() )->Offline );
 
 	if ( m_account->haveContactList() )
@@ -97,7 +97,7 @@ void YahooContact::sync(unsigned int flags)
 	else
 	{
 		QString newGroup = metaContact()->groups().first()->displayName();
-		if ( flags == Kopete::Contact::DisplayNameChanged)
+		if ( flags & Kopete::Contact::MovedBetweenGroup )
 		{
 			kdDebug(14180) << k_funcinfo << "contact changed groups. moving on server" << endl;
 			m_account->yahooSession()->changeBuddyGroup( contactId(), m_groupName, newGroup );
