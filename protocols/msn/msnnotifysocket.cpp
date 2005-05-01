aac374daf10100bff5a90e9fa855e6bd252a7688
Olivier Goffart <ogoffart@kde.org>
2005-05-01 13:06:41 +0000

    Ignore the error 715.
    BUG: 104497
    
    svn path=/trunk/kdenetwork/kopete/; revision=409010

--- protocols/msn/msnnotifysocket.cpp
+++ protocols/msn/msnnotifysocket.cpp
@@ -202,11 +202,13 @@ void MSNNotifySocket::handleError( uint code, uint id )
 	}
 	case 715:
 	{
+		/*
 		//if(handlev==m_account->accountId())
 		QString msg = i18n( "Your email address has not been verified with the MSN server.\n"
 			"You should have received a mail with a link to confirm your email address.\n"
 			"Some functions will be restricted if you do not confirm your email address." );
 		KMessageBox::queuedMessageBox( Kopete::UI::Global::mainWidget(), KMessageBox::Sorry, msg, i18n( "MSN Plugin" ) );//TODO don't show again
+		*/
 		break;
 	}
 	case 800:
