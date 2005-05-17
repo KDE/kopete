ae1bd9b8fc990c17118ba93aae1efd7a9927a1cf
Matt Rogers <mattr@kde.org>
2005-05-17 02:29:21 +0000

    in order to make oscar handle errors first, i gotta screw it up some. :)
    svn path=/branches/work/kopete/better-oscar-error-handling/; revision=414824

--- protocols/meanwhile/log.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-
-#define LOG(msg) \
-{                \
-    FILE *fp = fopen("/tmp/mean.out","a"); \
-    fprintf(fp,"%s\n",msg);                \
-    fclose(fp);                            \
-}
