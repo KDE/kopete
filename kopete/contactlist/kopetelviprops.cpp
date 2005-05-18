be82a4594e96ba1f6626f460b9183252cc3d8ead
Duncan Mac-Vicar Prett <duncan@kde.org>
2005-05-18 20:31:45 +0000

    backport: disable sync photo if you are not using server photo
    svn path=/branches/KDE/3.4/kdenetwork/kopete/; revision=415533

--- kopete/contactlist/kopetelviprops.cpp
+++ kopete/contactlist/kopetelviprops.cpp
@@ -172,6 +172,7 @@ KopeteMetaLVIProps::KopeteMetaLVIProps(KopeteMetaContactLVI *lvi, QWidget *paren
 	mainWidget->chkTrackChildPhoto->setChecked( item->metaContact()->photoSource() != 0 );
 	mainWidget->chkTrackChildDisplayName->setEnabled( item->metaContact()->contacts().count() > 0 );
 	mainWidget->chkTrackChildPhoto->setEnabled( item->metaContact()->contacts().count() > 0 );
+	mainWidget->chkSyncPhoto->setEnabled( mainWidget->chkTrackChildPhoto->isEnabled() );
 	mainWidget->chkSyncPhoto->setChecked( item->metaContact()->isPhotoSyncedWithKABC() );
 	mainWidget->cmbAccount->setEnabled( mainWidget->chkTrackChildDisplayName->isChecked() );
 	
@@ -300,6 +301,7 @@ KopeteMetaLVIProps::~KopeteMetaLVIProps()
 void KopeteMetaLVIProps::slotSetPhotoComboEnabled( bool on )
 {
 	mainWidget->cmbAccountPhoto->setEnabled(on);
+	mainWidget->chkSyncPhoto->setEnabled(on);
 	mainWidget->lblPhotoAccount->setEnabled(on);
 }
 
