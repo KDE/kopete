89f61dd2235c0e4cf8786bc7ba31c9d4684fc724
Cláudio da Silveira Pinheiro <taupter@gmail.com>
2005-05-21 17:39:02 +0000

    BUGFIX: Forgot to commit these new files. They're the basis of the VideoDeviceList thing.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=416486

---
+++ libkopete/avdevice/videodevicelistitem.h
@@ -0,0 +1,34 @@
+//
+// C++ Interface: videodevicelistitem
+//
+// Description: 
+//
+//
+// Author: Kopete Developers <kopete-devel@kde.org>, (C) 2005
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef KOPETE_AVVIDEODEVICELISTITEM_H
+#define KOPETE_AVVIDEODEVICELISTITEM_H
+
+namespace Kopete {
+
+namespace AV {
+
+/**
+@author Kopete Developers
+*/
+class VideoDeviceListItem{
+public:
+    VideoDeviceListItem();
+
+    ~VideoDeviceListItem();
+
+};
+
+}
+
+}
+
+#endif
