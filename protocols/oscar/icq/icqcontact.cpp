e1f9f6664b70bcca122c36ceffea0eeb5209c566
Matt Rogers <mattr@kde.org>
2005-07-28 01:54:19 +0000

    backport from trunk:
    better property detection
    
    
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=439396

--- protocols/oscar/icq/icqcontact.cpp
+++ protocols/oscar/icq/icqcontact.cpp
@@ -144,7 +144,7 @@ void ICQContact::loggedIn()
 	if ( m_ssiItem.waitingAuth() )
 		setOnlineStatus( mProtocol->statusManager()->waitingForAuth() );
 
-	if ( nickName().isEmpty() ) 
+	if ( hasProperty( "nickName" ) )
 	{
 		int time = ( KApplication::random() % 20 ) * 1000;
 		kdDebug(OSCAR_ICQ_DEBUG) << k_funcinfo << "updating nickname in " << time/1000 << " seconds" << endl;
