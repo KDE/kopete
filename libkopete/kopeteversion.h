9e4ad130b2892ee4d84f797a0cbf06fe24b509f2
Olivier Goffart <ogoffart@kde.org>
2005-08-22 08:26:51 +0000

    kopeteversion.h can be used by plugins to know the version
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=451925

---
+++ libkopete/kopeteversion.h
@@ -0,0 +1,29 @@
+/*
+    Kopete    (c) 2002-2005 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#ifndef _KOPETE_VERSION_H_
+#define _KOPETE_VERSION_H_
+
+#define KOPETE_VERSION_STRING "0.10.91  (svn, >= 20050822)"
+#define KOPETE_VERSION_MAJOR 0
+#define KOPETE_VERSION_MINOR 10
+#define KOPETE_VERSION_RELEASE 91
+#define KOPETE_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
+
+#define KOPETE_VERSION \
+  KOPETE_MAKE_VERSION(KOPETE_VERSION_MAJOR,KOPETE_VERSION_MINOR,KOPETE_VERSION_RELEASE)
+
+#define KOPETE_IS_VERSION(a,b,c) ( KOPETE_VERSION >= KOPETE_MAKE_VERSION(a,b,c) )
+
+
+#endif // _KOPETE_VERSION_H_
