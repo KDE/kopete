603b5f890d0eb4ecaaa40cec1376d9dacf157134
Cláudio da Silveira Pinheiro <taupter@gmail.com>
2005-08-24 23:56:10 +0000

    Quick fix for the devices not being detected because of the absence of ENABLE_AV symbol.
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=453028

--- libkopete/avdevice/videodevice.cpp
+++ libkopete/avdevice/videodevice.cpp
@@ -1,14 +1,19 @@
-//
-// C++ Implementation: videodevicelistitem
-//
-// Description: 
-//
-//
-// Author: Kopete Developers <kopete-devel@kde.org>, (C) 2005
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/*
+    videodevice.cpp  -  Kopete Video Device Low-level Support
+
+    Copyright (c) 2005 by Cláudio da Silveira Pinheiro   <taupter@gmail.com>
+
+    Kopete    (c) 2002-2003      by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU Lesser General Public            *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
 
 #include <iostream>
 #include <ostream>
