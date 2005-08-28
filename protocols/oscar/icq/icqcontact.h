21ba6915cba1d943579b11dc436de4e70ff5bb56
Matt Rogers <mattr@kde.org>
2005-08-28 00:49:06 +0000

    Fix bug 103251. Patch by Roman Jarosz. Thanks!
    BUG: 103251
    
    
    svn path=/branches/work/kopete/dev-0.12/kopete/; revision=454104

--- protocols/oscar/icq/icqcontact.h
+++ protocols/oscar/icq/icqcontact.h
@@ -112,6 +112,8 @@ private slots:
 	/** Authorize this contact */
 	void slotSendAuth();
 	
+	void slotAuthReplyDialogOkClicked();
+	
 	/** We have received an auth request */
 	void slotGotAuthRequest( const QString& contact, const QString& reason );
 	
--- protocols/oscar/icq/ui/icqauthreplydialog.cpp
+++ protocols/oscar/icq/ui/icqauthreplydialog.cpp
@@ -33,6 +33,7 @@ ICQAuthReplyDialog::ICQAuthReplyDialog( QWidget *parent, const char *name, bool 
 	
 	if ( !m_wasRequested )
 	{
+		this->setWFlags( this->getWFlags() | Qt::WDestructiveClose);
 		m_ui->lblReqReason->hide();
 		m_ui->lblRequestReason->hide();
 	}
