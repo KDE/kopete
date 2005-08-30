e74511280551878bb069498444ba5d3e198cf353
Andre Duffeck <duffeck@kde.org>
2005-08-30 14:59:21 +0000

    forwardport: Fix weird lost messages.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=455081

--- protocols/yahoo/yahoocontact.cpp
+++ protocols/yahoo/yahoocontact.cpp
@@ -239,7 +239,7 @@ void YahooContact::slotSendMessage( Kopete::Message &message )
 		pos = regExp.search( messageText, pos );
 		if ( pos >= 0 ) {
 			pos += regExp.matchedLength();
-			messageText.replace( regExp, QString::fromLatin1("<span\\1\\3><font face=\"\\2\">\\4</font></span>" ) );
+			messageText.replace( regExp, QString::fromLatin1("<span\\1\\3><font face=\"\\2\">\\4</span>" ) );
 		}
 	}
 	
@@ -250,7 +250,7 @@ void YahooContact::slotSendMessage( Kopete::Message &message )
 		pos = regExp.search( messageText, pos );
 		if ( pos >= 0 ) {
 			pos += regExp.matchedLength();
-			messageText.replace( regExp, QString::fromLatin1("<span\\1\\3><font size=\"\\2\">\\4</font></span>" ) );
+			messageText.replace( regExp, QString::fromLatin1("<span\\1\\3><font size=\"\\2\">\\4</span>" ) );
 		}
 	}
 	
