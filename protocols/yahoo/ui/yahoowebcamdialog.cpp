d72a3bff947c6aeec0c459240ff8da28e0c70b34
Laurent Montel <montel@kde.org>
2005-09-11 12:48:18 +0000

    Adapt to new api
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=459565

--- protocols/yahoo/ui/yahoowebcamdialog.cpp
+++ protocols/yahoo/ui/yahoowebcamdialog.cpp
@@ -43,7 +43,7 @@ YahooWebcamDialog::YahooWebcamDialog( YahooContact* contact, QWidget * parent, c
 	/*
 	QObject::connect( contact, SIGNAL( webcamClosed( int ) ), this, SLOT( webcamClosed( int ) ) );
 	*/
-	Q3Frame* page = plainPage();
+	QFrame* page = plainPage();
 	if ( page )
 	{
 		kdDebug(14180) << k_funcinfo << "Adding webcam image container" << endl;
