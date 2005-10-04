fa92d8e18a54dca856a9e3f849cf2ca522ffff4f
Matthias Granberry <matthias.granberry@gmail.com>
2005-10-04 18:21:16 +0000

    Populate AIM user info dialog
    svn path=/branches/work/kopete/dev-0.12/kopete/; revision=467251

--- protocols/oscar/aim/aimuserinfo.cpp
+++ protocols/oscar/aim/aimuserinfo.cpp
@@ -167,13 +167,10 @@ void AIMUserInfoDialog::slotUpdateProfile()
 {
 	kdDebug(14152) << k_funcinfo << "Got User Profile." << endl;
 
-/*	mMainWidget->txtOnlineSince->setText(m_contact->userInfo().onlinesince.toString());
-	mMainWidget->txtIdleTime->setText(QString::number(m_contact->userInfo().idletime));
-	mMainWidget->txtAwayMessage->setText(m_contact->awayMessage());
-	mMainWidget->txtWarnLevel->setText(QString::number(m_contact->userInfo().evil)); */
-
 	AIMProtocol* p = static_cast<AIMProtocol*>( mAccount->protocol() );
 	QString awayMessage = m_contact->property( p->awayMessage ).value().toString();
+	mMainWidget->txtAwayMessage->setText( awayMessage );
+	
 	if ( awayMessage.isNull() )
 	{
 		mMainWidget->txtAwayMessage->hide();
@@ -184,7 +181,15 @@ void AIMUserInfoDialog::slotUpdateProfile()
 		mMainWidget->txtAwayMessage->show();
 		mMainWidget->lblAwayMessage->show();
 	}
-
+	
+	QString onlineSince =  m_contact->property("onlineSince").value().toString();
+	//QString onlineSince = m_details.onlineSinceTime().toString();
+	mMainWidget->txtOnlineSince->setText( onlineSince );
+	
+	AIMContact* c = static_cast<AIMContact*>( m_contact );
+	mMainWidget->txtIdleTime->setText(c->formattedIdleTime());
+	mMainWidget->txtWarnLevel->setText(QString::number(c->warningLevel())); 
+	
 	QString contactProfile = m_contact->property( p->clientProfile ).value().toString();
 	if ( contactProfile.isNull() )
 	{
