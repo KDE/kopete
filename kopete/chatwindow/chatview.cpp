9b5bc2163eb786fb44646703062471786886d739
Matthias Granberry <matthias.granberry@gmail.com>
2005-12-17 22:02:23 +0000

    Count was already correct in 0.12 branch
    svn path=/branches/work/kopete/dev-0.12/kopete/; revision=489260

--- kopete/chatwindow/chatview.cpp
+++ kopete/chatwindow/chatview.cpp
@@ -409,7 +409,7 @@ void ChatView::updateChatState( KopeteTabState newState )
 	if( newState != Typing )
 	{
 		setStatusText( i18n( "One other person in the chat",
-			       "%n other people in the chat", m_manager->members().count() -1 ) );
+			       "%n other people in the chat", m_manager->members().count() ) );
 	}
 }
 
