d5604ef771bbfb7f9a2a35d827d5a615bf8d6b71
Adriaan de Groot <groot@kde.org>
2005-12-20 21:04:04 +0000

    Need sys/types for in.h on FreeBSD 4.X.
    Thanks for the patch to the most lanolinine ovidious lofi@freebsd
    for doing awful drudge work like compiling KDE on legacy systems
    in the name of providing long-life support for a release.
    
    CCMAIL: kde@freebsd.org
    
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=490117

--- protocols/jabber/libiris/iris/jabber/s5b.cpp
+++ protocols/jabber/libiris/iris/jabber/s5b.cpp
@@ -18,6 +18,8 @@
  *
  */
 
+#include <config.h>
+
 #include"s5b.h"
 
 #include<qtimer.h>
@@ -32,6 +34,9 @@
 #ifdef Q_OS_WIN
 # include <windows.h>
 #else
+# ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+# endif
 # include <netinet/in.h>
 #endif
 
