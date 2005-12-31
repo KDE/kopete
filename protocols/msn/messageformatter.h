00490f11ac00275a878eaa00b91498b2d3288b27
Gregg Edghill <gregg.edghill@gmail.com>
2005-12-31 14:49:23 +0000

    Commit transport bridge used to connect two peer directly.
    svn path=/branches/work/kopete/dev-0.12/kopete/; revision=492901

--- protocols/msn/messageformatter.h
+++ protocols/msn/messageformatter.h
@@ -1,7 +1,9 @@
 /*
-    messageformatter.h - msn p2p protocol
+    messageformatter.h - Peer to peer message formatter
 
-    Copyright (c) 2005      by Gregg Edghill          <gregg.edghill@gmail.com>
+    Copyright (c) 2005 by Gregg Edghill     <gregg.edghill@gmail.com>
+
+    Kopete    (c) 2002-2005 by the Kopete developers <kopete-devel@kde.org>
 
     *************************************************************************
     *                                                                       *
@@ -18,23 +20,21 @@
 
 #include <qobject.h>
 
-namespace P2P{
-	class Message;
-}
+namespace PeerToPeer {
 
 /**
-@author Kopete Developers
-*/
-namespace P2P{
-	class MessageFormatter : public QObject
-	{	Q_OBJECT
-		public:
-			MessageFormatter(QObject *parent = 0, const char *name = 0);
-			~MessageFormatter();
+ @author Gregg Edghill <gregg.edghill@gmail.com> */
+class MessageFormatter : public QObject
+{
+	Q_OBJECT
+public:
+	MessageFormatter(QObject *parent = 0, const char *name = 0);
+	~MessageFormatter();
 
-			Message readMessage(const QByteArray& stream, bool compact=false);
-			void writeMessage(const Message& message, QByteArray& stream, bool compact=false);
-	};
+// 			Message readMessage(const QByteArray& stream, bool compact=false);
+// 			void writeMessage(const Message& message, QByteArray& stream, bool compact=false);
+};
+
 }
 
 #endif
