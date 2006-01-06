fdc88a5f0f772cf00ad1edfbb76de525c67f7452
Olivier Goffart <ogoffart@kde.org>
2006-01-06 23:14:53 +0000

    support of  jabber:iq:gateway  (JEP-0100)  when adding legacy contact
    svn path=/branches/work/kopete/dev-0.12/kopete/; revision=495053

--- protocols/jabber/ui/jabberaddcontactpage.cpp
+++ protocols/jabber/ui/jabberaddcontactpage.cpp
@@ -38,13 +38,24 @@
 JabberAddContactPage::JabberAddContactPage (Kopete::Account * owner, QWidget * parent, const char *name):AddContactPage (parent, name)
 {
 	(new QVBoxLayout (this))->setAutoAdd (true);
-	if (owner->isConnected ())
+	
+	JabberTransport *transport=dynamic_cast<JabberTransport*>(owner);
+	JabberAccount *jaccount= transport ? transport->account() : dynamic_cast<JabberAccount*>(owner);
+	
+	if (jaccount->isConnected ())
 	{
 		jabData = new dlgAddContact (this);
 		jabData->show ();
-
+		
+		if(transport)
+		{
+			jabData->textLabel1->setText( i18n("Loading instruction from gateway...") );
+			XMPP::JT_Gateway * gatewayTask = new XMPP::JT_Gateway ( jaccount->client()->rootTask () );
+			QObject::connect (gatewayTask, SIGNAL (finished ()), this, SLOT (slotPromtReceived()));
+			gatewayTask->get ( transport->myself()->contactId() );
+			gatewayTask->go ( true );
+		}
 		canadd = true;
-
 	}
 	else
 	{
@@ -77,8 +88,13 @@ bool JabberAddContactPage::apply ( Kopete::Account *account, Kopete::MetaContact
 		
 		if(transport)
 		{
-			//TODO: support jabber:iq:gateway (JEP-0100)  or  JID escaping (JEP-0106)
-			contactId= contactId.replace('@','%') + "@" + transport->myself()->contactId();
+			XMPP::JT_Gateway * gatewayTask = new XMPP::JT_Gateway ( jaccount->client()->rootTask () );
+			JabberAddContactPage_there_is_no_possibility_to_add_assync_WORKAROUND *workaround = 
+					new JabberAddContactPage_there_is_no_possibility_to_add_assync_WORKAROUND( transport , parentContact );
+			QObject::connect (gatewayTask, SIGNAL (finished ()), workaround, SLOT (slotJidReceived()));
+			gatewayTask->set ( transport->myself()->contactId() , contactId );
+			gatewayTask->go ( true );
+			return true;
 		}
 		
 		QString displayName = parentContact->displayName ();
@@ -119,6 +135,82 @@ bool JabberAddContactPage::apply ( Kopete::Account *account, Kopete::MetaContact
 
 	return false;
 }
+
+void JabberAddContactPage::slotPromtReceived( )
+{
+	XMPP::JT_Gateway * task = (XMPP::JT_Gateway *) sender ();
+
+	if (task->success ())
+	{
+		jabData->lblID->setText( task->prompt() );
+		jabData->textLabel1->setText( task->desc() );
+	}
+	else
+	{
+		jabData->textLabel1->setText( i18n("An error occured while loading instructions from wateway.") );
+	}
+}
+
+JabberAddContactPage_there_is_no_possibility_to_add_assync_WORKAROUND::JabberAddContactPage_there_is_no_possibility_to_add_assync_WORKAROUND( JabberTransport *t, Kopete::MetaContact * mc )
+	: QObject(t) , metacontact(mc) ,  transport(t)
+{}
+
+void JabberAddContactPage_there_is_no_possibility_to_add_assync_WORKAROUND::slotJidReceived( )
+{
+	XMPP::JT_Gateway * task = (XMPP::JT_Gateway *) sender ();
+	
+	if (!task->success ())
+	{
+		return;
+		// maybe we should show an error message, but i don't like showing error message  - Olivier
+	}
+
+	QString contactId=task->prompt();
+	
+	Kopete::MetaContact* parentContact=metacontact;
+	JabberAccount *jaccount=transport->account();;
+	
+	/*\
+	 *   this is a copy of the end of JabberAddContactPage::apply
+	\*/
+	
+	QString displayName = parentContact->displayName ();
+		/*		
+	if ( displayName.isEmpty () )
+	displayName = contactId;
+		*/
+		// collect all group names
+	QStringList groupNames;
+	Kopete::GroupList groupList = parentContact->groups();
+	for(Kopete::Group *group = groupList.first(); group; group = groupList.next())
+		groupNames += group->displayName();
+
+	if ( jaccount->addContact ( contactId, parentContact, Kopete::Account::ChangeKABC ) )
+	{
+		XMPP::RosterItem item;
+		XMPP::Jid jid ( contactId );
+
+		item.setJid ( jid );
+		item.setName ( displayName );
+		item.setGroups ( groupNames );
+
+			// add the new contact to our roster.
+		XMPP::JT_Roster * rosterTask = new XMPP::JT_Roster ( jaccount->client()->rootTask () );
+
+		rosterTask->set ( item.jid(), item.name(), item.groups() );
+		rosterTask->go ( true );
+
+			// send a subscription request.
+		XMPP::JT_Presence *presenceTask = new XMPP::JT_Presence ( jaccount->client()->rootTask () );
+
+		presenceTask->sub ( jid, "subscribe" );
+		presenceTask->go ( true );
+
+		return;
+	}
+}
+
+
 
 #include "jabberaddcontactpage.moc"
 
