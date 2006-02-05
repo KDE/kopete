5df366dfa023281c7044b5faa4589adf5f1ed534
Andre Duffeck <duffeck@kde.org>
2006-02-05 20:09:30 +0000

    Fix "cannot find .rc file" error in conferences chatsession. Stupid stupid me. It is now possible to invite others to a already existing conference.
    svn path=/branches/work/kopete/dev-0.12/kopete/; revision=506122

--- protocols/yahoo/yahooconferencemessagemanager.cpp
+++ protocols/yahoo/yahooconferencemessagemanager.cpp
@@ -40,6 +40,7 @@ YahooConferenceChatSession::YahooConferenceChatSession( const QString & yahooRoo
 {
 
 	Kopete::ChatSessionManager::self()->registerChatSession( this );
+	setInstance(protocol->instance());
 
 	connect ( this, SIGNAL( messageSent ( Kopete::Message &, Kopete::ChatSession * ) ),
 			  SLOT( slotMessageSent ( Kopete::Message &, Kopete::ChatSession * ) ) );
