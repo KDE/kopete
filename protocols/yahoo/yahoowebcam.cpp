4b6fd86d118a1dd7895c827bae0b035e710318ab
Andre Duffeck <duffeck@kde.org>
2006-03-07 19:07:11 +0000

    250ms
    svn path=/branches/kopete/0.12/kopete/; revision=516594

--- protocols/yahoo/yahoowebcam.cpp
+++ protocols/yahoo/yahoowebcam.cpp
@@ -47,7 +47,7 @@ YahooWebcam::YahooWebcam( YahooAccount *account ) : QObject( 0, "yahoo_webcam" )
 	m_devicePool->open();
 	m_devicePool->setSize(320, 240);
 	m_devicePool->startCapturing();
-	m_updateTimer->start( 25 );
+	m_updateTimer->start( 250 );
 }
 
 YahooWebcam::~YahooWebcam()
