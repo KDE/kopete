a6ab5532a463290299619a36fc4d6d899bafdc04
Olivier Goffart <ogoffart@kde.org>
2006-03-10 22:44:41 +0000

    Attempt to fix Bug 118760 CCBUG: 118760
    Does this fix the problem ?
    
    
    
    svn path=/branches/kopete/0.12/kopete/; revision=517414

--- protocols/msn/msnsecureloginhandler.cpp
+++ protocols/msn/msnsecureloginhandler.cpp
@@ -74,7 +74,7 @@ void MSNSecureLoginHandler::slotLoginServerReceived(KIO::Job *loginJob)
 								"OrgVerb=GET,"
 								"OrgURL=http%3A%2F%2Fmessenger%2Emsn%2Ecom,"
 								"sign-in=" + KURL::encode_string(m_accountId) +
-								",pwd=" + KURL::encode_string( m_password ) +
+								",pwd=" + KURL::encode_string( m_password ).replace(',',"%2C") +
 								"," + m_authentification + "\r\n";
 
 //   warning, this debug contains the password
