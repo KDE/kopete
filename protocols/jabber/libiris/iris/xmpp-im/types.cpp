40c18aa8484b9c72caf4cbd5b1aa226474b6f60a
Olivier Goffart <ogoffart@kde.org>
2006-03-11 11:15:54 +0000

    revert changes to that file that i commited by mistake
    svn path=/branches/kopete/0.12/kopete/; revision=517531

--- protocols/jabber/libiris/iris/xmpp-im/types.cpp
+++ protocols/jabber/libiris/iris/xmpp-im/types.cpp
@@ -615,10 +615,8 @@ bool Message::fromStanza(const Stanza &s, int timeZoneOffset)
 				}
 				else if(e.tagName() == "thread")
 					d->thread = e.text();
-#if 0
 			}
 			else if (e.namespaceURI() == s.xhtmlImNS()) {
-#endif
 				 if (e.tagName() == "html") {
 					QDomNodeList htmlNL= e.childNodes();
 					for (unsigned int x = 0; x < htmlNL.count(); x++) {
