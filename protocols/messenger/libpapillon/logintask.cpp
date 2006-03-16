ac641348ceb84ca54419daf3286265d7262d4a14
Michaël Larouche <larouche@kde.org>
2006-03-16 21:54:41 +0000

    Added LoginTask (not complete)
    Also include modification of Makefile.am for both LoginTask and NotificationConnection.
    
    
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=519344

---
+++ protocols/messenger/libpapillon/logintask.cpp
@@ -0,0 +1,223 @@
+/*
+   logintask.cpp - Windows Live Messenger Login Task
+
+   Copyright (c) 2006 by Michaël Larouche <michael.larouche@kdemail.net>
+
+   *************************************************************************
+   *                                                                       *
+   * This library is free software; you can redistribute it and/or         *
+   * modify it under the terms of the GNU Lesser General Public            *
+   * License as published by the Free Software Foundation; either          *
+   * version 2 of the License, or (at your option) any later version.      *
+   *                                                                       *
+   *************************************************************************
+*/
+#include "logintask.h"
+
+// Qt includes
+#include <QLatin1String>
+#include <QStringList>
+
+// Papillon includes
+#include "transfer.h"
+
+namespace Papillon 
+{
+
+class LoginTask::Private
+{
+public:
+	Private()
+	 : currentState(LoginTask::StateVersion)
+	{}
+
+	QString passportId;
+	// TODO: Use QSecureArray
+	QString password;
+
+	LoginTask::LoginState currentState;
+
+	// Keep track of the expected transaction ID.
+	QString currentTransactionId;
+
+	QString tweenerTicket;
+};
+
+LoginTask::LoginTask(Task *parent)
+ : Task(parent), d(new Private)
+{
+}
+
+LoginTask::~LoginTask()
+{
+	delete d;
+}
+
+void LoginTask::setUserInfo(const QString &passportId, const QString &password)
+{
+	d->passportId = passportId;
+	d->password = password;
+}
+
+LoginTask::LoginState LoginTask::loginState() const
+{
+	return d->currentState;
+}
+
+bool LoginTask::take(Transfer *transfer)
+{
+	if( forMe(transfer) )
+	{
+		bool proceeded = false;
+		switch(d->currentState)
+		{
+			case StateVersion:
+			{
+				if( transfer->command() == QLatin1String("VER") )
+				{
+					d->currentState = StateCVR;
+					proceeded = true;
+					sendCvrCommand();
+				}
+				break;
+			}
+			case StateCVR:
+			{
+				if( transfer->command() == QLatin1String("CVR") )
+				{
+					d->currentState = StateTweenerInvite;
+					proceeded = true;
+					sendTweenerInviteCommand();
+				}
+				break;
+			}
+			case StateTweenerInvite:
+			{
+				if( transfer->command() == QLatin1String("USR") )
+				{
+					if( transfer->arguments[0] == QLatin1String("TWN") && transfer->arguments[1] == QLatin1String("S") )
+					{
+						d->currentState = StateTweenerConfirm;
+						proceeded = true;
+						// TODO: Get the ticket then send the ticket.
+						d->currentState = StateError;
+						setError(0, QString());
+					}
+				}
+				else if( transfer->command() == QLatin1String("XFR") )
+				{
+					QString newServer = readTransfer->arguments()[1].section(":", 0, 0);
+					QString tempPort = readTransfer->arguments()[1].section(":", 1, 1);
+					bool dummy;
+					int newPort = tempPort.toUInt(&dummy);
+
+					proceeded = true;
+					emit redirection(newServer, tempPort);
+				}
+				break;
+			}
+			case StateTweenerConfirmed:
+			{
+				if( transfer->command() == QLatin1String("USR") )
+				{
+					if( transfer->arguments[0] == QLatin1String("OK") )
+					{
+						proceeded = true;
+						d->currentState = StateFinish;
+						// End the login task.
+						setSuccess(0, QString());
+					}
+				}
+				break;
+			}
+			default:
+				return false;
+		}
+
+		return proceeded;
+	}
+
+	return false;
+}
+
+bool LoginTask::forMe(Transfer *transfer)
+{
+	if( transfer->type == Transfer::TransactionTransfer )
+	{
+		if( transfer->transactionId() == d->currentTransactionId )
+			return true;
+	}
+
+	return false;
+}
+
+void LoginTask::onGo()
+{
+	Q_ASSERT(!d->passportId.isEmpty());
+	Q_ASSERT(!d->password.isEmpty());
+
+	sendVersionCommand();
+}
+
+void LoginTask::sendVersionCommand()
+{
+	Transfer *versionTransfer = new Transfer(Transfer::TransactionTransfer);
+	versionTransfer->setCommand( QLatin1String("VER") );
+
+	d->currentTransactionId = QString::number( connection()->transactionId() );
+	versionTransfer->setTransactionId( d->currentTransactionId );
+
+	QStringList arguments = QString("MSNP11 MSNP10 CVR0").split(" ");
+	versionTransfer->setArguments(arguments);
+
+	send(versionTransfer);
+}
+
+void LoginTask::sendCvrCommand()
+{
+	Transfer *cvrTransfer = new Transfer(Transfer::TransactionTransfer);
+	cvrTransfer->setCommand( QLatin1String("CVR") );
+	
+	d->currentTransactionId = QString::number( connection()->transactionId() );
+	cvrTransfer->setTransactionId( d->currentTransactionId );
+
+	QString arguments = QString("0x040c winnt 5.1 i386 MSNMSGR 7.0.0777 msmsgs %1").arg(d->passportId);
+	QStringList argsList = arguments.split(" ");
+	cvrTransfer->setArguments(argsList);
+
+	send(cvrTransfer);
+}
+
+void LoginTask::sendTweenerInviteCommand()
+{
+	Transfer *twnTransfer = new Transfer(Transfer::TransactionTransfer);
+	twnTransfer->setCommand("USR");
+
+	d->currentTransactionId = QString::number( connection()->transactionId() );
+	twnTransfer->setTransactionId( d->currentTransactionId );
+
+	QString arguments = QString("TWN I %1").arg(d->passportId);
+	QStringList args = arguments.split(" ");
+	twnTransfer->setArguments(args);
+
+	send(twnTransfer);
+}
+
+void LoginTask::sendTweenerConfirmation()
+{
+	Transfer *twnTransfer = new Transfer(Transfer::TransactionTransfer);
+	twnTransfer->setCommand("USR");
+
+	d->currentTransactionId = QString::number( connection()->transactionId() );
+	twnTransfer->setTransactionId( d->currentTransactionId );
+
+	QString arguments = QString("TWN S %1").arg(d->tweenerTicket);
+	QStringList args = arguments.split(" ");
+	twnTransfer->setArguments(args);
+
+	send(twnTransfer);
+}
+
+}
+
+#include "logintask.moc"
---
+++ protocols/messenger/libpapillon/logintask.h
@@ -0,0 +1,146 @@
+/*
+   logintask.h - Windows Live Messenger Login Task
+
+   Copyright (c) 2006 by Michaël Larouche <michael.larouche@kdemail.net>
+
+   *************************************************************************
+   *                                                                       *
+   * This library is free software; you can redistribute it and/or         *
+   * modify it under the terms of the GNU Lesser General Public            *
+   * License as published by the Free Software Foundation; either          *
+   * version 2 of the License, or (at your option) any later version.      *
+   *                                                                       *
+   *************************************************************************
+*/
+#ifndef PAPILLONLOGINTASK_H
+#define PAPILLONLOGINTASK_H
+
+#include <task.h>
+#include <papillon_macros.h>
+
+namespace Papillon 
+{
+
+class Transfer;
+/**
+ * Login on Windows Live Messenger.
+ *
+ * First you create the Login Task, you must set the user info.
+ * @code
+ * LoginTask *login = new LoginTask( connection->rootTask() );
+ * login->setUserInfo("example@passport.com", "password");
+ * connect(login, SIGNAL(finished()), ...);
+ * connect(login, SIGNAL(redirection(const QString &, quint16)), ...);
+ * login->go();
+ * @endcode
+ * Use success() to check if the login was successful.
+ * Use loginState() to check for the error if the login wasn't succesful.
+ * @author Michaël Larouche <michael.larouche@kdemail.net>
+*/
+class PAPILLON_EXPORT LoginTask : public Task
+{
+	Q_OBJECT
+public:
+	/**
+	 * Current state of the login process.
+	 */
+	enum LoginState 
+	{
+		/**
+	 	 * Begin state, sending the version command.
+		 */
+		StateVersion,
+		/**
+		 * Sending the CVR command.
+		 */
+		StateCVR,
+		/**
+		 * Ask for the tweener ticket.
+		 */
+		StateTweenerInvite,
+		/**
+		 * Now confirm the tweener.
+		 */
+		StateTweenerConfirmed,
+		/**
+		 * Login was sucessful.
+		 */
+		StateFinish,
+		/**
+		 * A unknow error occured.
+		 */
+		StateError,
+		/**
+		 * Nexus service returned a bad passport error.
+		 */
+		StateBadPassword
+	};
+
+	/**
+	 * Create a new Login Task.
+	 * @param parent parent Task
+	 */
+	LoginTask(Task *parent);
+	/**
+	 * d-tor (duh)
+	 */
+	~LoginTask();
+
+	/**
+	 * Set the user info to log with.
+	 * @param passportId passport id to log with.
+	 * @param password user password.
+	 * TODO: Use a QSecureArray to pass the password.
+	 */
+	void setUserInfo(const QString &passportId, const QString &password);
+
+	/**
+	 * Inherited from Task.
+	 * Proceed the given transfer according to the current LoginState.
+	 * Move to the next login step if required.
+	 * @param transfer given Transfer.
+	 * @return true if we proceed this transfer.
+	 */
+	virtual bool take(Transfer *transfer);
+
+	/**
+	 * Get the current state of the Login Task.
+	 * @return the current LoginState.
+	 */
+	LoginState loginState() const;
+
+protected:
+	/**
+	 * Inherited from Task.
+	 * Helper method to check if the Transfer is for us.
+	 * @param transfer give Transfer.
+	 * @return true if the Transfer is for us.
+	 */
+	virtual bool forMe(Transfer *transfer);
+	/**
+	 * Inherited from Task.
+	 * Start the login process.
+	 */
+	virtual void onGo();
+
+signals:
+	/**
+	 * Emitted when the login need to redirect to another server.
+	 * Resulting slot should close the notification connection, connect to the new one and recreate a new Login Task.
+	 */
+	void redirection(const QString &server, quint16 port);
+
+private slots:
+	void sendVersionCommand();
+	void sendCvrCommand();
+	void sendTweenerInviteCommand();
+	void sendTweenerConfirmation();
+
+private:
+	class Private;
+	Private *d;
+};
+
+}
+
+#endif
