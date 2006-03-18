5f8572e82d9aa71e06cda3d58e7a225cc40f7581
Roman Jarosz <kedgedev@gmail.com>
2006-03-18 16:17:05 +0000

    Fix bug 115772: kopete crash after change my icq status to online. CloseConnectionTask is deleted when its root connection is closed. BUG: 115772
    svn path=/branches/kopete/0.12/kopete/; revision=520030

--- protocols/oscar/liboscar/client.cpp
+++ protocols/oscar/liboscar/client.cpp
@@ -110,7 +110,6 @@ public:
 	SSIAuthTask* ssiAuthTask;
 	ICQUserInfoRequestTask* icqInfoTask;
 	UserInfoTask* userInfoTask;
-	CloseConnectionTask* closeConnectionTask;
 	TypingNotifyTask * typingNotifyTask;
 	//Managers
 	SSIManager* ssiManager;
@@ -157,7 +156,6 @@ Client::Client( QObject* parent )
 	d->ssiAuthTask = 0L;
 	d->icqInfoTask = 0L;
 	d->userInfoTask = 0L;
-	d->closeConnectionTask = 0L;
 	d->stage = ClientPrivate::StageOne;
 	d->typingNotifyTask = 0L;
 	d->awayMsgRequestTimer = new QTimer();
@@ -371,7 +369,7 @@ void Client::startStageTwo()
 {
 	//create a new connection and set it up
 	Connection* c = createConnection( d->host, QString::number( d->port ) );
-	d->closeConnectionTask = new CloseConnectionTask( c->rootTask() );
+	new CloseConnectionTask( c->rootTask() );
 
 	//create the new login task
 	m_loginTaskTwo = new StageTwoLoginTask( c->rootTask() );
@@ -1288,7 +1286,6 @@ void Client::deleteStaticTasks()
 	delete d->ssiAuthTask;
 	delete d->icqInfoTask;
 	delete d->userInfoTask;
-	delete d->closeConnectionTask;
 	delete d->typingNotifyTask;
 
 	d->errorTask = 0;
@@ -1298,7 +1295,6 @@ void Client::deleteStaticTasks()
 	d->ssiAuthTask = 0;
 	d->icqInfoTask = 0;
 	d->userInfoTask = 0;
-	d->closeConnectionTask = 0;
 	d->typingNotifyTask = 0;
 }
 
