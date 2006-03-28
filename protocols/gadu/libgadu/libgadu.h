be5865049e20402d247b0d5ef2d669b7294d67e8
Grzegorz Jaskiewicz <gj@pointblue.com.pl>
2006-03-28 19:17:53 +0000

    update gadu lib
    svn path=/branches/kopete/0.12/kopete/; revision=523660

--- protocols/gadu/libgadu/libgadu.h
+++ protocols/gadu/libgadu/libgadu.h
@@ -437,7 +437,9 @@ enum gg_failure_t {
 	GG_FAILURE_PASSWORD,		/* nieprawid³owe has³o */
 	GG_FAILURE_404, 		/* XXX nieu¿ywane */
 	GG_FAILURE_TLS,			/* b³±d negocjacji TLS */
-	GG_FAILURE_NEED_EMAIL 		/* serwer roz³±czy³ nas z pro¶b± o zmianê emaila */
+	GG_FAILURE_NEED_EMAIL, 		/* serwer roz³±czy³ nas z pro¶b± o zmianê emaila */
+	GG_FAILURE_INTRUDER,		/* za du¿o prób po³±czenia siê z nieprawid³owym has³em */
+	GG_FAILURE_UNAVAILABLE		/* serwery s± wy³±czone */
 };
 
 /*
