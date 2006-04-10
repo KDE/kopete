280bcaa9693ca035116544c65363a67ccbbb5d32
Michaël Larouche <larouche@kde.org>
2006-04-10 20:43:01 +0000

    Added stub file soapactions.h that will contains helper functions to create SOAP actions.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=528364

---
+++ protocols/messenger/libpapillon/http/soapactions.h
@@ -0,0 +1,29 @@
+/*
+   soapactions.h - Helper functions to create SOAP actions.
+
+   Copyright (c) 2006 by Michaël Larouche <michael.larouche@kdemail.net>
+
+   *************************************************************************
+   *                                                                       *
+   * This library is free software; you can redistribute it and/or         *
+   * modify it under the terms of the GNU Lesser General Public            *
+   * License as published by the Free Software Foundation; either          *
+   * version 2 of the License, or (at your option) any later version.      *
+   *                                                                       *
+   *************************************************************************
+*/
+#ifndef PAPILLON_SOAP_ACTIONS_H
+#define PAPILLON_SOAP_ACTIONS_H
+
+namespace Papillon
+{
+
+/**
+ * @brief SoapActions namespace contains helper function to create standard SOAP actions used through Messenger protocol.
+ */
+namespace SoapActions
+{
+}
+
+}
+#endif
