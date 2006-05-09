2987b614dad1642f98273f0970dd61a6efde4845
Grzegorz Jaskiewicz <gj@pointblue.com.pl>
2006-05-09 07:16:33 +0000

    reflect recent changes in protocol
    svn path=/branches/kopete/0.12/kopete/; revision=538861

--- protocols/gadu/libgadu/libgadu.c
+++ protocols/gadu/libgadu/libgadu.c
@@ -1,10 +1,11 @@
 /* $Id$ */
 
 /*
- *  (C) Copyright 2001-2003 Wojtek Kaniewski <wojtekka@irc.pl>
+ *  (C) Copyright 2001-2006 Wojtek Kaniewski <wojtekka@irc.pl>
  *                          Robert J. Wo¼ny <speedy@ziew.org>
  *                          Arkadiusz Mi¶kiewicz <arekm@pld-linux.org>
  *                          Tomasz Chiliñski <chilek@chilan.com>
+ *                          Adam Wysocki <gophi@ekg.apcoh.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License Version
@@ -41,6 +42,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
 #ifdef __GG_LIBGADU_HAVE_OPENSSL
 #  include <openssl/err.h>
@@ -208,6 +210,8 @@ int gg_resolve(int *fd, int *pid, const char *hostname)
 	}
 
 	if (!res) {
+		close(pipes[0]);
+
 		if ((a.s_addr = inet_addr(hostname)) == INADDR_NONE) {
 			struct in_addr *hn;
 		
@@ -951,8 +955,10 @@ void gg_free_session(struct gg_session *sess)
 		sess->resolver = NULL;
 	}
 #else
-	if (sess->pid != -1)
+	if (sess->pid != -1) {
+		kill(sess->pid, SIGTERM);
 		waitpid(sess->pid, NULL, WNOHANG);
+	}
 #endif
 
 	if (sess->fd != -1)
@@ -1099,6 +1105,7 @@ void gg_logoff(struct gg_session *sess)
 	}
 #else
 	if (sess->pid != -1) {
+		kill(sess->pid, SIGTERM);
 		waitpid(sess->pid, NULL, WNOHANG);
 		sess->pid = -1;
 	}
