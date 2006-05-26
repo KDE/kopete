af45804c4c12c92a14eeb5f2c44c43e2558bb885
Hui Jin <blueangel.jin@gmail.com>
2006-05-26 12:14:21 +0000

    Copy the kopete/protocols/testbed to kopete/protocol/qq. It compiles, and runs well. Due to the similarity of testbed and qq, the new protocol does not appear.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=544902

---
+++ protocols/qq/testbedaddcontactpage.cpp
@@ -0,0 +1,73 @@
+/*
+    testbedaddcontactpage.cpp - Kopete Testbed Protocol
+
+    Copyright (c) 2003      by Will Stephenson		 <will@stevello.free-online.co.uk>
+    Kopete    (c) 2002-2003 by the Kopete developers <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU General Public                   *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#include "testbedaddcontactpage.h"
+
+#include <qlayout.h>
+#include <qradiobutton.h>
+//Added by qt3to4:
+#include <QVBoxLayout>
+#include <qlineedit.h>
+#include <kdebug.h>
+
+#include "kopeteaccount.h"
+#include "kopetemetacontact.h"
+#include "ui_testbedaddui.h"
+
+TestbedAddContactPage::TestbedAddContactPage( QWidget* parent )
+		: AddContactPage(parent)
+{
+	kDebug(14210) << k_funcinfo << endl;
+	QVBoxLayout* l = new QVBoxLayout( this );
+	QWidget* w = new QWidget();
+	m_testbedAddUI = new Ui::TestbedAddUI;
+	m_testbedAddUI->setupUi( w );
+	l->addWidget( w );
+}
+
+TestbedAddContactPage::~TestbedAddContactPage()
+{
+	delete m_testbedAddUI;
+}
+
+bool TestbedAddContactPage::apply( Kopete::Account* a, Kopete::MetaContact* m )
+{
+    if ( validateData() )
+	{
+		bool ok = false;
+		QString type;
+		QString name;
+		if ( m_testbedAddUI->m_rbEcho->isChecked() )
+		{
+			type = m_testbedAddUI->m_uniqueName->text();
+			name = QString::fromLatin1( "Echo Contact" );
+			ok = true;
+		}
+		if ( ok )
+			return a->addContact(type, /* FIXME: ? name, */ m, Kopete::Account::ChangeKABC );
+		else
+			return false;
+	}
+	return false;
+}
+
+bool TestbedAddContactPage::validateData()
+{
+    return true;
+}
+
+
+#include "testbedaddcontactpage.moc"
---
+++ protocols/qq/ui/testbedwebcamdialog.cpp
@@ -0,0 +1,85 @@
+/*
+    Kopete Testbed Protocol
+
+    Copyright (c) 2006 by Cláudio da Silveira Pinheiro   <taupter@gmail.com>
+    Kopete    (c) 2002-2006 by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
+#include "testbedwebcamdialog.h"
+#include <webcamwidget.h>
+#include "avdevice/videodevicepool.h"
+
+#include <q3frame.h>
+#include <qobject.h>
+#include <qwidget.h>
+#include <qlabel.h>
+#include <qlayout.h>
+#include <q3vbox.h>
+//Added by qt3to4:
+#include <QPixmap>
+#include <Q3VBoxLayout>
+#include <kdebug.h>
+#include <klocale.h>
+
+TestbedWebcamDialog::TestbedWebcamDialog( const QString &contactId, QWidget * parent, const char * name )
+: KDialogBase( KDialogBase::Plain, Qt::WDestructiveClose, parent, name, false, i18n( "Webcam for %1" ).arg( contactId ),
+                   KDialogBase::Close, KDialogBase::Close, true /*seperator*/ )
+{
+	setInitialSize( QSize(320,290) );
+	
+	setEscapeButton( KDialogBase::Close );
+//	QObject::connect( this, SIGNAL( closeClicked() ), this, SIGNAL( closingWebcamDialog() ) );
+
+	QWidget *page = plainPage();
+	setMainWidget(page);
+
+	Q3VBoxLayout *topLayout = new Q3VBoxLayout( page, 0, spacingHint() );	
+	mImageContainer = new Kopete::WebcamWidget( page );
+	mImageContainer->setMinimumSize(320,240);
+	mImageContainer->setText( i18n( "No webcam image received" ) );
+	mImageContainer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	topLayout->add( mImageContainer );
+	
+	show();
+	
+	mVideoDevicePool = Kopete::AV::VideoDevicePool::self();
+	mVideoDevicePool->open();
+	mVideoDevicePool->setSize(320, 240);
+	mVideoDevicePool->startCapturing();
+	mVideoDevicePool->getFrame();
+	mVideoDevicePool->getImage(&mImage);
+kdDebug() << "Just captured 1st frame" << endl;
+
+	mPixmap=QPixmap(320,240);
+	if (mPixmap.convertFromImage(mImage,0) == true)
+		mImageContainer->updatePixmap(mPixmap);
+	connect(&qtimer, SIGNAL(timeout()), this, SLOT(slotUpdateImage()) );
+	qtimer.start(0,FALSE);
+}
+
+TestbedWebcamDialog::~ TestbedWebcamDialog( )
+{
+	mVideoDevicePool->stopCapturing();
+	mVideoDevicePool->close();
+}
+
+void TestbedWebcamDialog::slotUpdateImage()
+{
+	mVideoDevicePool->getFrame();
+kdDebug() << "Getting image" << endl;
+	mVideoDevicePool->getImage(&mImage);
+kdDebug() << "BitBlitting image" << endl;
+	mImageContainer->updatePixmap( QPixmap( mImage ) );
+}
+
+
+#include "testbedwebcamdialog.moc"
