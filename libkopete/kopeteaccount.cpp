184c629b7aa7b2951dd4438256b69f6d678ef19f
Michel Hermier <michel.hermier@gmail.com>
2006-07-17 12:24:56 +0000

    Compile KActionMenu::kMenu => ::menu
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=563371

--- libkopete/kopeteaccount.cpp
+++ libkopete/kopeteaccount.cpp
@@ -370,12 +370,12 @@ KActionMenu * Account::actionMenu()
 #warning No icon shown, we should go away from QPixmap genered icons with overlays.
 	QString nick = myself()->property( Kopete::Global::Properties::self()->nickName()).value().toString();
 
-	menu->kMenu()->addTitle( myself()->onlineStatus().iconFor( myself() ),
+	menu->menu()->addTitle( myself()->onlineStatus().iconFor( myself() ),
 		nick.isNull() ? accountLabel() : i18n( "%2 <%1>", accountLabel(), nick )
 	);
 
 	OnlineStatusManager::self()->createAccountStatusActions(this, menu);
-	menu->kMenu()->addSeparator();
+	menu->menu()->addSeparator();
 
 	KAction *propertiesAction = new KAction( i18n("Properties"), 0, "actionAccountProperties" );
 	QObject::connect( propertiesAction, SIGNAL(triggered(bool)), this, SLOT( editAccount() ) );
