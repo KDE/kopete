39438c488e4fb3a4da12b1ed3a5e0127e7a78b7e
Laurent Montel <montel@kde.org>
2006-11-09 10:50:19 +0000

    Fix export
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=603532

--- protocols/oscar/liboscar/liboscar_export.h
+++ protocols/oscar/liboscar/liboscar_export.h
@@ -4,6 +4,20 @@
 #define LIBOSCAR_EXPORT_H
 
 #include <kdemacros.h>
+
+#if defined Q_OS_WIN
+
+#ifndef LIBOSCAR_EXPORT
+# ifdef MAKE_LIBOSCAR_LIB
+#  define LIBOSCAR_EXPORT  KDE_EXPORT
+# else
+#  define LIBOSCAR_EXPORT KDE_IMPORT
+# endif
+#endif
+
+#else
 #define LIBOSCAR_EXPORT KDE_EXPORT
+#endif
 
 #endif
+
