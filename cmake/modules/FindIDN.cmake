47844f0639c6d0a8c7d671faba3f8cfd318aff07
Will Stephenson <wstephenson@kde.org>
2006-12-19 18:38:45 +0000

    Make the libidn check, and general protocol error reporting a bit smarter
    svn path=/trunk/KDE/kdenetwork/cmake/modules/FindIDN.cmake; revision=614973

---
+++ cmake/modules/FindIDN.cmake
@@ -0,0 +1,43 @@
+# cmake macro to test IDN library
+
+# Copyright (c) 2006, Will Stephenson <wstephenson@kde.org>
+#
+#  IDN_FOUND - Test has found Decibel dependencies
+#  IDN_INCLUDES - Include needed for Decibel
+#  IDN_LIBRARIES - Libraries needed for Decibel
+#  IDN_DEFINITIONS - Compiler swithces required for using Decibel
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+INCLUDE(UsePkgConfig)
+
+PKGCONFIG(libidn _IDNIncDir _IDNLinkDir _IDNLinkFlags _IDNCflags)
+
+set(IDN_INCLUDE_DIR ${_IDNIncDir})
+
+set(IDN_DEFINITIONS ${_IDNCflags})
+set(IDN_INCLUDES ${IDN_INCLUDE_DIR} )
+
+FIND_LIBRARY(IDN_LIBRARY NAMES idn
+  PATHS
+  ${_IDNLinkDir} 
+  NO_DEFAULT_PATH 
+)
+
+if (IDN_INCLUDES AND IDN_LIBRARY)
+   set(IDN_FOUND TRUE)
+endif (IDN_INCLUDES AND IDN_LIBRARY)
+
+if (IDN_FOUND)
+  if (NOT IDN_FIND_QUIETLY)
+    message(STATUS "Found IDN: ${IDN_LIBRARY}")
+  endif (NOT IDN_FIND_QUIETLY)
+else (IDN_FOUND)
+  if (IDN_FIND_REQUIRED)
+    message(SEND_ERROR "Could NOT find IDN")
+  endif (IDN_FIND_REQUIRED)
+endif (IDN_FOUND)
+
+MARK_AS_ADVANCED(IDN_INCLUDE_DIR IDN_LIBRARY)
+
