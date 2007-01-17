457c1a738fd2ab7569c60066c6d0485e87b07b1f
Laurent Montel <montel@kde.org>
2007-01-17 18:42:44 +0000

    Fix connect signal/slot
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=624700

--- plugins/highlight/highlightpreferences.cpp
+++ plugins/highlight/highlightpreferences.cpp
@@ -52,7 +52,7 @@ HighlightPreferences::HighlightPreferences(QWidget *parent, const QStringList &a
 	m_config = new HighlightConfig;
 
 	connect(preferencesDialog.m_list , SIGNAL(currentItemChanged(QListWidgetItem*,QListWidgetItem*)) , this , SLOT(slotCurrentFilterChanged()));
-	connect(preferencesDialog.m_list , SIGNAL(doubleClicked ( QListViewItem *, const QPoint &, int )) , this , SLOT(slotRenameFilter()));
+	connect(preferencesDialog.m_list , SIGNAL(itemDoubleClicked(QListWidgetItem*)) , this , SLOT(slotRenameFilter()));
 	connect(preferencesDialog.m_add , SIGNAL(pressed()) , this , SLOT(slotAddFilter()));
 	connect(preferencesDialog.m_remove , SIGNAL(pressed()) , this , SLOT(slotRemoveFilter()));
 	connect(preferencesDialog.m_rename , SIGNAL(pressed()) , this , SLOT(slotRenameFilter()));
