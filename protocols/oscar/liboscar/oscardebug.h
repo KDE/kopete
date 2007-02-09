2a68dc2bac293fa5278b0a5bbca76d10a2196262
Matt Rogers <mattr@kde.org>
2007-02-09 04:58:32 +0000

    fix debug define
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=631782

--- protocols/oscar/liboscar/oscardebug.h
+++ protocols/oscar/liboscar/oscardebug.h
@@ -27,7 +27,7 @@
 
 //uncomment to get excessive amounts of debug info from
 //various places in the code
-//#define OSCAR_EXCEContactVE_DEBUG
+//#define OSCAR_EXCESSIVE_DEBUG
 
 //uncomment to get packet dumps in the debug output
 //#define OSCAR_PACKET_PARSING_DEBUG
