3eb2128cf1774d0f6b979e351969fa2a994325c8
Thorben Kröger <thorbenk@gmx.net>
2007-02-13 17:16:37 +0000

    Add a copyright notice. All of these files were added during the UI3 to UI4 conversion done about a year ago.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=633266

--- kopete/config/appearance/appearanceconfig_emoticons.h
+++ kopete/config/appearance/appearanceconfig_emoticons.h
@@ -1,3 +1,20 @@
+/*
+    appearanceconfig_emoticons.h
+
+    Copyright (c) 2006      by Thorben Kröger         <thorbenk@gmx.net>
+
+    Kopete    (c) 2002-2007  by the Kopete developers  <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This program is free software; you can redistribute it and/or modify  *
+    * it under the terms of the GNU General Public License as published by  *
+    * the Free Software Foundation; either version 2 of the License, or     *
+    * (at your option) any later version.                                   *
+    *                                                                       *
+    *************************************************************************
+*/
+
 #ifndef __APPEARANCECONFIG_EMOTICONS_H
 #define __APPEARANCECONFIG_EMOTICONS_H
 
