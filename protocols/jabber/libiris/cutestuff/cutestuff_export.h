5d59985d2ccf3c731f84b99b8a22b717c2f3726e
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-02-21 19:05:45 +0000

    who had the idea to add two foo_export.h for one lib? -> fixed exports for win32 here too
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=636052

--- protocols/jabber/libiris/cutestuff/cutestuff_export.h
+++ protocols/jabber/libiris/cutestuff/cutestuff_export.h
@@ -3,14 +3,16 @@
 
 #include <QtGlobal>
 
-#define CUTESTUFF_EXPORT Q_DECL_EXPORT
-
+#ifndef CUTESTUFF_EXPORT
+# ifdef Q_OS_WIN
+#  ifdef MAKE_IRIS_KOPETE_LIB
+#   define CUTESTUFF_EXPORT Q_DECL_EXPORT
+#  else
+#   define CUTESTUFF_EXPORT Q_DECL_IMPORT
+#  endif
+# else
+#  define CUTESTUFF_EXPORT Q_DECL_EXPORT
+# endif
 #endif
-#ifndef CUTESTUFF_EXPORT_H
-#define CUTESTUFF_EXPORT_H
-
-#include <QtGlobal>
-
-#define CUTESTUFF_EXPORT Q_DECL_EXPORT
 
 #endif
