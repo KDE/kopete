b651d13727a9f0037b75f8d103032bfe049c57dc
Isaac Clerencia <isaac@warp.es>
2007-06-19 17:53:47 +0000

    Fix problem related to QCA BIC
    QCA::SASL::Params user, pass and realm have been moved to a d-pointer, and
    now they must be accesed via some methods. 
    
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=677680

--- protocols/jabber/libiris/iris/xmpp-core/stream.cpp
+++ protocols/jabber/libiris/iris/xmpp-core/stream.cpp
@@ -1338,9 +1338,9 @@ void ClientStream::sasl_needParams(const QCA::SASL::Params& p)
 		d->sasl->setAuthzid(d->jid.bare());
 		//d->sasl->setAuthzid("infiniti.homelesshackers.org");
 	}*/
-	if(p.user || p.pass || p.realm) {
+	if(p.needUserName() || p.needPassword() || p.canSendRealm()) {
 		d->state = NeedParams;
-		needAuthParams(p.user, p.pass, p.realm);
+		needAuthParams(p.needUserName(), p.needPassword(), p.canSendRealm());
 	}
 	else
 		d->sasl->continueAfterParams();
