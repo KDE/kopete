3ed93be401b14e6560f9e4389a0b77b7a63c8016
Laurent Montel <montel@kde.org>
2007-10-11 21:35:38 +0000

    Q3Support--
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=724282

--- plugins/contactnotes/contactnotesedit.cpp
+++ plugins/contactnotes/contactnotesedit.cpp
@@ -16,7 +16,7 @@
 */
 
 #include <qlabel.h>
-#include <q3textedit.h>
+#include <QTextEdit>
 
 
 #include <klocale.h>
@@ -41,7 +41,7 @@ ContactNotesEdit::ContactNotesEdit(Kopete::MetaContact *m,ContactNotesPlugin *p)
 	w->setSpacing(KDialog::spacingHint());
 	m_label = new QLabel(i18n("Notes about %1:", m->displayName()) , w );
 	m_label->setObjectName( QLatin1String("m_label") );
-	m_linesEdit= new Q3TextEdit ( w , "m_linesEdit");
+	m_linesEdit= new QTextEdit ( w );
 
 	m_linesEdit->setText(p->notes(m));
 
