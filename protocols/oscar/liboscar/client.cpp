9444a0da6c211d34a80b3fbc5a7eb42743beeed0
Roman Jarosz <kedgedev@gmail.com>
2007-11-11 14:50:38 +0000

    Backport: Filter out Miranda's invisible check.
    CCBUG: 148035
    
    
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=735379

--- protocols/oscar/liboscar/client.cpp
+++ protocols/oscar/liboscar/client.cpp
@@ -564,6 +564,10 @@ void Client::receivedMessage( const Oscar::Message& msg )
 	}
 	else if ( ! msg.hasProperty( Oscar::Message::AutoResponse ) )
 	{
+		// Filter out miranda's invisible check
+		if ( msg.messageType() == 0x0004 && msg.textArray().isEmpty() )
+			return;
+
 		// let application handle it
 		kdDebug( OSCAR_RAW_DEBUG ) << k_funcinfo << "Emitting receivedMessage" << endl;
 		emit messageReceived( msg );
