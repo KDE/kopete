05e5d4a42b0e4ca73dc4ae11b31d6fc1e3a41c0e
Will Stephenson <wstephenson@kde.org>
2007-11-12 00:15:11 +0000

    Rewrite the identity status widget to use a more recognisable list widget containing the accounts, and a pushbutton to change display picture.
    I had some problems with this as Kopete::Identity is only superficially
    integrated with the rest of libkopete, so it's not possible to know a
    new account's real identity at the usual
    Kopete::AccountManager::accountRegistered() signal.  As a safe solution
    for 4.0 the identity status widget is hidden when this signal is
    received.  After 4.0, Kopete::Identity will need to be properly
    integrated.  
    
    Still to do is to connect the widgets up to display photo changes in the 
    config dialog and make sure display name changes are effective.
    
    
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=735548

--- kopete/identity/identitystatuswidget.cpp
+++ kopete/identity/identitystatuswidget.cpp
@@ -22,6 +22,7 @@
 #include <KIcon>
 #include <KMenu>
 #include <QTimeLine>
+#include <QToolTip>
 #include <QCursor>
 #include <QUrl>
 #include <QPalette>
@@ -30,6 +31,7 @@
 #include <kopeteaccountmanager.h>
 #include <kopetecontact.h>
 #include <kopeteprotocol.h>
+#include <kopetestdaction.h>
 #include <avatardialog.h>
 #include <KDebug>
 
@@ -42,6 +44,7 @@ public:
 	QString photoPath;
 	// Used to display changing nickname in red
 	QString lastNickName;
+	QHash<QListWidgetItem *,Kopete::Account *> accountHash;
 };
 
 IdentityStatusWidget::IdentityStatusWidget(Kopete::Identity *identity, QWidget *parent)
@@ -57,26 +60,27 @@ IdentityStatusWidget::IdentityStatusWidget(Kopete::Identity *identity, QWidget *
 			 this, SLOT(slotAnimate(qreal)) );
 
 	d->ui.setupUi(this);
+	d->ui.accounts->setContextMenuPolicy( Qt::CustomContextMenu );
 	QWidget::setVisible( false );
 
 	setIdentity(identity);
-	slotLoad();
 
 	// user input signals
+	connect( d->ui.accounts, SIGNAL(customContextMenuRequested(const QPoint &)),
+			this, SLOT(showAccountContextMenu(const QPoint &)) );
 	connect( d->ui.nickName, SIGNAL(editingFinished()), this, SLOT(slotSave()) );
 	connect( d->ui.nickName, SIGNAL(textChanged(QString)), this, SLOT(slotNickNameTextChanged(QString)) );
-	connect( d->ui.photo, SIGNAL(linkActivated(const QString&)), 
-			 this, SLOT(slotPhotoLinkActivated(const QString &)));
-	connect( d->ui.accounts, SIGNAL(linkActivated(const QString&)),
-			 this, SLOT(slotAccountLinkActivated(const QString &)));
-	connect( Kopete::AccountManager::self(),
-			SIGNAL(accountOnlineStatusChanged(Kopete::Account*, const Kopete::OnlineStatus&, const Kopete::OnlineStatus&)),
-			this, SLOT(slotUpdateAccountStatus()));
+	connect( d->ui.photo, SIGNAL(clicked()), 
+			 this, SLOT(slotPhotoClicked()));
+
+	connect( Kopete::AccountManager::self(), SIGNAL(accountRegistered(Kopete::Account*)),
+			this, SLOT(slotAccountRegistered(Kopete::Account*)));
+	connect( Kopete::AccountManager::self(), SIGNAL(accountUnregistered(const Kopete::Account*)),
+			this, SLOT(slotAccountUnregistered(const Kopete::Account*)));
 }
 
 IdentityStatusWidget::~IdentityStatusWidget()
 {
-	delete d->timeline;
 	delete d;
 }
 
@@ -84,7 +88,8 @@ void IdentityStatusWidget::setIdentity(Kopete::Identity *identity)
 {
 	if (d->identity)
 	{
-		disconnect( d->identity, SIGNAL(identityChanged(Kopete::Identity*)), this, SLOT(slotUpdateAccountStatus()));
+		// TODO: Handle identity changes
+		//disconnect( d->identity, SIGNAL(identityChanged(Kopete::Identity*)), this, SLOT(slotUpdateAccountStatus()));
 		slotSave();
 	}
 
@@ -93,7 +98,9 @@ void IdentityStatusWidget::setIdentity(Kopete::Identity *identity)
 
 	if (d->identity)
 	{
-		connect( d->identity, SIGNAL(identityChanged(Kopete::Identity*)), this, SLOT(slotUpdateAccountStatus()));
+		//TODO: Handle identity changes
+		//connect( d->identity, SIGNAL(identityChanged(Kopete::Identity*)), this, SLOT(slotUpdateAccountStatus()));
+		;
 	}
 }
 
@@ -118,8 +125,9 @@ void IdentityStatusWidget::slotAnimate(qreal amount)
 		return;
 	}
 	
-	if (amount == 1)
+	if (amount == 1.0)
 	{
+		layout()->setSizeConstraint( QLayout::SetDefaultConstraint );
 		setFixedHeight( sizeHint().height() );
 		d->ui.nickName->setFocus();
 		return;
@@ -134,40 +142,123 @@ void IdentityStatusWidget::slotAnimate(qreal amount)
 void IdentityStatusWidget::slotLoad()
 {
 	// clear
-	d->ui.photo->setText(QString("<a href=\"identity::getavatar\">%1</a>").arg(i18n("No Photo")));
 	d->ui.nickName->clear();
-	d->ui.identityStatus->clear();
-	d->ui.identityName->clear();
 	d->ui.accounts->clear();
+	d->accountHash.clear();
 
 	if (!d->identity)
 		return;
 
 	Kopete::Global::Properties *props = Kopete::Global::Properties::self();
-	
+
 	// photo
 	if (d->identity->hasProperty(props->photo().key()))
 	{
 		d->photoPath = d->identity->property(props->photo()).value().toString();
-		d->ui.photo->setText(QString("<a href=\"identity::getavatar\"><img src=\"%1\" width=48 height=48></a>")
-								.arg(d->photoPath));
-	}
+		d->ui.photo->setIcon( QIcon( d->identity->property(props->photo()).value().toString() ) );
+	} else {
+		d->ui.photo->setIcon( KIcon( "user" ) );
+    }
 
 	// nickname
 	if (d->identity->hasProperty(props->nickName().key()))
 	{
 		// Set lastNickName to make red highlighting works when editing the identity nickname
 		d->lastNickName = d->identity->property(props->nickName()).value().toString();
-
 		d->ui.nickName->setText( d->lastNickName );
 	}
 
 	d->ui.identityName->setText(d->identity->label());
 
-	//acounts
-	slotUpdateAccountStatus();
-	//TODO: online status
-	
+	foreach(Kopete::Account *a, d->identity->accounts()) {
+		addAccountItem( a );
+	}
+	if ( d->identity->accounts().isEmpty() ) {
+		QListWidgetItem * item = new QListWidgetItem( KIcon("configure" ), i18nc("Label to tell the user no accounts existed", "No accounts configured" ), d->ui.accounts );
+	}
+	resizeAccountListWidget();
+}
+
+void IdentityStatusWidget::slotAccountRegistered( Kopete::Account *account )
+{
+	// Kopete::Identity is poorly integrated as of Kopete 0.40.0.  As a result Kopete::Accounts are
+	// always created as belonging to the default Identity, which means that they are added to the
+	// wrong identity in the IdentityStatus widget.  To fix this properly it would be necessary to 
+	// initialise the identities before the accounts on startup and pass the identity into the
+	// account constructor.  I'm not making changes like that for KDE 4.0 so just hide this widget
+	// now.  See also slotAccountUnregistered()
+
+	// the following code can be reenabled when Accounts have the right Identity on
+	// accountRegistered()
+#if 0
+	addAccountItem( account );
+	resizeAccountListWidget();
+#else
+	QWidget::setVisible( false );
+#endif
+}
+
+void IdentityStatusWidget::slotAccountUnregistered( const Kopete::Account *account )
+{
+	QListWidgetItem * item = 0;
+
+	QHashIterator<QListWidgetItem*, Kopete::Account *> i( d->accountHash );
+	while ( i.hasNext() ) {
+		i.next();
+		Kopete::Account * candidate = i.value();
+		if ( candidate == account ) {
+			item = i.key();
+		}
+	}
+	if( !item )
+		return;
+	d->ui.accounts->takeItem( d->ui.accounts->row( item ) );
+	d->accountHash.remove( item );
+	delete item;
+	resizeAccountListWidget();
+}
+
+void IdentityStatusWidget::addAccountItem( Kopete::Account *account )
+{
+	// debug to diagnose if the account was created with the right identity.  see comment in
+	// slotAccountRegistered
+	//kDebug() << "Adding Account item for identity: " << ( account->identity() ? account->identity()->label() : "" ) << ", showing identity " << ( d->identity ? d->identity->label() : "" )<< " in widget.";
+	if ( !account || ( account->identity() != d->identity ) ) {
+		return;
+	}
+
+	connect( account->myself(),
+			SIGNAL(onlineStatusChanged(Kopete::Contact *, const Kopete::OnlineStatus &, const Kopete::OnlineStatus &)),
+			this, SLOT(slotAccountStatusIconChanged(Kopete::Contact *)) );
+
+	QListWidgetItem * item = new QListWidgetItem( account->accountIcon(), account->accountLabel(), d->ui.accounts );
+	item->setToolTip( account->myself()->toolTip() );
+	d->accountHash.insert( item, account );
+
+	slotAccountStatusIconChanged( account->myself() );
+}
+
+void IdentityStatusWidget::slotAccountStatusIconChanged( Kopete::Contact *contact )
+{
+	///kdDebug( 14000 ) << k_funcinfo << contact->property( Kopete::Global::Properties::self()->awayMessage() ).value() << endl;
+	Kopete::OnlineStatus status = contact->onlineStatus();
+	QListWidgetItem * item = 0;
+	QHashIterator<QListWidgetItem*, Kopete::Account *> i( d->accountHash );
+	while ( i.hasNext() ) {
+		i.next();
+		Kopete::Account * candidate = i.value();
+		if ( candidate == contact->account() ) {
+			item = i.key();
+		}
+	}
+	if( !item )
+		return;
+
+	QPixmap pm = status.iconFor( contact->account() );
+	if( pm.isNull() )
+		item->setIcon( KIconLoader::unknown() );
+	else
+		item->setIcon( QIcon( pm ) );
 }
 
 void IdentityStatusWidget::slotNickNameTextChanged(const QString &text)
@@ -175,12 +266,11 @@ void IdentityStatusWidget::slotNickNameTextChanged(const QString &text)
 	if ( d->lastNickName != text )
 	{
 		QPalette palette;
-		palette.setColor(d->ui.nickName->foregroundRole(), Qt::red);
+		palette.setColor( d->ui.nickName->foregroundRole(), QPalette::HighlightedText );
 		d->ui.nickName->setPalette(palette);
 	}
 	else
 	{
-		// If the nickname is the same, reset the palette
 		d->ui.nickName->setPalette(QPalette());
 	}
 
@@ -210,68 +300,41 @@ void IdentityStatusWidget::slotSave()
 		// and reset the palette
 		d->lastNickName = d->ui.nickName->text();
 		d->ui.nickName->setPalette(QPalette());
-	}
-
 	//TODO check what more to do
-
-}
-
-void IdentityStatusWidget::slotAccountLinkActivated(const QString &link)
-{
-	// Account links are in the form:
-	// accountmenu:protocolId:accountId
-	QStringList args = link.split(":");
-	if (args[0] != "accountmenu")
-		return;
-
-	Kopete::Account *a = Kopete::AccountManager::self()->findAccount(QUrl::fromPercentEncoding(args[1].toAscii()), 
-																	 QUrl::fromPercentEncoding(args[2].toAscii()));
-	if (!a)
-		return;
-
-	KActionMenu *actionMenu = a->actionMenu();
-	actionMenu->menu()->exec(QCursor::pos());
-	delete actionMenu;
-}
-
-void IdentityStatusWidget::slotPhotoLinkActivated(const QString &link)
-{
-	if (link == "identity::getavatar")
-	{
-		d->photoPath = Kopete::UI::AvatarDialog::getAvatar(this, d->photoPath);
-		slotSave();
-		slotLoad();
 	}
 }
 
-
-void IdentityStatusWidget::slotUpdateAccountStatus()
+void IdentityStatusWidget::showAccountContextMenu( const QPoint & point )
 {
-  if (!d->identity)
-  {
-    // no identity or already destroyed, ignore
-    return;
-  }
+	QListWidgetItem * item = d->ui.accounts->itemAt( point );
+	if ( item && !d->accountHash.isEmpty() ) {
+		Kopete::Account * account = d->accountHash[ item ];
+		if ( account ) {
+			KActionMenu *actionMenu = account->actionMenu();
+			actionMenu->menu()->exec( d->ui.accounts->mapToGlobal( point ) );
+			delete actionMenu;
+		}
+	}
+}
 
-  // Always clear text before changing it: otherwise icon changes are not reflected
-  d->ui.accounts->clear();
+void IdentityStatusWidget::slotPhotoClicked()
+{
+    d->photoPath = Kopete::UI::AvatarDialog::getAvatar(this, d->photoPath);
+    slotSave();
+    slotLoad();
+}
 
-  QString text("<qt>");
-  foreach(Kopete::Account *a, d->identity->accounts())
-  {
-	  Kopete::Contact *self = a->myself();
-	  QString onlineStatus = self ? self->onlineStatus().description() : i18n("Offline");
-	  text += i18nc( "Account tooltip information: <nobr>ICON <b>PROTOCOL:</b> NAME (<i>STATUS</i>)<br/>",
-				    "<nobr><a href=\"accountmenu:%2:%3\"><img src=\"kopete-account-icon:%2:%3\"> %1 (<i>%4</i>)</a><br/>",
-		a->accountLabel(), QString(QUrl::toPercentEncoding( a->protocol()->pluginId() )),
-		QString(QUrl::toPercentEncoding( a->accountId() )), onlineStatus );
-  }
-  
-  text += QLatin1String("</qt>");
-  d->ui.accounts->setText( text );
-
-  // Adding/removing accounts changes needed size
-  setFixedHeight( sizeHint().height() );
+void IdentityStatusWidget::resizeAccountListWidget()
+{
+	int frameWidth = d->ui.accounts->frameWidth();
+	int itemHeight = d->ui.accounts->itemDelegate()->sizeHint(
+			QStyleOptionViewItem(), d->ui.accounts->model()->index( 0, 0 ) ).height();
+	int itemCount = d->ui.accounts->count();
+	d->ui.accounts->setFixedHeight( 2 * frameWidth
+			+ itemHeight * ( itemCount ? itemCount : 1 ) );
+	layout()->invalidate();
+	setFixedHeight( sizeHint().height() );
+	//adjustSize();
 }
 
 #include "identitystatuswidget.moc"
--- kopete/identity/identitystatuswidget.h
+++ kopete/identity/identitystatuswidget.h
@@ -23,6 +23,8 @@
 
 namespace Kopete
 {
+class Account;
+class Contact;
 class Identity;
 }
 
@@ -55,16 +57,20 @@ private slots:
 	void slotLoad();
 	void slotSave();
 
-	void slotAccountLinkActivated(const QString &link);
-	void slotPhotoLinkActivated(const QString &link);
-	void slotUpdateAccountStatus();
+	void showAccountContextMenu( const QPoint & );
+	void slotPhotoClicked();
 	/**
 	 * @brief Show nickname in red when modified
 	 * @param text text changed in nickname textbox
 	 */
 	void slotNickNameTextChanged(const QString &text);
+	void slotAccountRegistered( Kopete::Account *account );
+	void slotAccountUnregistered( const Kopete::Account *account );
+	void slotAccountStatusIconChanged( Kopete::Contact *contact );
 
 private:
+	void addAccountItem( Kopete::Account *account );
+	void resizeAccountListWidget();
 	class Private;
 	Private *d;
 };
