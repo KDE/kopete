e0cefbcb5101a06eec5a862247ccd17b39836917
Will Stephenson <wstephenson@kde.org>
2007-11-24 23:24:20 +0000

    * Give the chat window splitter a sane default * Save and restore splitter position * Hide the XMLGUI-provided main toolbar by default * Show the chat related actions in a separate Chat Toolbar
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=741153

--- kopete/chatwindow/chatview.cpp
+++ kopete/chatwindow/chatview.cpp
@@ -77,6 +77,7 @@ public:
 	bool isActive;
 	bool sendInProgress;
 	bool visibleMembers;
+	QSplitter * splitter;
 };
 
 ChatView::ChatView( Kopete::ChatSession *mgr, ChatWindowPlugin *parent )
@@ -98,7 +99,7 @@ ChatView::ChatView( Kopete::ChatSession *mgr, ChatWindowPlugin *parent )
 	//FIXME: don't widgets start off hidden anyway?
 	hide();
 
-	QSplitter *splitter = new QSplitter( Qt::Vertical, vbox );
+	d->splitter = new QSplitter( Qt::Vertical, vbox );
 
 	//Create the view dock widget (KHTML Part), and set it to no docking (lock it in place)
 	m_messagePart = new ChatMessagePart( mgr , this );
@@ -106,10 +107,12 @@ ChatView::ChatView( Kopete::ChatSession *mgr, ChatWindowPlugin *parent )
 	//Create the bottom dock widget, with the edit area, statusbar and send button
 	m_editPart = new ChatTextEditPart( mgr, vbox );
 
-	splitter->addWidget(m_messagePart->view());
-	splitter->addWidget(m_editPart->widget());
-	splitter->setStretchFactor(0, 3);
-	splitter->setStretchFactor(1, 0);
+	d->splitter->addWidget(m_messagePart->view());
+	d->splitter->addWidget(m_editPart->widget());
+	d->splitter->setChildrenCollapsible( false );
+	QList<int> sizes;
+	sizes << 240 << 40;
+	d->splitter->setSizes( sizes ); 
 
 	// FIXME: is this used these days? it seems totally unnecessary
 	connect( editPart(), SIGNAL( toolbarToggled(bool)), this, SLOT(slotToggleRtfToolbar(bool)) );
@@ -744,8 +747,8 @@ void ChatView::messageSentSuccessfully()
 void ChatView::saveOptions()
 {
 	KSharedConfig::Ptr config = KGlobal::config();
-
-//	writeDockConfig ( config, QLatin1String( "ChatViewDock" ) );
+	KConfigGroup kopeteChatWindowMainWinSettings( config, ( msgManager()->form() == Kopete::ChatSession::Chatroom ? QLatin1String( "KopeteChatWindowGroupMode" ) : QLatin1String( "KopeteChatWindowIndividualMode" ) ) );
+	kopeteChatWindowMainWinSettings.writeEntry( QLatin1String("ChatViewSplitter"), d->splitter->saveState().toBase64() );
 	saveChatSettings();
 	config->sync();
 }
@@ -792,33 +795,11 @@ void ChatView::loadChatSettings()
 
 void ChatView::readOptions()
 {
-	KSharedConfig::Ptr config = KGlobal::config();
-
-	/** THIS IS BROKEN !!! */
-	//dockManager->readConfig ( config, QLatin1String("ChatViewDock") );
-#if 0
-	//Work-around to restore dock widget positions
-	config->setGroup( QLatin1String( "ChatViewDock" ) );
-
-	membersDockPosition = static_cast<K3DockWidget::DockPosition>(
-		config->readEntry( QLatin1String( "membersDockPosition" ), K3DockWidget::DockRight ) );
-
-	QString dockKey = QLatin1String( "viewDock" );
-	if ( d->visibleMembers )
-	{
-		if( membersDockPosition == K3DockWidget::DockLeft )
-			dockKey.prepend( QLatin1String( "membersDock," ) );
-		else if( membersDockPosition == K3DockWidget::DockRight )
-			dockKey.append( QLatin1String( ",membersDock" ) );
-	}
-
-	dockKey.append( QLatin1String( ",editDock:sepPos" ) );
+	KConfigGroup kopeteChatWindowMainWinSettings( KGlobal::config(), ( msgManager()->form() == Kopete::ChatSession::Chatroom ? QLatin1String( "KopeteChatWindowGroupMode" ) : QLatin1String( "KopeteChatWindowIndividualMode" ) ) );
 	//kDebug(14000) << "reading splitterpos from key: " << dockKey;
-	int splitterPos = config->readEntry( dockKey, 70 );
-	editDock->manualDock( viewDock, K3DockWidget::DockBottom, splitterPos );
-	viewDock->setDockSite( K3DockWidget::DockLeft | K3DockWidget::DockRight );
-	editDock->setEnableDocking( K3DockWidget::DockNone );
-#endif
+	QByteArray state;
+	state = kopeteChatWindowMainWinSettings.readEntry( QLatin1String("ChatViewSplitter"), state );
+	d->splitter->restoreState( QByteArray::fromBase64( state ) );
 }
 
 void ChatView::setActive( bool value )
