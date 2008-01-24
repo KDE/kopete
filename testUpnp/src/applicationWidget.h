5835bd800d3a1a116e8bbd473b58eb1134d7ea31
Bertrand Demay <bertranddemay@gmail.com>
2008-01-24 18:57:48 +0000

    Add licence.
    svn path=/branches/work/kopete/iup-isi-group/; revision=765835

--- testUpnp/src/applicationWidget.h
+++ testUpnp/src/applicationWidget.h
@@ -1,3 +1,22 @@
+/*
+    applicationWidget.h -
+
+    Copyright (c) 2007-2008 by Romain Castan      <romaincastan@gmail.com>
+    Copyright (c) 2007-2008 by Bertrand Demay     <bertranddemay@gmail.com>
+    Copyright (c) 2007-2008 by Julien Hubatzeck   <reineur31@gmail.com>
+    Copyright (c) 2007-2008 by Michel Saliba      <msalibaba@gmail.com>
+
+    Kopete    (c) 2002-2008 by the Kopete developers <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU Lesser General Public            *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
 
 #ifndef _APPLICATIONWIDGET_H_
 #define _APPLICATIONWIDGET_H_
--- testUpnp/src/tests/devicetest.h
+++ testUpnp/src/tests/devicetest.h
@@ -1,3 +1,20 @@
+/*
+    devicetest.h -
+
+    Copyright (c) 2007-2008 by Kevin Kin-foo     <>
+
+    Kopete    (c) 2002-2008 by the Kopete developers <kopete-devel@kde.org>
+
+    *************************************************************************
+    *                                                                       *
+    * This library is free software; you can redistribute it and/or         *
+    * modify it under the terms of the GNU Lesser General Public            *
+    * License as published by the Free Software Foundation; either          *
+    * version 2 of the License, or (at your option) any later version.      *
+    *                                                                       *
+    *************************************************************************
+*/
+
 #ifndef DEVICE_TEST_H
 #define DEVICE_TEST_H
 
