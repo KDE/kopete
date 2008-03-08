1116771511cc62b99d030ba51f2cb07a89c3d62f
Michel Saliba <msalibaba@gmail.com>
2008-03-08 10:23:54 +0000

    modifiying messengeraccount constructor
    svn path=/branches/work/kopete/iup-isi-group/; revision=783506

--- protocols/messenger/messengeraccount.cpp
+++ protocols/messenger/messengeraccount.cpp
@@ -1,6 +1,7 @@
 /*
  * messengeraccount.cpp - Windows Live Messenger Kopete Account.
  *
+ * Copyright (c) 2007 by Zhang Panyong <pyzhang@gmail.com>
  * Copyright (c) 2006 by Michaël Larouche <larouche@kde.org>
  * 
  * Kopete    (c) 2002-2003 by the Kopete developers  <kopete-devel@kde.org>
@@ -18,43 +19,301 @@
 
 // KDE includes
 #include <kaction.h>
+#include <kactionmenu.h>
+#include <kconfig.h>
 #include <kdebug.h>
-#include <klocale.h>
+#include <kinputdialog.h>
+#include <kmessagebox.h>
 #include <kmenu.h>
+#include <kstandarddirs.h>
+#include <kcodecs.h>
+#include <klocale.h>
+#include <kicon.h>
+#include <kconfiggroup.h>
 
+#include <QtCore/QFile>
+#include <QtCore/QRegExp>
+#include <QValidator>
+#include <QtGui/QImage>
+#include <QtCore/QList>
+#include <QtCore/QCryptographicHash>
+#include <QMessageBox>
+
+#include "messengercontact.h"
 // Kopete includes
 #include "kopetemetacontact.h"
 #include "kopeteonlinestatus.h"
 #include "kopetecontactlist.h"
-
-// Messenger includes
-#include "messengerprotocol.h"
-#include "messengercontact.h"
+#include "kopetegroup.h"
+#include "kopetepassword.h"
+#include "kopeteuiglobal.h"
+#include "kopeteglobal.h"
+#include "kopetechatsessionmanager.h"
 
 MessengerAccount::MessengerAccount(MessengerProtocol *protocol, const QString &accountId)
  : Kopete::PasswordedAccount(protocol, accountId.toLower(), false)
 {
 	setMyself( new MessengerContact(this, accountId, Kopete::ContactList::self()->myself()) );
+
+	m_openInboxAction = new KAction( KIcon("mail"), i18n( "Open Inbo&x..." ), this );
+        //, "m_openInboxAction" );
+	QObject::connect( m_openInboxAction, SIGNAL(triggered(bool)), this, SLOT(slotOpenInbox()) );
+
+	m_changeDNAction = new KAction( KIcon("help-contents"),i18n( "&Change Display Name..." ), this );
+        //, "renameAction" );
+	QObject::connect( m_changeDNAction, SIGNAL(triggered(bool)), this, SLOT(slotChangePublicName()) );
+
+	m_editUserInfoAction = new KAction( KIcon("help-contents"),i18n( "&Edit User Info" ), this );
+        //, "editUserInfoAction" );
+	QObject::connect( m_editUserInfoAction, SIGNAL(triggered(bool)), this, SLOT(slotUserInfo()) );
+
+	m_startChatAction = new KAction( KIcon("mail"), i18n( "&Start Chat..." ), this );
+        //, "startChatAction" );
+	QObject::connect( m_startChatAction, SIGNAL(triggered(bool)), this, SLOT(slotStartChat()) );
+
+	//create a MessengerClient
+	m_messengerClient = new Papillon::Client(new Papillon::QtConnector(this),this);
+
+// 	KConfigGroup *config= configGroup();
+// 	m_blockList   = config->readEntry(  "blockList", QStringList() ) ;
+// 	m_allowList   = config->readEntry(  "allowList", QStringList() ) ;
+// 	m_reverseList = config->readEntry(  "reverseList", QStringList()  ) ;
+
+	// getting list of contacts order by type
+	Papillon::ContactList *cl = new Papillon::ContactList(m_messengerClient);
+	m_blockList = cl->blockList();
+	m_allowList = cl->allowList();
+	m_reverseList = cl->reverseList();
+
+// 	m_messengerClient->ContactList()->blockList(&m_blockList, "Block");
+// 	m_messengerClient->ContactList()->allowList(&m_allowList, "Allow");
+// 	m_messengerClient->ContactList()->reverseList(&m_reverseList, "Reverse");
+
+	// Set the client Id for the myself contact.  It sets what Messenger feature we support.
+	m_clientId = MessengerProtocol::MessengerC4 | MessengerProtocol::InkFormatGIF | MessengerProtocol::SupportMultiPacketMessaging;
+
+	//m_initialPresence = 
 }
 
 MessengerAccount::~MessengerAccount()
 {
-	
+	//disconnect ( Kopete::Account::Manual );
+	delete m_messengerClient;
 }
 
-void MessengerAccount::fillActionMenu( KActionMenu *actionMenu )
+QString MessengerAccount::serverName() const
 {
-	Kopete::Account::fillActionMenu( actionMenu );
+	return configGroup()->readEntry(  "serverName" , MESSENGER_DEFAULT_SERVER );
 }
+
+int MessengerAccount::serverPort() const
+{
+	return configGroup()->readEntry(  "serverPort" , MESSENGER_DEFAULT_PORT );
+}
+
+bool MessengerAccount::useHttpMethod() const
+{
+	return configGroup()->readEntry(  "useHttpMethod" , false );
+}
+
+QString MessengerAccount::myselfClientId() const
+{
+	return QString::number(m_clientId, 10);
+}
+
+QString MessengerAccount::pictureUrl()
+{
+	return m_pictureFilename;
+}
+
+void MessengerAccount::setPictureUrl(const QString &url)
+{
+	m_pictureFilename = url;
+}
+
+QString MessengerAccount::pictureObject()
+{
+	if(m_pictureObj.isNull())
+		resetPictureObject(true); //silent=true to keep infinite loop away
+	return m_pictureObj;
+}
+
+void MessengerAccount::resetPictureObject(bool silent)
+{
+	QString old=m_pictureObj;
+
+	if(!configGroup()->readEntry("exportCustomPicture", false))
+	{
+		m_pictureObj="";
+		myself()->removeProperty( Kopete::Global::Properties::self()->photo() );
+	}
+	else
+	{
+		QFile pictFile( pictureUrl() );
+		if (!pictFile.open(QIODevice::ReadOnly))
+		{
+			kDebug(14140) << "Could not open avatar picture.";
+
+			m_pictureObj="";
+			myself()->removeProperty( Kopete::Global::Properties::self()->photo() );
+		}
+		else
+		{
+			QByteArray ar = pictFile.readAll();
+
+			QByteArray sha1d = QCryptographicHash::hash(ar, QCryptographicHash::Sha1).toBase64();
+
+			QString size = QString::number( pictFile.size() );
+			QString all = "Creator"+accountId()+"Size"+size+"Type3Locationkopete.tmpFriendlyAAA=SHA1D"+ sha1d;
+			m_pictureObj="<msnobj Creator=\"" + accountId() + "\" Size=\"" + size  + "\" Type=\"3\" Location=\"kopete.tmp\" Friendly=\"AAA=\" SHA1D=\""+sha1d+"\" SHA1C=\""+
+				QString( QCryptographicHash::hash(all.toUtf8(), QCryptographicHash::Sha1).toBase64() )  +"\"/>";
+			myself()->setProperty( Kopete::Global::Properties::self()->photo(), pictureUrl() );
+		}
+	}
+/*
+	if( old != m_pictureObj && isConnected() && m_notifySocket && !silent )
+	{
+		kDebug(14140) << "Changing avatar(and status) on server";
+		//update the msn pict
+		m_notifySocket->setStatus( myself()->onlineStatus() );
+	}*/
+}
+
+// KActionMenu *MessengerAccount::actionMenu()
+// {
+// 	KActionMenu *m_actionMenu = Kopete::Account::actionMenu();
+// 
+// 	if(isConnected())
+// 	{
+// 		m_openInboxAction->setEnabled( true );
+// 		m_startChatAction->setEnabled( true );
+// 		m_changeDNAction->setEnabled( true );
+// 
+// 	}
+// 	else
+// 	{
+// 		m_openInboxAction->setEnabled( false );
+// 		m_startChatAction->setEnabled( false );
+// 		m_changeDNAction->setEnabled( false );
+// 	}
+// 	m_actionMenu->addSeperator();
+// 
+// 	m_actionMenu->addAction( m_changeDNAction );
+// 	m_actionMenu->addAction( m_startChatAction );
+// 	m_actionMenu->addAction( m_openInboxAction );
+// 
+// #if defined MESSENGER_DEBUG
+// 	KActionMenu *debugMenu = new KActionMenu( "Debug", this );
+// 
+// 	KAction *rawCmd = new KAction( i18n( "Send Raw C&ommand..." ), this );
+//         //, "m_debugRawCommand" );
+// 	QObject::connect( rawCmd, SIGNAL(triggered()), this, SLOT(slotDebugRawCommand()) );
+// 	debugMenu->addAction(rawCmd);
+// 
+// 	m_actionMenu->addSeparator();
+// 	m_actionMenu->addAction( debugMenu );
+// #endif
+// 
+// 	return m_actionMenu;
+// }
+
+void MessengerAccount::createNotificationServer( const QString &host, uint port )
+{
+// 	if(m_notifySocket) //we are switching from one to another notifysocket.
+// 	{
+// 		//remove every slots to that socket, so we won't delete receive signals
+// 		// from the old socket thinking they are from the new one
+// 		QObject::disconnect( m_notifySocket , 0, this, 0 );
+// 		m_notifySocket->deleteLater(); //be sure it will be deleted
+// 		m_notifySocket=0L;
+// 	}
+// 
+// 	m_msgHandle.clear();
+// 
+// 	myself()->setOnlineStatus( MessengerProtocol::protocol()->CNT );
+// 
+// 
+// 	m_notifySocket = new MessengerNotifySocket( this, accountId() , m_password);
+// 	m_notifySocket->setUseHttpMethod( useHttpMethod() );
+// 
+// 	QObject::connect( m_notifySocket, SIGNAL( groupAdded( const QString&, const QString& ) ),
+// 		SLOT( slotGroupAdded( const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( groupRenamed( const QString&, const QString& ) ),
+// 		SLOT( slotGroupRenamed( const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( groupListed( const QString&, const QString& ) ),
+// 		SLOT( slotGroupAdded( const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( groupRemoved( const QString& ) ),
+// 		SLOT( slotGroupRemoved( const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( contactList(const QString&, const QString&, const QString&, uint, const QString& ) ),
+// 		SLOT( slotContactListed(const QString&, const QString&, const QString&, uint, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL(contactAdded(const QString&, const QString&, const QString&, const QString&, const QString& ) ),
+// 		SLOT( slotContactAdded(const QString&, const QString&, const QString&, const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( contactRemoved(const QString&, const QString&, const QString&, const QString& ) ),
+// 		SLOT( slotContactRemoved(const QString&, const QString&, const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( statusChanged( const Kopete::OnlineStatus & ) ),
+// 		SLOT( slotStatusChanged( const Kopete::OnlineStatus & ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( invitedToChat( const QString&, const QString&, const QString&, const QString&, const QString& ) ),
+// 		SLOT( slotCreateChat( const QString&, const QString&, const QString&, const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( startChat( const QString&, const QString& ) ),
+// 		SLOT( slotCreateChat( const QString&, const QString& ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( socketClosed() ),
+// 		SLOT( slotNotifySocketClosed() ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( newContactList() ),
+// 		SLOT( slotNewContactList() ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( receivedNotificationServer(const QString&, uint )  ),
+// 		SLOT(createNotificationServer(const QString&, uint ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( hotmailSeted( bool ) ),
+// 		m_openInboxAction, SLOT( setEnabled( bool ) ) );
+// 	QObject::connect( m_notifySocket, SIGNAL( errorMessage(int, const QString& ) ),
+// 		SLOT( slotErrorMessageReceived(int, const QString& ) ) );
+// 
+// 	m_notifySocket->setStatus( m_connectstatus );
+// 	m_notifySocket->connect(host, port);
+}
+
+
+
+
+
+
+
 
 void MessengerAccount::connectWithPassword(const QString &password)
 {
-	
+	//kDebug(MESSENGER_DEBUG) << k_funcinfo << "connect with password"<<endl;
+	if(password.isNull())
+	{
+		//kDebug(MESSENGER_DEBUG ) << k_funcinfo <<"Abort connection (null password)"  << endl;
+		//return;
+	}
+
+	if(isConnected())
+	{
+		//kDebug(MESSENGER_DEBUG) << k_funcinfo << "Ignore connect request(already connected)"<<endl;
+		//return;
+	}
+
+	if ( contacts().count() <= 1 )
+	{
+		// Maybe the contact list.xml has been removed, and the serial number not updated
+		// ( the 1 is for the myself contact )
+		configGroup()->writeEntry( "serial", 0 );
+	}
+	// TODO create an action list in private of the account class
+	m_openInboxAction->setEnabled( false );
+
+	QString server = configGroup()->readEntry( "serverName", MESSENGER_DEFAULT_SERVER );
+	int port = configGroup()->readEntry( "serverPort", MESSENGER_DEFAULT_PORT );
+	// TODO do the connection to the server with the client
+// 	d->client->setServer(&server, port);
+// 	d->client->userContact()->setLoginInformation( accountId(), password );
+// 	d->client->connectToServer();
 }
 
 void MessengerAccount::disconnect()
 {
-	
+	//kDebug(MESSENGER_DEBUG) << k_funcinfo << "attempt to set status offline"<<endl;
+	//d->client->disconnectFromServer();
 }
 
 void MessengerAccount::setOnlineStatus(const Kopete::OnlineStatus& status, const Kopete::StatusMessage &reason)
@@ -64,12 +323,99 @@ void MessengerAccount::setOnlineStatus(const Kopete::OnlineStatus& status, const
 
 void MessengerAccount::setStatusMessage(const Kopete::StatusMessage &statusMessage)
 {
-	
+	myself()->setStatusMessage(statusMessage);
+	//d->client->setStatusMessage(statusMessage);
 }
 
 bool MessengerAccount::createContact(const QString &contactId, Kopete::MetaContact *parentMetaContact)
 {
-	return false;
+	if ( !parentMetaContact->isTemporary())// && m_notifySocket)
+	{
+		//m_addWizard_metaContact = parentMetaContact;
+
+		// TODO: finish contact add
+		//addContactServerside(contactId, metaContact->groups());
+		// FIXME: Find out if this contact was really added or not!
+		return true;
+	}
+	else
+	{
+		// This is a temporary contact.		
+		MessengerContact *newContact = new MessengerContact( this, contactId, parentMetaContact );
+		//TODO define a method to make deleted a contact
+		//newContact->setDeleted(true);
+		return true;
+	}
+return 0;
+}
+
+// void MessengerContact::slotUserInfo()
+// {
+// 	myself()->slotUserInfo();
+// }
+ 
+void MessengerAccount::setPublicName( const QString &publicName )
+{
+// 	if ( d->client )
+// 	{
+// 		d->client->changePublicName( publicName, QString() );
+// 	}
+}
+
+// MessengerClient * MessengerAccount::client()
+// {
+// 	return d->client;
+// }
+
+void MessengerAccount::slotStartChat()
+{
+	bool ok;
+	QString handle ;
+
+	handle = KInputDialog::getText( i18n( "Start Chat - Messenger Plugin" ),
+		i18n( "Please enter the email address of the person with whom you want to chat:" ), QString(), &ok ).toLower();
+	if ( ok )
+	{
+// 		if ( MessengerProtocol::validContactId( handle ) )
+// 		{
+// 			if ( !contacts()[ handle ] )
+// 				addContact( handle, handle, 0L, Kopete::Account::Temporary );
+// 
+// 			contacts()[ handle ]->execute();
+// 		}
+// 		else
+// 		{
+// 			KMessageBox::queuedMessageBox( Kopete::UI::Global::mainWidget(), KMessageBox::Sorry,
+// 				i18n( "<qt>You must enter a valid email address.</qt>" ), i18n( "Messenger Plugin" ) );
+// 		}
+	}
+}
+
+void MessengerAccount::slotChangePublicName()
+{
+// 	if ( !isConnected() )
+// 	{
+// 		return;
+// 	}
+
+	bool ok;
+	QString name = KInputDialog::getText( i18n( "Change Display Name - Messenger Plugin" ),
+			i18n( "Enter the new display name by which you want to be visible to your friends on Messenger:" ),
+			myself()->property( Kopete::Global::Properties::self()->nickName()).value().toString(), &ok );
+
+	if ( ok )
+	{
+		if ( name.length() > 387 )
+		{
+			KMessageBox::error( Kopete::UI::Global::mainWidget(),
+					i18n( "<qt>The display name you entered is too long. Please use a shorter name.\n"
+						"Your display name has <b>not</b> been changed.</qt>" ),
+					i18n( "Change Display Name - Messenger Plugin" ) );
+			return;
+		}
+		setPublicName( name );
+	}
+	//return d->client;
 }
 
 #include "messengeraccount.moc"
--- protocols/messenger/messengeraccount.h
+++ protocols/messenger/messengeraccount.h
@@ -18,10 +18,28 @@
 #define MESSENGERACCOUNT_H
 
 #include <kopetepasswordedaccount.h>
+#include "messengerprotocol.h"
+#include "kaction.h"
+#include "Papillon/Client"
+#include "Papillon/ContactList"
+#include "Papillon/QtConnector"
+#include <QStringList>
 
+#define MESSENGER_DEFAULT_PORT	1863
+#define MESSENGER_DEFAULT_SERVER	"muser.messenger.hotmail.com"
 
 class KActionMenu;
 
+namespace Papillon
+{
+    class Base;
+    class Contact;
+    class Client;
+    class Global;
+    class Presence;
+    class ContactList;
+    class QtConnector;
+}
 namespace Kopete 
 { 
 	class MetaContact;
@@ -30,29 +48,108 @@ namespace Kopete
 
 class MessengerProtocol;
 
-/**
- * @author Michaël Larouche <larouche@kde.org>
- */
+class MessengerAccountPrivate;
+
 class MessengerAccount : public Kopete::PasswordedAccount
 {
 	Q_OBJECT
 public:
-	MessengerAccount(MessengerProtocol *parent, const QString &accountId);
+	MessengerAccount(MessengerProtocol *protocol, const QString &accountId);
 	~MessengerAccount();
 
-	virtual void fillActionMenu( KActionMenu *actionMenu );
+	/* Fills the menu for this account. */
+	//virtual void fillActionMenu( KActionMenu *actionMenu );
+
+	/* Return the resource of the client */
+	QString serverName () const;
+	int serverPort () const;
+
+	/* to get the protocol from the account */
+	MessengerProtocol *protocol () const
+	{
+		return m_protocol;
+	}
+
+	Papillon::Client *client () const
+	{
+		return m_messengerClient;
+	}
+
+	/* Tells the user to connect first before they can do whatever it is
+	 * that they want to do. */
+	void errorConnectFirst ();
+
+	/* Tells the user that the connection was lost while we waited for
+	 * an answer of him. */
+	void errorConnectionLost ();
+
+	/*
+	 * called when the account is removed in the config ui */
+	//virtual bool removeAccount();
+
+	 /*
+	 * picture */
+	QString pictureUrl();
+	void setPictureUrl(const QString &url);
+	QString pictureObject();
+	void resetPictureObject(bool silent=false);
+
+
+	bool useHttpMethod() const;
+	QString myselfClientId() const;
+	void setPublicName( const QString &publicName );
+
+	//Actions
+	KAction * m_openInboxAction;
+	KAction * m_changeDNAction;
+	KAction * m_editUserInfoAction;
+	KAction * m_startChatAction;
+
+	Kopete::OnlineStatus m_connectstatus;
+	QStringList m_msgHandle;
+	bool m_newContactList;
+	uint m_clientId;
+
+	// server data
+	QList<Papillon::Contact*> m_allowList;
+	QList<Papillon::Contact*> m_blockList;
+	QList<Papillon::Contact*> m_reverseList;
 
 public slots:
-	virtual void connectWithPassword(const QString &password);
-	virtual void disconnect();
 	
+	/* Connects to the server. */
+	void connectWithPassword(const QString &password);
+	/* Disconnects from the server. */
+	void disconnect();
+	/* Disconnect with a reason */
+	//void disconnect ( Kopete::Account::DisconnectReason reason );
+	/* Disconnect with a reason, and status */
+	//void disconnect( Kopete::Account::DisconnectReason reason, XMPP::Status &status );
+	/* Reimplemented from Kopete::Account */
 	virtual void setOnlineStatus(const Kopete::OnlineStatus& status, const Kopete::StatusMessage &reason = Kopete::StatusMessage());
+
 	virtual void setStatusMessage(const Kopete::StatusMessage &statusMessage);
-	
+
+
+	void slotChangePublicName();
+	void slotStartChat();
+private slots:
+      /**
+	 * When the dispatch server sends us the notification server to use.
+	 */
+	void createNotificationServer( const QString &host, uint port );
 protected:
 	virtual bool createContact(const QString &contactId, Kopete::MetaContact *parentMetaContact);
 	
 private:
-	
+	//MessengerNotifySocket *m_notifySocket;
+	MessengerProtocol *m_protocol;
+	// backend for this account
+	QString m_pictureObj; //a cache of the <msnobj>
+	QString m_pictureFilename; // the picture filename.
+	Papillon::Client * m_messengerClient;
+
+	/* Initial presence to set after connecting. */
+	//Papillon::Presence::Status m_initialPresence;
 };
 #endif
