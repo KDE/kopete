328c0986881d9a6d475205be563ba7c25ae6ef7b
Michel Saliba <msalibaba@gmail.com>
2008-03-08 18:23:02 +0000

    adding implementations of methods of messengercontact
    svn path=/branches/work/kopete/iup-isi-group/; revision=783569

--- protocols/messenger/messengercontact.cpp
+++ protocols/messenger/messengercontact.cpp
@@ -1,6 +1,8 @@
 /*
  * messengercontact.cpp - Windows Live Messenger Kopete Contact.
  *
+ * Copyright (c) 2007 by Zhang Panyong <pyzhang@gmail.com>
+ * 
  * Copyright (c) 2006 by Michaël Larouche <larouche@kde.org>
  * 
  * Kopete    (c) 2002-2003 by the Kopete developers  <kopete-devel@kde.org>
@@ -18,11 +20,25 @@
 
 // KDE includes
 #include <kaction.h>
+#include <kdebug.h>
+#include <kfiledialog.h>
+#include <klineedit.h>
+#include <klocale.h>
+#include <kstandarddirs.h>
+#include <kmessagebox.h>
+#include <ktoolinvocation.h>
+#include <ktemporaryfile.h>
+#include <kconfig.h>
+#include <kglobal.h>
+#include <qregexp.h>
+#include <kio/job.h>
+#include <kdialog.h>
+#include <kicon.h>
 
 // Kopete includes
 #include "kopetechatsessionmanager.h"
-#include "kopetechatsession.h"
-#include "kopetemetacontact.h"
+// #include "kopetechatsession.h"
+// #include "kopetemetacontact.h"
 
 // Messenger includes
 #include "messengeraccount.h"
@@ -30,8 +46,14 @@
 MessengerContact::MessengerContact(MessengerAccount *account, const QString &contactId, Kopete::MetaContact *parent)
  : Kopete::Contact(account, contactId, parent)
 {
-}
 
+}
+MessengerContact::~MessengerContact()
+{}
+void MessengerContact::sync(unsigned int)
+{}
+void MessengerContact::deleteContact()
+{}
 bool MessengerContact::isReachable()
 {
 	return false;
@@ -39,17 +61,124 @@ bool MessengerContact::isReachable()
 
 void MessengerContact::serialize(QMap< QString, QString >& serializedData, QMap< QString, QString >& addressBookData)
 {
-	
+	// Contact id and display name are already set for us, only add the rest
 }
 
 QList<KAction *> *MessengerContact::customContextMenuActions()
 {
+	QList<KAction*> *m_actionCollection = new QList<KAction*>;
+
+	// Block/unblock Contact
+	QString blockLabel = i18n( "Unblock User" );// = isBlocked() ? i18n( "Unblock User" ) : i18n( "Block User" );
+	if( !actionBlock )
+	{
+		actionBlock = new KAction( KIcon("messenger_blocked"), blockLabel, this );
+                //, "actionBlock" );
+		connect( actionBlock, SIGNAL(triggered(bool)), this, SLOT( slotBlockUser()) );
+
+		//show profile
+		actionShowProfile = new KAction( i18n("View User's Live Profile"), this );
+                //, "actionShowProfile" );
+		connect( actionBlock, SIGNAL(triggered(bool)), this, SLOT(slotShowProfile()) );
+
+		// Send mail (only available if it is an hotmail account)
+		actionSendMail = new KAction( KIcon("mail"), i18n("Send Email..."), this );
+                //, "actionSendMail" );
+		connect( actionSendMail, SIGNAL(triggered(bool)), this, SLOT(slotSendMail()) );
+
+		// Invite to receive webcam
+		actionWebcamReceive = new KAction( KIcon("webcamreceive"), i18n( "View Contact's Webcam" ), this );
+                //, "msnWebcamReceive" ) ;
+		connect( actionWebcamReceive, SIGNAL(triggered(bool)), this, SLOT(slotWebcamReceive()) );
+
+		//Send webcam action
+		actionWebcamSend = new KAction( KIcon("webcamsend"), i18n( "Send Webcam" ), this );
+                //, "msnWebcamSend" ) ;
+		connect( actionWebcamSend, SIGNAL(triggered(bool)), this, SLOT(slotWebcamSend()) );
+	}
+	else
+	{
+		actionBlock->setText( blockLabel );
+	}
+
+	//actionSendMail->setEnabled( static_cast<MessengerAccount*>(account())->isLivemail());
+
+	m_actionCollection->append( actionBlock );
+	m_actionCollection->append( actionShowProfile );
+	m_actionCollection->append( actionSendMail );
+	m_actionCollection->append( actionWebcamReceive );
+	m_actionCollection->append( actionWebcamSend );
+
+
+	return m_actionCollection;
+
 	return 0;
 }
 
 Kopete::ChatSession *MessengerContact::manager(CanCreateFlags canCreate)
 {
 	return 0;
+}
+
+void MessengerContact::slotUserInfo()
+{
+// 	m_infoWidget = new MessengerUserInfoWidget( Kopete::UI::Global::mainWidget() );
+// 	QObject::connect( m_infoWidget, SIGNAL( finished() ), this, SLOT( closeUserInfoDialog() ) );
+// 	m_infoWidget->setContact( this );
+// 	m_infoWidget->show();
+}
+
+void MessengerContact::closeUserInfoDialog()
+{
+// 	QObject::disconnect( this, 0, m_infoWidget, 0 );
+// 	m_infoWidget->delayedDestruct();
+// 	m_infoWidget = 0L;
+}
+
+void MessengerContact::sendFile( const KUrl &sourceURL, const QString &altFileName, uint /*fileSize*/ )
+{
+	QString filePath;
+
+	//If the file location is null, then get it from a file open dialog
+	if( !sourceURL.isValid() )
+	{
+		filePath = KFileDialog::getOpenFileName( KUrl(), "*", 0l  , i18n( "Kopete File Transfer" ));
+	}
+	else
+	{
+		filePath = sourceURL.path(KUrl::RemoveTrailingSlash);
+	}
+
+	//kDebug(14140) << "MessengerContact::sendFile: File chosen to send:" << fileName << endl;
+
+	if ( filePath.isEmpty() )
+	{
+// 		kDebug(MESSENGER_DEBUG) << "files empty, assuming cancel" << endl;
+// 		return;
+	}
+
+	//kDebug(MESSENGER_DEBUG) << "files: '" << files << "' " << endl;
+
+	//TODO Send the file
+	//mAccount->engine()->sendFiles(filePath, altFileName, fileSize);
+}
+
+void MessengerContact::slotUserProfile()
+{   
+	//kDebug(MESSENGER_DEBUG) << k_funcinfo << endl;
+
+	QString profileSiteString = QLatin1String("http://spaces.live.com/profile.aspx?mem=");// + userId();
+	KToolInvocation::invokeBrowser( profileSiteString );
+} 
+
+void MessengerContact::slotBlockUser()
+{
+
+}
+
+void MessengerContact::slotSendMail()
+{
+
 }
 
 #include "messengercontact.moc"
--- protocols/messenger/messengercontact.h
+++ protocols/messenger/messengercontact.h
@@ -19,28 +19,83 @@
 
 #include <QMap>
 #include <QList>
-
+#include <kaction.h>
 #include <kopetecontact.h>
+#include <kopetechatsession.h>
+#include <kopetemetacontact.h>
+//#include <kopeteonlinestatus.h>
+#include <kopetegroup.h>
+#include "messengeraccount.h"
+#include "messengerprotocol.h"
+//#include "ui/messengeruserinfowidget.h"
 
-class KAction;
-
-namespace Kopete
-{
-	class ChatSession;
-	class MetaContact;
-}
-class MessengerAccount;
+// namespace Kopete
+// {
+// 	class ChatSession;
+// 	class MetaContact;
+// }
+//class MessengerAccount;
 
 class MessengerContact : public Kopete::Contact
 {
 	Q_OBJECT
 public:
 	MessengerContact(MessengerAccount *account, const QString &contactId, Kopete::MetaContact *parent);
+	~MessengerContact();
 
 	virtual bool isReachable();
-    virtual void serialize(QMap< QString, QString >& serializedData, QMap< QString, QString >& addressBookData);
+	virtual void serialize(QMap< QString, QString >& serializedData, QMap< QString, QString >& addressBookData);
 	
 	virtual QList<KAction *> *customContextMenuActions();
 	virtual Kopete::ChatSession *manager( CanCreateFlags canCreate = CannotCreate );
+	void closeUserInfoDialog();
+	
+	//bool isBlocked();
+
+	QString guid();
+	QString phoneHome();
+	QString phoneWork();
+	QString phoneMobile();
+
+public slots:
+	virtual void slotUserInfo();
+	virtual void deleteContact();
+	virtual void sendFile( const KUrl &sourceURL = KUrl(),
+						   const QString &fileName = QString(), uint fileSize = 0L );
+	void slotUserProfile();
+	void slotBlockUser();
+	void slotSendMail();
+
+	virtual void sync( unsigned int cvhanged= 0xff);
+private:
+	QMap<QString, Kopete::Group *> m_serverGroups;
+	//MessengerUserInfoWidget *m_infoWidget;
+
+	bool m_blocked;
+	bool m_allowed;
+	bool m_deleted;
+	bool m_reversed;
+	bool m_moving;
+	bool m_phone_mob;
+	
+	uint m_clientFlags;
+
+	QString m_phoneHome;
+	QString m_phoneWork;
+	QString m_phoneMobile;
+
+
+	KAction *actionBlock;
+	KAction *actionShowProfile;
+	KAction *actionSendMail;
+	KAction *actionWebcamReceive;
+	KAction *actionWebcamSend;
+
+	QString m_obj; //the MessengerObject
+
+	//Kopete::OnlineStatus m_currentStatus;
+
+	//MSNProtocol::deserializeContact need to acess some contact insternals
+	friend class MessengerProtocol;
 };
 #endif
