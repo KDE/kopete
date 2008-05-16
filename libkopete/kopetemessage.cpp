4302d51fca7cf5738a222f22fcf6d833e735228d
Detlev Casanova <detlev.casanova@gmail.com>
2008-05-16 07:49:03 +0000

    BUG: 145307 CCBUG: 145307  * Keeps end of line in message part when sending a message which contains several lines.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=808260

--- libkopete/kopetemessage.cpp
+++ libkopete/kopetemessage.cpp
@@ -322,7 +322,7 @@ QString Message::escapedBody() const
 
 //		all this regex business is to take off the outer HTML document provided by QTextDocument
 //		remove the head
-		QRegExp badStuff ("<![^<>]*>|<head[^<>]*>.*</head[^<>]*>|</?html[^<>]*>|</?body[^<>]*>|</?p[^<>]*>");
+		QRegExp badStuff ("<![^<>]*>|<head[^<>]*>.*</head[^<>]*>|</?html[^<>]*>|</?body[^<>]*>");
 		html = html.remove (badStuff);
 //		remove newlines that may be present, since they end up being displayed in the chat window. real newlines are represented with <br>, so we know \n's are meaningless
 		html = html.remove ("\n");
