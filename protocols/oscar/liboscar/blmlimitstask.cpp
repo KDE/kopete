c355a33516a0449e8d4cd54427a5eb247a7afdc9
Roman Jarosz <kedgedev@gmail.com>
2008-05-28 21:08:49 +0000

    SVN commit 813801 by rjarosz:
    Backport commit 813783.
    Fix bug 162479: ICQ server send "Invalid SNAC header" error.
    Fix bug 154280: ICQ contact looses authorization after moving to another group.
    
    CCBUG: 162479
    CCBUG: 154280
    
    
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=813871

--- protocols/oscar/liboscar/blmlimitstask.cpp
+++ protocols/oscar/liboscar/blmlimitstask.cpp
@@ -83,7 +83,10 @@ void BLMLimitsTask::onGo()
 	kdDebug(OSCAR_RAW_DEBUG) << k_funcinfo << "Sending BLM limits request" << endl;
 	FLAP f = { 0x02, 0, 0 };
 	SNAC s = { 0x0003, 0x0002, 0x0000, client()->snacSequence() };
+
 	Buffer* buffer = new Buffer();
+	buffer->addTLV16( 0x0005, 0x0003 );
+
 	Transfer *t = createTransfer( f, s, buffer );
 	send( t );
 }
