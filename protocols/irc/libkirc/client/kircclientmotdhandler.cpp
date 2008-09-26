085e70eed3acc29595cc3ff2bb26ad1e2bb37b4c
Michel Hermier <michel.hermier@gmail.com>
2008-09-26 22:18:25 +0000

    Make the ClientMotdHandler doing the MOTD contained in a single text event using the MOTD_FULL event.
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=865181

--- protocols/irc/libkirc/client/kircclientmotdhandler.cpp
+++ protocols/irc/libkirc/client/kircclientmotdhandler.cpp
@@ -41,7 +41,12 @@ class KIrc::ClientMotdHandlerPrivate
 	: public KIrc::HandlerPrivate
 {
 public:
+	// FIXME: use a QMap instead so that we don't mix multiple MOTD when agregating
+	QString motd;
 };
+
+// FIXME: MOTD can comes from other servers, with our current implementation
+//        we say that MOTD come only from our server wich is wrong
 
 using namespace KIrc;
 
@@ -72,9 +77,14 @@ void ClientMotdHandler::receivedServerMessage(KIrc::Context *context, const KIrc
 KIrc::Handler::Handled ClientMotdHandler::RPL_MOTDSTART(KIrc::Context *context, const KIrc::Message &message, KIrc::Socket *socket)
 {
 	CHECK_ARGS(1, 1);
+	Q_D(ClientMotdHandler);
 
 	KIrc::ClientSocket *client = static_cast<KIrc::ClientSocket*>( socket );
-	KIrc::TextEvent *event=new KIrc::TextEvent( "Motd_START", client->server(), client->owner(), message.suffix() );
+	KIrc::TextEvent *event;
+
+	d->motd.clear();
+
+	event=new KIrc::TextEvent( "MOTD_START", client->server(), client->owner(), message.suffix() );
 	context->postEvent( event );
 
 	return KIrc::Handler::CoreHandled;
@@ -86,14 +96,21 @@ KIrc::Handler::Handled ClientMotdHandler::RPL_MOTDSTART(KIrc::Context *context, 
 KIrc::Handler::Handled ClientMotdHandler::RPL_MOTD(KIrc::Context *context, const KIrc::Message &message, KIrc::Socket *socket)
 {
 	CHECK_ARGS(1, 1);
-
-	//remove the "- " in front.
-	QByteArray text=message.suffix();
-	if ( text.startsWith( '-' ) )
-		text.remove( 0, 2 );
+	Q_D(ClientMotdHandler);
 
 	KIrc::ClientSocket *client = static_cast<KIrc::ClientSocket*>( socket );
-	KIrc::TextEvent *event=new KIrc::TextEvent( "Motd", client->server(), client->owner(), text );
+	KIrc::TextEvent *event;
+
+	//remove the "- " in front.
+	QByteArray text = message.suffix();
+	if ( text.startsWith( "- " ) )
+		text.remove( 0, 2 );
+
+	if ( !d->motd.isEmpty() )
+		d->motd.append( '\n' );
+	d->motd.append( text );
+
+	event = new KIrc::TextEvent( "MOTD", client->server(), client->owner(), text );
 	context->postEvent( event );
 
 	return KIrc::Handler::CoreHandled;
@@ -105,10 +122,16 @@ KIrc::Handler::Handled ClientMotdHandler::RPL_MOTD(KIrc::Context *context, const
 KIrc::Handler::Handled ClientMotdHandler::RPL_ENDOFMOTD(KIrc::Context *context, const KIrc::Message &message, KIrc::Socket *socket)
 {
 	CHECK_ARGS(1, 1);
+	Q_D(ClientMotdHandler);
 
-//	postMotdMotd(message);
 	KIrc::ClientSocket *client = static_cast<KIrc::ClientSocket*>( socket );
-	KIrc::TextEvent *event=new KIrc::TextEvent( "Motd_END", client->server(), client->owner(), message.suffix() );
+	KIrc::TextEvent *event;
+
+	event = new KIrc::TextEvent( "MOTD_FULL", client->server(), client->owner(), d->motd );
+	context->postEvent( event );
+	d->motd.clear();
+
+	event = new KIrc::TextEvent( "MOTD_END", client->server(), client->owner(), message.suffix() );
 	context->postEvent( event );
 
 	return KIrc::Handler::CoreHandled;
@@ -122,6 +145,6 @@ KIrc::Handler::Handled ClientMotdHandler::ERR_NOMOTD(KIrc::Context *context, con
 {
 	CHECK_ARGS(1, 1);
 
-//	postErrorMotd(message);
+//	postErrorEvent(message);
 	return KIrc::Handler::NotHandled;
 }
