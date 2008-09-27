60dbaf13a820dc4911ae9d08a8cd29f5953cc9f6
Will Stephenson <wstephenson@kde.org>
2008-09-27 08:55:54 +0000

    Backport r842122 from trunk - fix for https://bugzilla.novell.com:443/show_bug.cgi?id=354139
    svn path=/branches/KDE/3.5/kdenetwork/kopete/; revision=865247

--- protocols/groupwise/libgroupwise/tasks/getdetailstask.cpp
+++ protocols/groupwise/libgroupwise/tasks/getdetailstask.cpp
@@ -104,10 +104,27 @@ ContactDetails GetDetailsTask::extractUserDetails(Field::MultiField * details )
 		const Field::FieldListIterator end = fl.end();
 		for ( Field::FieldListIterator it = fl.begin(); it != end; ++it )
 		{
-			Field::SingleField * propField = static_cast<Field::SingleField *>( *it );
-			QString propName = propField->tag();
-			QString propValue = propField->value().toString();
-			propMap.insert( propName, propValue );
+			Field::SingleField * propField = dynamic_cast<Field::SingleField *>( *it );
+			if ( propField ) {
+				QString propName = propField->tag();
+				QString propValue = propField->value().toString();
+				propMap.insert( propName, propValue );
+			} else {
+				Field::MultiField * mf2;
+				if ( ( mf2 = dynamic_cast<Field::MultiField *>( *it ) ) ) {
+					Field::FieldList fl2 = mf2->fields();
+					const Field::FieldListIterator end = fl2.end();
+					for ( Field::FieldListIterator it2 = fl2.begin(); it2 != end; ++it2 )
+					{
+						propField = dynamic_cast<Field::SingleField *>( *it2 );
+						if ( propField ) {
+							QString propName = propField->tag();
+							QString propValue = propField->value().toString();
+							propMap.insert( propName, propValue );
+						}
+					}
+				}
+			}
 		}
 	}
 	if ( !propMap.empty() )
