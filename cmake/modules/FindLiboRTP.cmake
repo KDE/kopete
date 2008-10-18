78ee71be7ff8d4e238b5401b6ccb202c7171bb34
Detlev Casanova <detlev.casanova@gmail.com>
2008-10-18 11:51:13 +0000

    * Check for right oRTP version in cmake.
    svn path=/branches/work/soc-kopete-jingle/; revision=872877

--- cmake/modules/FindLiboRTP.cmake
+++ cmake/modules/FindLiboRTP.cmake
@@ -14,9 +14,17 @@ FIND_PATH(LIBORTP_INCLUDE_DIR ortp/ortp.h)
 FIND_LIBRARY(LIBORTP_LIBRARY NAMES ortp)
 
 IF( LIBORTP_INCLUDE_DIR AND LIBORTP_LIBRARY )
-#  EXECUTE_PROCESS(COMMAND gcc -o ortpversion -lortp ${LIBORTP_INCLUDE_DIR} ortpversion.c RESULT_VARIABLE result)
-#  MESSAGE(gcc -o ortpversion -lortp ${LIBORTP_INCLUDE_DIR} ortpversion.c)
-#  MESSAGE(${result})
-  SET( LIBORTP_FOUND TRUE)
-  MESSAGE( STATUS "Found libortp: ${LIBORTP_LIBRARY}")
+  
+  EXECUTE_PROCESS(COMMAND gcc -o ortpversion -lortp -L${LIBORTP_INCLUDE_DIR} ${CMAKE_SOURCE_DIR}/cmake/modules/ortpversion.c RESULT_VARIABLE result)
+  if (NOT result)
+    EXECUTE_PROCESS(COMMAND ./ortpversion 0 13 RESULT_VARIABLE result)
+  endif(NOT result)
+  if (NOT result)
+#   Version OK
+    SET( LIBORTP_FOUND TRUE)
+    MESSAGE( STATUS "Found libortp: ${LIBORTP_LIBRARY}")
+  elseif(result)
+#   Version non OK
+    MESSAGE( STATUS "You need ortp version 0.13 minimum !")
+  endif(NOT result)
 ENDIF( LIBORTP_INCLUDE_DIR AND LIBORTP_LIBRARY )
