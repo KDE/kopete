522ff1b397090f264f268e65bff8c0982f74f109
Roman Jarosz <kedgedev@gmail.com>
2008-11-08 19:33:11 +0000

    Revert commit 869875 and backport correct fix for crash during KDE logout. Please test if the crash is gone as I can't reproduce it here.
    CCBUG: 172011
    
    
    svn path=/branches/KDE/4.1/kdenetwork/kopete/; revision=881699

--- libkopete/kopetepluginmanager.cpp
+++ libkopete/kopetepluginmanager.cpp
@@ -62,14 +62,17 @@ public:
 		if ( shutdownMode != DoneShutdown )
 			kWarning( 14010 ) << "Destructing plugin manager without going through the shutdown process! Backtrace is: " << endl << kBacktrace();
 
-		// Quick cleanup of the remaining plugins, hope it helps
-		// Note that deleting it.value() causes slotPluginDestroyed to be called, which
-		// removes the plugin from the list of loaded plugins.
+		// Clean up loadedPlugins manually, because PluginManager can't access our global
+		// static once this destructor has started.
 		while ( !loadedPlugins.empty() )
 		{
 			InfoToPluginMap::ConstIterator it = loadedPlugins.begin();
 			kWarning( 14010 ) << "Deleting stale plugin '" << it.value()->objectName() << "'";
-			delete it.value();
+			KPluginInfo info = it.key();
+			Plugin *plugin = it.value();
+			loadedPlugins.remove(info);
+			plugin->disconnect(&instance, SLOT(slotPluginDestroyed(QObject*)));
+			delete plugin;
 		}
 	}
 
@@ -244,10 +247,6 @@ void PluginManager::slotShutdownTimeout()
 void PluginManager::slotShutdownDone()
 {
 	kDebug( 14010 ) ;
-
-	// Disconnect any remaining plugins (bug 172011).
-	for ( PluginManagerPrivate::InfoToPluginMap::ConstIterator it = _kpmp->loadedPlugins.begin(); it != _kpmp->loadedPlugins.end(); ++it )
-		disconnect( it.value(), SIGNAL(destroyed(QObject*)), this, 0 );
 
 	_kpmp->shutdownMode = PluginManagerPrivate::DoneShutdown;
 
