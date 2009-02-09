3b19a11c5049b08b239461821cb3f20e373404f1
Pino Toscano <pino@kde.org>
2009-02-09 14:32:11 +0000

    simplify
    svn path=/trunk/kdereview/kopete/protocols/skype/; revision=923830

--- protocols/skype/Messages.sh
+++ protocols/skype/Messages.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
-$EXTRACTRC `find -name \*.kcfg` >> rc.cpp || exit 11
-$EXTRACTRC `find -name \*.rc` >> rc.cpp || exit 12
-$EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 13
+$EXTRACTRC `find -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find -name \*.cpp` -o $podir/kopete_skype.pot
