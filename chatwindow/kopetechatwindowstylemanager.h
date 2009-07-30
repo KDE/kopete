8e1caad9fcc19a8c89e27d4877f915e09b436e03
Pierre-Alexandre St-Jean <pierrealexandre.stjean@gmail.com>
2009-07-30 20:02:46 +0000

    Refactored to use the new archive factory
    svn path=/branches/work/soc-chatwindow/; revision=1004758

--- chatwindow/kopetechatwindowstylemanager.h
+++ chatwindow/kopetechatwindowstylemanager.h
@@ -2,8 +2,9 @@
     kopetechatwindowstylemanager.h - Manager all chat window styles
 
     Copyright (c) 2005      by Michaël Larouche     <larouche@kde.org>
+    Copyright (c) 2009      by Pierre-Alexandre St-Jean     <pierrealexandre.stjean@gmail.com>
 
-    Kopete    (c) 2002-2005 by the Kopete developers <kopete-devel@kde.org>
+    Kopete    (c) 2002-2009 by the Kopete developers <kopete-devel@kde.org>
 
     *************************************************************************
     *                                                                       *
@@ -21,6 +22,7 @@
 #include <QObject>
 #include <QHash>
 
+#include <karchive.h>
 #include <kfileitem.h>
 #include <kopete_export.h>
 
@@ -45,110 +47,113 @@ class ChatWindowStyle;
  */
 class KOPETECHATWINDOW_SHARED_EXPORT ChatWindowStyleManager : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	/**
-	 * The StyleInstallStatus enum. It gives better return value for installStyle().
-	 * - StyleInstallOk : The install went fine.
-	 * - StyleNotValid : The archive didn't contain a valid Chat Window style.
-	 * - StyleNoDirectoryValid : It didn't find a suitable directory to install the theme.
-	 * - StyleCannotOpen : The archive couldn't be openned.
-	 * - StyleUnknow : Unknow error.
-	 */
-	enum StyleInstallStatus { StyleInstallOk = 0, StyleNotValid, StyleNoDirectoryValid, StyleCannotOpen, StyleUnknow };
+    /**
+     * The StyleInstallStatus enum. It gives better return value for installStyle().
+     * - StyleInstallOk : The install went fine.
+     * - StyleNotValid : The archive didn't contain a valid Chat Window style.
+     * - StyleNoDirectoryValid : It didn't find a suitable directory to install the theme.
+     * - StyleCannotOpen : The archive couldn't be openned.
+     * - StyleUnknow : Unknow error.
+     */
+    enum StyleInstallStatus { StyleInstallOk = 0, StyleNotValid, StyleNoDirectoryValid, StyleCannotOpen, StyleUnknow };
 
-	/**
-	 * Destructor.
-	 */
-	~ChatWindowStyleManager();
+    /**
+     * Destructor.
+     */
+    ~ChatWindowStyleManager();
 
-	/**
-	 * Singleton access to this class.
-	 * @return the single instance of this class.
-	 */
-	static ChatWindowStyleManager *self();
+    /**
+     * Singleton access to this class.
+     * @return the single instance of this class.
+     */
+    static ChatWindowStyleManager *self();
 
-	/**
-	 * List all availables styles.
-	 * Init KDirLister and thus KDirWatch that watch for new styles.
-	 */
-	void loadStyles();
+    /**
+     * List all availables styles.
+     * Init KDirLister and thus KDirWatch that watch for new styles.
+     */
+    void loadStyles();
 
-	/**
-	 * Get all available styles.
-	 */
-	QStringList getAvailableStyles() const;
+    /**
+     * Get all available styles.
+     */
+    QStringList getAvailableStyles() const;
 
 public slots:
-	/**
-	 * Install a new style into user style directory
-	 * Note that you must pass a path to a archive.
-	 *
-	 * @param styleBundlePath Path to the container file to install.
-	 * @return A status code from StyleInstallStatus enum.
-	 */
-	int installStyle(const QString &styleBundlePath);
+    /**
+     * Install a new style into user style directory
+     * Note that you must pass a path to a archive.
+     *
+     * @param styleBundlePath Path to the container file to install.
+     * @return A status code from StyleInstallStatus enum.
+     */
+    int installStyle(const QString &styleBundlePath);
 
-	/**
-	 * Remove a style from user style directory
-	 *
-	 * @param styleName the name of the style to remove.
-	 * @return true if the deletion went without problems.
-	 */
-	bool removeStyle(const QString &styleName);
+    /**
+     * Remove a style from user style directory
+     *
+     * @param styleName the name of the style to remove.
+     * @return true if the deletion went without problems.
+     */
+    bool removeStyle(const QString &styleName);
 
-	/**
-	 * Get a instance of a ChatWindowStyle from the pool.
-	 * If they are no instance for the specified style, it gets created.
-	 * DO NOT DELETE the resulting pointer, it is handled by this class.
-	 *
-	 * @param styleName name for the specified style. If style with this name
-	 *        exists in both global and user directories, the user one will be returned
-	 * @return the instance of ChatWindow for the specified style or 0 if valid style
-	 *         wasn't found. DO NOT DELETE IT.
-	 */
-	ChatWindowStyle *getStyleFromPool(const QString &styleName);
+    /**
+     * Get a instance of a ChatWindowStyle from the pool.
+     * If they are no instance for the specified style, it gets created.
+     * DO NOT DELETE the resulting pointer, it is handled by this class.
+     *
+     * @param styleName name for the specified style. If style with this name
+     *        exists in both global and user directories, the user one will be returned
+     * @return the instance of ChatWindow for the specified style or 0 if valid style
+     *         wasn't found. DO NOT DELETE IT.
+     */
+    ChatWindowStyle *getStyleFromPool(const QString &styleName);
 
-	/**
-	 * Get a instance of a ChatWindowStyle from the pool.
-	 * If they are no instance for the specified style, it gets created.
-	 * If the style doesn't exists or is invalid default style is returned or 0.
-	 * DO NOT DELETE the resulting pointer, it is handled by this class.
-	 *
-	 * @param styleName name for the specified style. If style with this name
-	 *        exists in both global and user directories, the user one will be returned
-	 * @return the instance of ChatWindow for the specified style or 0 if valid style
-	 *         wasn't found. DO NOT DELETE IT.
-	 */
-	ChatWindowStyle *getValidStyleFromPool(const QString &styleName);
+    /**
+     * Get a instance of a ChatWindowStyle from the pool.
+     * If they are no instance for the specified style, it gets created.
+     * If the style doesn't exists or is invalid default style is returned or 0.
+     * DO NOT DELETE the resulting pointer, it is handled by this class.
+     *
+     * @param styleName name for the specified style. If style with this name
+     *        exists in both global and user directories, the user one will be returned
+     * @return the instance of ChatWindow for the specified style or 0 if valid style
+     *         wasn't found. DO NOT DELETE IT.
+     */
+    ChatWindowStyle *getValidStyleFromPool(const QString &styleName);
 
 signals:
-	/**
-	 * This signal is emitted when all styles finished to list.
-	 * Used to inform and/or update GUI.
-	 */
-	void loadStylesFinished();
+    /**
+     * This signal is emitted when all styles finished to list.
+     * Used to inform and/or update GUI.
+     */
+    void loadStylesFinished();
 
 private slots:
-	/**
-	 * KDirLister found new files.
-	 * @param dirList new files found.
-	 */
-	void slotNewStyles(const KFileItemList &dirList);
-	/**
-	 * KDirLister finished a job.
-	 * Emit loadStylesFinished() if they are no directory left in the stack.
-	 */
-	void slotDirectoryFinished();
+    /**
+     * KDirLister found new files.
+     * @param dirList new files found.
+     */
+    void slotNewStyles(const KFileItemList &dirList);
+    /**
+     * KDirLister finished a job.
+     * Emit loadStylesFinished() if they are no directory left in the stack.
+     */
+    void slotDirectoryFinished();
 
 private:
-	/**
-	 * Private constructor(it's a singleton class)
-	 * Call loadStyles() to list all avaiable styles.
-	 */
-	ChatWindowStyleManager(QObject *parent = 0);
-	class Private;
-	Private * const d;
+    /**
+     * Private constructor(it's a singleton class)
+     * Call loadStyles() to list all avaiable styles.
+     */
+    ChatWindowStyleManager(QObject *parent = 0);
+    class Private;
+    Private * const d;
+
+    bool archiveIsValid(const KArchiveDirectory* rootDir);
+    QString findWritableStyleDirs();
 };
 
 #endif
