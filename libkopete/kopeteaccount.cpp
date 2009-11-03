b8132a202c3bc5accc36104ddd951cee00e9f694
Roman Jarosz <kedgedev@gmail.com>
2009-11-03 21:47:37 +0000

    Backport commit 1044089.
    Don't show "You are not allowed to add yourself to the contact list" after login \
    because user can't do anything about it from Kopete. If user tries to add own contact \
    30 seconds after login to protocols which doesn't support it or it wasn't implemented \
    yet we show the error.
    
    CCBUG: 212704
    
    svn path=/branches/KDE/4.3/kdenetwork/kopete/; revision=1044484

--- libkopete/kopeteaccount.cpp
+++ libkopete/kopeteaccount.cpp
@@ -91,6 +91,7 @@ public:
 	QString customIcon;
 	Kopete::OnlineStatus restoreStatus;
 	Kopete::StatusMessage restoreMessage;
+	QDateTime lastLoginTime;
 };
 
 Account::Account( Protocol *parent, const QString &accountId )
@@ -275,9 +276,17 @@ Kopete::MetaContact* Account::addContact( const QString &contactId, const QStrin
 
 	if ( !protocol()->canAddMyself() && contactId == d->myself->contactId() )
 	{
-		KMessageBox::queuedMessageBox( Kopete::UI::Global::mainWidget(), KMessageBox::Error,
-			i18n("You are not allowed to add yourself to the contact list. The addition of \"%1\" to account \"%2\" will not take place.", contactId, accountId()), i18n("Error Creating Contact")
-		);
+		if ( isConnected() && d->lastLoginTime.secsTo(QDateTime::currentDateTime()) > 30 )
+		{
+			KMessageBox::queuedMessageBox( Kopete::UI::Global::mainWidget(), KMessageBox::Error,
+			                               i18n("You are not allowed to add yourself to the contact list. The addition of \"%1\" to account \"%2\" will not take place.", contactId, accountId()), i18n("Error Creating Contact")
+			                             );
+		}
+		else
+		{
+			kWarning(14010) << "You are not allowed to add yourself to the contact list. The addition of" << contactId
+			                << "to account" << accountId() << "will not take place.";
+		}
 		return false;
 	}
 
@@ -341,9 +350,18 @@ bool Account::addContact(const QString &contactId , MetaContact *parent, AddMode
 {
 	if ( !protocol()->canAddMyself() && contactId == myself()->contactId() )
 	{
-	    	KMessageBox::error( Kopete::UI::Global::mainWidget(),
-			i18n("You are not allowed to add yourself to the contact list. The addition of \"%1\" to account \"%2\" will not take place.", contactId, accountId()), i18n("Error Creating Contact")
-		);
+		if ( isConnected() && d->lastLoginTime.secsTo(QDateTime::currentDateTime()) > 30 )
+		{
+			KMessageBox::queuedMessageBox( Kopete::UI::Global::mainWidget(), KMessageBox::Error,
+			                               i18n("You are not allowed to add yourself to the contact list. The addition of \"%1\" to account \"%2\" will not take place.", contactId, accountId()), i18n("Error Creating Contact")
+			                             );
+		}
+		else
+		{
+			kWarning(14010) << "You are not allowed to add yourself to the contact list. The addition of" << contactId
+			                << "to account" << accountId() << "will not take place.";
+		}
+		
 		return 0L;
 	}
 
@@ -468,6 +486,9 @@ void Account::slotOnlineStatusChanged( Contact * /* contact */,
 {
 	const bool wasOffline = !oldStatus.isDefinitelyOnline();
 	const bool isOffline  = !newStatus.isDefinitelyOnline();
+
+	if (wasOffline && !isOffline)
+		d->lastLoginTime = QDateTime::currentDateTime();
 
 	if ( wasOffline || newStatus.status() == OnlineStatus::Offline )
 	{
