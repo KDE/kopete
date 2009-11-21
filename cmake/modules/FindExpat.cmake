8f1381da3d7b3fa8b73ced02dde10501b972b50f
Pali Rohár <pali.rohar@gmail.com>
2009-11-21 10:21:17 +0000

    converted automake files to cmake in google libjingle removed automake and vcproj files in google libjingle build necessary google libjingle applications together with kopete jabber protocol
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=1052305

---
+++ cmake/modules/FindExpat.cmake
@@ -0,0 +1,28 @@
+# cmake macro to test Expat
+
+# Copyright (c) 2009, Pali Rohár <pali.rohar@gmail.com>
+#
+# EXPAT_FOUND
+# EXPAT_INCLUDE_DIR
+# EXPAT_LIBRARY
+
+include ( FindPackageHandleStandardArgs )
+
+if ( EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY )
+	set ( EXPAT_FOUND true )
+else ( ILBC_INCLUDE_DIR AND ILBC_LIBRARY )
+	find_path ( EXPAT_INCLUDE_DIR expat.h )
+	find_library ( EXPAT_LIBRARY NAMES expat )
+
+	if ( EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY )
+		set ( EXPAT_FOUND true )
+		message ( STATUS "Found Expat: ${EXPAT_LIBRARY}" )
+	else ( EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY )
+		set ( EXPAT_FOUND false )
+		message ( STATUS "Not found Expat" )
+	endif ( EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY )
+
+	mark_as_advanced ( EXPAT_INCLUDE_DIR EXPAT_LIBRARY )
+
+endif ( EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY )
+
