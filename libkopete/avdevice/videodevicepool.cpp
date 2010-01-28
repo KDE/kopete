fcd5eb9c47067c8282fd76aee28cf21e303723c1
Frank Schaefer <fschaefer.oss@googlemail.com>
2010-01-28 12:44:58 +0000

    Fix loading of the video-settings in case of device-switching with multiple clients
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=1081469

--- libkopete/avdevice/videodevicepool.cpp
+++ libkopete/avdevice/videodevicepool.cpp
@@ -685,7 +685,7 @@ unsigned int VideoDevicePool::inputs()
 void VideoDevicePool::loadSelectedDevice()
 {
 	kDebug() << "called";
-	if (hasDevices() && (m_clients == 0))
+	if (hasDevices())
 	{
 		KConfigGroup config(KGlobal::config(), "Video Device Settings");
 		QString currentdevice = config.readEntry("Current Device", QString());
@@ -719,7 +719,7 @@ void VideoDevicePool::loadSelectedDevice()
 void VideoDevicePool::loadDeviceConfig()
 {
 	kDebug() << "called";
-	if (hasDevices() && (m_clients == 0))
+	if (hasDevices())
 	{
 		KConfigGroup config(KGlobal::config(), "Video Device Settings");
 		// Load input and apply
