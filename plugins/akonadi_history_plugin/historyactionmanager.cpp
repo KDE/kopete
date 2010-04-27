af075f2264ba855d097446b521a259a3bb2eb7e0
kaushik saurabh <roideuniverse@gmail.com>
2010-04-27 11:41:00 +0000

    The history action manager classes to manage history and addition of tags
    svn path=/branches/work/soc-logging-framework/kopete/; revision=1119556

---
+++ plugins/akonadi_history_plugin/historyactionmanager.cpp
@@ -0,0 +1,63 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "historyactionmanager.h"
+#include "akonadihistoryplugin.h"
+
+#include <kgenericfactory.h>
+#include <KAction>
+#include <KActionCollection>
+
+#include "kopetechatsession.h"
+
+HistoryActionManager::HistoryActionManager(Kopete::ChatSession* parent, QObject* hPlugin)
+      : QObject(parent) ,KXMLGUIClient(parent) , m_manager(parent) 
+{
+	kDebug() << "" ;
+	m_hPlugin = qobject_cast<AkonadiHistoryPlugin*>(hPlugin);
+	
+	setComponentData( m_hPlugin->xmlGuiInstance() );
+
+	// Refuse to build this client, it is based on wrong parameters
+	if ( !m_manager || m_manager->members().isEmpty() )
+		deleteLater();
+	
+	kDebug() << "setting up actions";
+	
+	KAction *applytag = new KAction( KIcon("view-history"), i18n("Apply &Label" ), this);
+	actionCollection()->addAction("tagThisChatHistory", applytag);
+	applytag->setShortcut(KShortcut (Qt::CTRL + Qt::Key_L));
+
+	connect( applytag, SIGNAL(triggered(bool)), this, SLOT(slotAddTag()) );
+	
+	setXMLFile ( "tagchatui.rc" );
+	
+}
+
+HistoryActionManager::~HistoryActionManager()
+{
+
+}
+
+void HistoryActionManager::slotAddTag()
+{
+	kDebug() << "Slot add Tag called :) ";
+}
+
---
+++ plugins/akonadi_history_plugin/historyactionmanager.h
@@ -0,0 +1,49 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#ifndef HISTORYACTIONMANAGER_H
+#define HISTORYACTIONMANAGE
+
+#include "akonadihistoryplugin.h"
+
+#include <KXMLGUIClient>
+
+namespace Kopete { class ChatSession; }
+
+class HistoryActionManager : public QObject , public KXMLGUIClient
+{
+    Q_OBJECT
+public:
+	
+	HistoryActionManager(Kopete::ChatSession* parent = 0 , QObject* hPlugin = 0 );
+	~HistoryActionManager();
+	
+private:
+	
+	QPointer<AkonadiHistoryPlugin> m_hPlugin;
+	Kopete::ChatSession * m_manager;
+	
+private slots:
+	void slotAddTag();
+		
+	
+};
+
+#endif // HISTORYACTIONMANAGER_H
