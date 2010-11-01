95877fc9aa6352e13f8e15ae5f3d6ef19b0603c8
Pino Toscano <pino@kde.org>
2010-11-01 11:35:39 +0000

    do not extract messages from sources in 'tests' subdirectories
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=1191894

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 $EXTRACTRC --context="Translators: The %FOO% placeholders are variables that are substituted in the code, please leave them untranslated" --tag-group=none --tag kopete-i18n styles/*.xsl > xml_doc.cpp || exit 11
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg | egrep -v '(libkopete/compat|protocols/testbed)'` >> rc.cpp || exit 12
-LIST=`find . -name \*.h -o -name \*.cpp -o -name \*.c | egrep -v '(libkopete/compat|protocols/testbed|protocols/jabber/kioslave)'`
+LIST=`find . -name \*.h -o -name \*.cpp -o -name \*.c | grep -v '/tests/' | egrep -v '(libkopete/compat|protocols/testbed|protocols/jabber/kioslave)'`
 if test -n "$LIST"; then
 	$XGETTEXT $LIST rc.cpp -o $podir/kopete.pot
 fi
