91b9ec55ecd562af70b2d975e92a2e65b783d7a1
Lamarque Souza <lamarque@kde.org>
2011-03-05 03:18:18 +0000

    Backporting r1223433: improve contact search system.
    svn path=/branches/KDE/4.6/kdenetwork/kopete/; revision=1223777

--- kopete/contactlist/contactlistproxymodel.cpp
+++ kopete/contactlist/contactlistproxymodel.cpp
@@ -24,8 +24,10 @@
 #include "kopetegroup.h"
 #include "kopetemetacontact.h"
 #include "kopetecontactlist.h"
+#include "kopetecontact.h"
 #include "kopeteappearancesettings.h"
 #include "kopeteitembase.h"
+#include <kabc/stdaddressbook.h>
 
 namespace Kopete {
 
@@ -128,9 +130,33 @@ bool ContactListProxyModel::filterAcceptsRow ( int sourceRow, const QModelIndex 
 		int connectedContactsCount = model->data( current, Kopete::Items::ConnectedCountRole ).toInt();
 		int totalContactsCount = model->data( current, Kopete::Items::TotalCountRole ).toInt();
 
-		// TODO: Find out how to check if we should hide the group if no metaContact was found.
 		if ( !filterRegExp().isEmpty() )
-			return true;
+		{
+			// This shows or hides the contacts group folder if something was found.
+			// Walk through the group's metacontacts and see it the search result was found
+			// If it is found then we can show this group folder.
+			// This is fairly slow with > group 10000 contacts. Reasonable? (ginge)
+			for ( int i = 0; i < model->rowCount( current ); i++ )
+			{
+				QModelIndex qmi = model->index( i, 0, current );
+
+				if ( model->data( qmi, Kopete::Items::TypeRole ) != Kopete::Items::MetaContact )
+					continue;
+
+				QObject* mcObject = qVariantValue<QObject*>( model->data( qmi, Kopete::Items::ObjectRole ) );
+				Kopete::MetaContact *mc = qobject_cast<Kopete::MetaContact*>( mcObject );
+
+				// Do a better search.
+				if ( searchContactInfo( mc, filterRegExp() ) )
+				{
+					qobject_cast<Kopete::Group*>(groupObject)->setExpanded(true);
+					return true;
+				}
+			}
+
+			return false;
+		}
+
 
 		if ( !showEmpty && totalContactsCount == 0 )
 			return false;
@@ -145,8 +171,11 @@ bool ContactListProxyModel::filterAcceptsRow ( int sourceRow, const QModelIndex 
 	{
 		if ( !filterRegExp().isEmpty() )
 		{
-			QString mcName = model->data( current, Qt::DisplayRole ).toString();
-			return mcName.contains( filterRegExp() );
+			QObject* contactObject = qVariantValue<QObject*>( model->data( current, Kopete::Items::ObjectRole ) );
+			Kopete::MetaContact *mc = qobject_cast<Kopete::MetaContact*>(contactObject);
+
+			// Do a better search
+			return searchContactInfo( mc, filterRegExp() );
 		}
 
 		bool alwaysVisible = model->data( current, Kopete::Items::AlwaysVisible ).toBool();
@@ -202,6 +231,34 @@ void ContactListProxyModel::forceSort()
 	sortScheduled = false;
 	sort( -1, Qt::AscendingOrder );
 	sort( 0, Qt::AscendingOrder );
+}
+
+// Better search. Now a search will look in the metacontact display name, the invidual account contact names and any email addresses.
+bool ContactListProxyModel::searchContactInfo(Kopete::MetaContact *mc, QRegExp searchPattern) const
+{
+	// Check the display name
+	if ( mc->displayName().contains( searchPattern ) )
+		return true;
+
+	// Check the address book
+	KABC::Addressee addressee = KABC::StdAddressBook::self()->findByUid( mc->kabcId() );
+	if ( !addressee.isEmpty() )
+	{
+		QString emailAddr = addressee.fullEmail();
+
+		if ( emailAddr.contains( searchPattern ) )
+			return true;
+	}
+
+	// Check alternative names
+	foreach( Kopete::Contact* c , mc->contacts() )
+	{
+		// Search each metacontacts' contacts
+		if ( c->contactId().contains( searchPattern ) )
+			return true;
+	}
+
+	return false;
 }
 
 }
