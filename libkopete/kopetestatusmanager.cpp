db996aac9f76e63c29a8391b5c3684963da15862
Lamarque Souza <lamarque@kde.org>
2011-04-17 10:20:43 +0000

    Set the status message when using the away toggle shortcut.
    FIXED-IN: 4.6.3
    
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=1228308

--- libkopete/kopetestatusmanager.cpp
+++ libkopete/kopetestatusmanager.cpp
@@ -326,6 +326,8 @@ void StatusManager::setGlobalStatusMessage( const Kopete::StatusMessage &statusM
 	config.writeEntry( "GlobalStatusMessage", d->globalStatusMessage.message() );
 	config.sync();
 
+	// Iterate each connected account, updating its status message but keeping the
+	// same onlinestatus.
 	QList<Kopete::Account*> accountList = Kopete::AccountManager::self()->accounts();
 	foreach ( Kopete::Account *account, accountList )
 	{
@@ -444,7 +446,8 @@ bool StatusManager::globalAway()
 {
 	return ( d->globalStatusCategory == OnlineStatusManager::Away ||
 	         d->globalStatusCategory == OnlineStatusManager::ExtendedAway ||
-	         d->globalStatusCategory == OnlineStatusManager::Busy );
+	         d->globalStatusCategory == OnlineStatusManager::Busy ||
+	         d->globalStatusCategory == OnlineStatusManager::Offline );
 }
 
 void StatusManager::accountUnregistered( const Kopete::Account *account )
