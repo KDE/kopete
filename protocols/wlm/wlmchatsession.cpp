c57f69dd0cf0b23fae94922a795d18cfab62f0a4
Raymond Wooninck <tittiatcoke@gmail.com>
2013-03-14 08:06:52 +0000

    Revert commit regarding giflib5, this should go to reviewboard first
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=1344214

--- protocols/wlm/wlmchatsession.cpp
+++ protocols/wlm/wlmchatsession.cpp
@@ -65,10 +65,6 @@
 #include "wlmchatsessioninkaction.h"
 #ifdef HAVE_GIFLIB
 #include <gif_lib.h>
-/* old giflib has no GIFLIB_MAJOR, define to avoid cpp warnings */
-#ifndef GIFLIB_MAJOR
-#define GIFLIB_MAJOR 4
-#endif
 #endif
 
 WlmChatSession::WlmChatSession (Kopete::Protocol * protocol,
@@ -471,15 +467,11 @@ printGifErrorMessage()
 {
 #ifdef HAVE_GIFLIB
 #ifdef HAVE_GIF_ERROR_STRING // giflib 4.2.0+
-#if GIFLIB_MAJOR >= 5
-        fprintf(stderr, "GIF-LIB error (exact reporting not implemented)\n");
-#else
         const char * errorString = GifErrorString();
         if (errorString)
             fprintf(stderr, "GIF-LIB error: %s\n", errorString);
         else
             fprintf(stderr, "GIF-LIB undefined error: %d\n", GifError());
-#endif
 #else // older giflib versions, libungif
         PrintGifError();
 #endif // HAVE_GIF_ERROR_STRING
@@ -491,10 +483,6 @@ void
 WlmChatSession::convertToGif( const QPixmap & ink, QString filename)
 {
 #ifdef HAVE_GIFLIB
-#if GIFLIB_MAJOR >= 5
-#define FreeMapObject  GifFreeMapObject
-#define MakeMapObject  GifMakeMapObject
-#endif
     int i, status;
     GifFileType *GifFile;
     ColorMapObject *screenColourmap;
@@ -537,11 +525,7 @@ WlmChatSession::convertToGif( const QPixmap & ink, QString filename)
         }
     }
 
-#if GIFLIB_MAJOR >= 5
-    GifFile= EGifOpenFileName(QFile::encodeName(filename).constData(), 0, NULL);
-#else
     GifFile= EGifOpenFileName(QFile::encodeName(filename).constData(), 0);
-#endif
     if (!GifFile) {
         FreeMapObject(imageColourmap);
         FreeMapObject(screenColourmap);
