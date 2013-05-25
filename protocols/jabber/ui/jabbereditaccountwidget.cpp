a2fda8d374f79aaf9db38fd03cd26ab47710815d
Pali Rohár <pali.rohar@gmail.com>
2013-05-25 14:01:32 +0000

    Fix crashing kopete compiled with google talk support when creating new jabber account
    * account()->myself() is NULL when creating new account, so use mID->text ()
    
    svn path=/trunk/KDE/kdenetwork/kopete/; revision=1356240

--- protocols/jabber/ui/jabbereditaccountwidget.cpp
+++ protocols/jabber/ui/jabbereditaccountwidget.cpp
@@ -363,7 +363,7 @@ bool JabberEditAccountWidget::validateData ()
 	}
 
 #ifdef GOOGLETALK_SUPPORT
-	XMPP::Jid jid ( account()->myself()->contactId () );
+	XMPP::Jid jid ( mID->text () );
 	if ( GoogleTalk->isChecked() && ( mServer->text().trimmed() != "talk.google.com" && jid.domain() != "gmail.com" ) )
 	{
 		KMessageBox::sorry(this, i18n("Google Talk libjingle support is only for GTalk/Gmail account, which connect to server talk.google.com."), i18n("Invalid Google Talk"));
