665097dc0c760c76de756f8211fff8c7a367ab78
Burkhard Lück <lueck@hube-lueck.de>
2013-07-25 20:48:04 +0200

    remove separate catalog for cryptography plugins, messages are extracted to kopete main catalog like messages from all other plugins

--- plugins/cryptography/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` rc.cpp -o $podir/kopete-cryptography.pot
-rm -f rc.cpp
