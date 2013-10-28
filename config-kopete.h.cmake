a08ab3f88b6bcb55c4cd2a075c99e3b9438f0701
Pali Rohár <pali.rohar@gmail.com>
2013-10-28 14:24:10 +0100

    Remove USE_SMPPPD because it is not used

--- config-kopete.h.cmake
+++ config-kopete.h.cmake
@@ -8,9 +8,6 @@
 /* Define to compile with GSM SMS support */
 /* #undef INCLUDE_SMSGSM */
 
-/* enable support for the smpppd */
-/* #undef USE_SMPPPD */
-
 /* Define if you have a tm_gmtoff member in struct tm */
 #define HAVE_TM_GMTOFF 1
 
