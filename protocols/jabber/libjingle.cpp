94dd49285061ca035b5684dbb18231c5bbd80600
Pali Rohár <pali.rohar@gmail.com>
2015-06-17 18:03:45 +0200

    Set lowest resource priority for jabber libjingle part

--- protocols/jabber/libjingle.cpp
+++ protocols/jabber/libjingle.cpp
@@ -294,6 +294,8 @@ void Libjingle::read() {
 			c = true;
 			emit(connected());
 //			qDebug() << "connected";
+			//Set lowest resource priority
+			write("priority -127");
 		} else if ( line.startsWith("logged out...") ) {
 			//We are logouted - bad username or passwd
 			QString res = line.section("logged out...", -1).trimmed();
